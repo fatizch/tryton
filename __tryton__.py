fc4a84e0f799333baa7e3998d606e67a31c6f61c
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2012-04-02 16:56:53 +0200

    example tryton module

---
+++ __tryton__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'CoopBusiness',
+    'version' : '0.0.1',
+    'author' : 'Jonathan Schemoul',
+    'email': 'jonathan.Schemoul@gmail.com',
+    'website': '',
+    'description': 'CoopBusiness module',
+    'depends' : [],
+    'xml' : ['opportunity.xml'
+             ],
+    'translation': ['party',
+                    'ir'
+                    ],
+}
