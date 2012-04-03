41a902cc74aab732abf6fe5e7646cd3b635b4455
Romain Seon <romain.seon@coopengo.com>
2012-04-03 18:22:58 +0200

    1st delivery for business rules

--- coopbusiness/product.py
+++ coopbusiness/product.py
@@ -6,8 +6,9 @@ class Coverage(ModelSQL, ModelView):
     _name = 'product.coverage'     
     _description = __doc__     
     name = fields.Char('Name', required=True, select=1)     
-    code = fields.Char('CodeToto', size=10,required=True, select=1)    
-	
+    code = fields.Char('Code', size=10,required=True, select=1)    
+    managers = fields.One2Many('product.businessrulemanager','belongs_to','Business rule managers')
+    
 Coverage()
 
 class ProductOptionsCoverage(ModelSQL):
@@ -15,9 +16,9 @@ class ProductOptionsCoverage(ModelSQL):
     _name = 'product-options-coverage'
     product = fields.Many2One('product.product','Product',select=1, required=True)
     coverage = fields.Many2One('product.coverage','Coverage',select=1, required=True)
-	
+
 ProductOptionsCoverage()	
-	
+
 class Product(ModelSQL, ModelView):
     'Product'     
     _name = 'product.product'     
@@ -25,5 +26,28 @@ class Product(ModelSQL, ModelView):
     name = fields.Char('Name', required=True, select=1)     
     code = fields.Char('Code', size=10,required=True, select=1)     
     options = fields.Many2Many('product-options-coverage','product','coverage','Possible Options')
-	
+
 Product()
+
+class BusinessRule(ModelSQL, ModelView):
+    'Business rule'
+    _name = 'product.businessrule'
+    _description = __doc__
+    name = fields.Char('Name', required=True, select=1)     
+    code = fields.Char('Code', size=10,required=True, select=1)
+    manager = fields.Many2One('product.businessrulemanager','Manager', required=True)    
+
+BusinessRule()
+
+class BusinessRuleManager(ModelSQL, ModelView):
+    'Business rule manager'
+    _name = 'product.businessrulemanager'
+    _description = __doc__
+    name = fields.Char('Name', required=True, select=1)     
+    code = fields.Char('Code', size=10,required=True, select=1)
+    belongs_to = fields.Many2One('product.coverage','Belongs to', required=True)
+    business_rules = fields.One2Many('product.businessrule','manager','Business rules')
+
+
+BusinessRuleManager()
+    
