fc9a8c4d840acce2e137fb1be0b3e3ada7813fa5
Jean Cavallo <jean.cavallo@coopengo.com>
2012-04-05 10:45:27 +0200

    Reworked directory structure to enable module setups

---
+++ insurance_product/__tryton__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'CoopBusiness',
+    'version' : '0.0.1',
+    'author' : 'Jonathan Schemoul',
+    'email': 'jonathan.Schemoul@gmail.com',
+    'website': '',
+    'description': 'CoopBusiness module',
+    'depends' : ['party'],
+    'xml' : ['product.xml'],
+    'translation': ['ir'
+                    ],
+}
