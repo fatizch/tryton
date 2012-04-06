4ef622333369d99b6b4f3cf425e1d907f70a5bdd
Jean Cavallo <jean.cavallo@coopengo.com>
2012-04-06 09:19:25 +0200

    Restoring previous version of __tryton__ files

---
+++ insurance_contract/__tryton__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'CoopBusiness',
+    'version' : '0.0.1',
+    'author' : 'Coopengo',
+    'email': 'admin@coopengo.com',
+    'website': '',
+    'description': 'CoopBusiness module',
+    'depends' : ['insurance_product','party'],
+    'xml' : ['contract.xml'
+             ],
+    'translation': ['ir'
+                    ],
+}
