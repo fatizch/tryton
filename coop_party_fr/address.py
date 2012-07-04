cfaf7bd4fb4e2542280f9b28bbbe5e23a0703dc6
Romain Seon <romain.seon@coopengo.com>
2012-07-04 11:07:11 +0200

    add support for AFNOR address on fr layer

---
+++ coop_party_fr/address.py
@@ -0,0 +1,88 @@
+import copy
+
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.modules.party.address import STATES, DEPENDS
+__all__ = ['Address']
+
+
+class Address(ModelSQL, ModelView):
+    "Address"
+    __name__ = 'party.address'
+
+    line1 = fields.Char('Recipient', 38,
+        '''AFNOR - Line 1
+        For individual : Identity of the addressee -
+        Form of address or given name or surname...
+        For companies : Organization identification -
+        Organization name, Legal Status''',
+        required=True, states=STATES, depends=DEPENDS)
+    line2 = fields.Char('Add. complement', 38,
+        '''AFNOR - Line 2
+        For individual : Delivery Point Access Data
+        Door or Letterbox number, floor, staircase
+        For companies : Individual Identification Form of Address -
+        Given Name, Surname, function, Department''',
+        states=STATES, depends=DEPENDS)
+    line3 = fields.Char('Building', 38,
+        '''AFNOR - Line 3
+        Delivery point location
+        Wing or Building or Construction or Industrial zone''',
+        states=STATES, depends=DEPENDS)
+
+    @classmethod
+    def __setup__(cls):
+        super(Address, cls).__setup__()
+        cls.street = copy.copy(cls.street)
+        cls.street.size = 38
+        if cls.street.on_change is None:
+            cls.street.on_change = []
+        cls.street.on_change.append('street')
+        cls.street.help = '''AFNOR - Line 4
+            Street number or plot and thoroughfare -
+            Street or Avenue or Village...'''
+
+        cls.streetbis = copy.copy(cls.streetbis)
+        cls.streetbis.size = 38
+        cls.streetbis.string = 'Post Office'
+        cls.streetbis.help = '''AFNOR - Line 5
+            Delivery Service
+            Identification Thoroughfare Complement BP (P.O box)
+            and Locality (if different from the distribution area indicator'''
+        if cls.streetbis.on_change is None:
+            cls.streetbis.on_change = []
+        cls.streetbis.on_change.append('streetbis')
+
+        cls.city = copy.copy(cls.city)
+        if cls.city.on_change is None:
+            cls.city.on_change = []
+        cls.city.on_change.append('city')
+
+    def on_change_street(self):
+        #AFNOR rule, no comma after street number and line 4 should be upper
+        return {'street': self.street.replace(',', '').upper()}
+
+    def on_change_streetbis(self):
+        #AFNOR rule, line 5 should be in uppercase
+        return {'streetbis': self.streetbis.upper()}
+
+    def on_change_city(self):
+        #AFNOR rule, line 6 must be in uppercase
+        return {'city': self.city.upper()}
+
+    def get_full_address(self, name):
+        res = ''
+        if self.line1:
+            res = self.line1 + '\n'
+        if self.line2:
+            res += self.line2 + '\n'
+        if self.line3:
+            res += self.line3 + '\n'
+        if self.street:
+            res += self.street + '\n'
+        if self.streetbis:
+            res += self.streetbis + '\n'
+        if self.zip and self.city:
+            res += '%s %s' % (self.zip, self.city)
+        if self.country and self.country.code != 'FR':
+            res += self.country.name
+        return res
