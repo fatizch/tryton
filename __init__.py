87fb88246cf15a4be7c755c392c8e61c1e982335
Jean Cavallo <jean.cavallo@coopeno.com>
2012-07-16 15:43:02 +0200

    Index: __init__.py
    ===================================================================
    new file mode 100644

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from trytond.pool import Pool
+from .rule_engine import *
+
+
+def register():
+    Pool.register(
+        Context,
+        Rule,
+        TestCase,
+        TestCaseValue,
+        TreeElement,
+        ContextTreeElement,
+        TestRuleStart,
+        TestRuleTest,
+        RunTestsReport,
+        module='rule_engine', type_='model')
+    Pool.register(
+        TestRule,
+        CreateTestValues,
+        RunTests,
+        module='rule_engine', type_='wizard')
