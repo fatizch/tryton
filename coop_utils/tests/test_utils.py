96d69d4a1289b979deaf485e38046330561f6a29
Jean CAVALLO <jean.cavallo@coopengo.com>
2012-08-21 17:34:33 +0200

    Moved PricingResultLine tests from utils to product

--- coop_utils/tests/test_utils.py
+++ coop_utils/tests/test_utils.py
@@ -8,7 +8,6 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
-from trytond.modules.coop_utils import PricingResultLine
 from trytond.modules.coop_utils import utils
 
 
@@ -27,36 +26,6 @@ class LaboratoryTestCase(unittest.TestCase):
         Test depends.
         '''
         test_depends()
-
-    def test0010Pricing_Line_Results(self):
-        '''
-            Tests pricing lines
-        '''
-        p1 = PricingResultLine(value=10, name='Alpha', desc=[])
-        p2 = PricingResultLine(value=30, name='Beta', desc=[])
-
-        p_add = p1 + p2
-
-        self.assertEqual(p_add.value, 40)
-        self.assertEqual(p_add.name, '')
-        self.assertEqual(len(p_add.desc), 2)
-        self.assertEqual(p_add.desc, [p1, p2])
-
-        p1 += p2
-
-        self.assertEqual(p1.value, 40)
-        self.assertEqual(p1.name, 'Alpha')
-        self.assertEqual(len(p1.desc), 1)
-
-        sub_p = p1.desc[0]
-
-        self.assertEqual(sub_p.value, p2.value)
-        self.assertEqual(sub_p.name, p2.name)
-        self.assertEqual(sub_p.desc, p2.desc)
-
-        self.assertEqual(sub_p, p2)
-
-        p1 += p2
 
     def test0020get_module_path(self):
         self.assert_(utils.get_module_path('coop_utils'))
--- insurance_product/tests/test_product.py
+++ insurance_product/tests/test_product.py
@@ -9,6 +9,7 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
+from trytond.modules.insurance_product import PricingResultLine
 
 
 class LaboratoryTestCase(unittest.TestCase):
@@ -49,9 +50,12 @@ class LaboratoryTestCase(unittest.TestCase):
             euro.code = 'EUR'
             euro.save()
 
+            # Coverage A
+
             prm_a = self.pricing()
-            prm_a.price = 12
             prm_a.config_kind = 'simple'
+            prm_a.price = 12
+            prm_a.per_sub_elem_price = 1
 
             gbr_a = self.gbr()
             gbr_a.kind = 'ins_product.pricing_rule'
@@ -61,8 +65,8 @@ class LaboratoryTestCase(unittest.TestCase):
             gbr_a.pricing_rule = [prm_a]
 
             prm_b = self.pricing()
-            prm_b.price = 15
             prm_b.config_kind = 'simple'
+            prm_b.price = 15
 
             gbr_b = self.gbr()
             gbr_b.kind = 'ins_product.pricing_rule'
@@ -80,28 +84,139 @@ class LaboratoryTestCase(unittest.TestCase):
             coverage_a.name = 'Alpha Coverage'
             coverage_a.start_date = datetime.date.today()
 
+            coverage_a.pricing_mgr = [brm_a]
+
+            coverage_a.save()
+
+            # Coverage B
+
+            prm_c = self.pricing()
+            prm_c.config_kind = 'simple'
+            prm_c.price = 30
+
+            gbr_c = self.gbr()
+            gbr_c.kind = 'ins_product.pricing_rule'
+            gbr_c.start_date = datetime.date.today()
+            gbr_c.end_date = datetime.date.today() + \
+                                            datetime.timedelta(days=10)
+            gbr_c.pricing_rule = [prm_c]
+
+            brm_b = self.brm()
+            brm_b.business_rules = [gbr_c]
+
             coverage_b = self.coverage()
             coverage_b.code = 'BET'
             coverage_b.name = 'Beta Coverage'
             coverage_b.start_date = datetime.date.today() + \
                                             datetime.timedelta(days=5)
 
-            coverage_a.pricing_mgr = [brm_a]
+            coverage_b.pricing_mgr = [brm_b]
 
-            coverage_a.save()
             coverage_b.save()
 
-            coverage_a, = self.coverage.search([('code', '=', 'ALP')])
-            coverage_b, = self.coverage.search([('code', '=', 'BET')])
+            # Coverage C
 
-            product_a = self.product()
+            erm_a = self.eligibility()
+            erm_a.config_kind = 'simple'
+            erm_a.is_eligible = False
+
+            gbr_d = self.gbr()
+            gbr_d.kind = 'ins_product.eligibility_rule'
+            gbr_d.start_date = datetime.date.today()
+            gbr_d.eligibility_rule = [erm_a]
+
+            brm_c = self.brm()
+            brm_c.business_rules = [gbr_d]
+
+            coverage_c = self.coverage()
+            coverage_c.code = 'GAM'
+            coverage_c.name = 'Gamma Coverage'
+            coverage_c.start_date = datetime.date.today()
+
+            coverage_c.eligibility_mgr = [brm_c]
+
+            coverage_c.save()
+
+            # Coverage D
+
+            erm_d = self.eligibility()
+            erm_d.config_kind = 'simple'
+            erm_d.is_eligible = True
+            erm_d.is_sub_elem_eligible = False
+
+            gbr_g = self.gbr()
+            gbr_g.kind = 'ins_product.eligibility_rule'
+            gbr_g.start_date = datetime.date.today()
+            gbr_g.eligibility_rule = [erm_d]
+
+            brm_f = self.brm()
+            brm_f.business_rules = [gbr_g]
+
+            coverage_d = self.coverage()
+            coverage_d.code = 'DEL'
+            coverage_d.name = 'Delta Coverage'
+            coverage_d.start_date = datetime.date.today()
+
+            coverage_d.eligibility_mgr = [brm_f]
+
+            coverage_d.save()
+
+            # Product Eligibility Manager
+
+            erm_b = self.eligibility()
+            erm_b.config_kind = 'simple'
+            erm_b.is_eligible = True
+
+            gbr_e = self.gbr()
+            gbr_e.kind = 'ins_product.eligibility_rule'
+            gbr_e.start_date = datetime.date.today()
+            gbr_e.eligibility_rule = [erm_b]
+
+            brm_d = self.brm()
+            brm_d.business_rules = [gbr_e]
+
+            # Product
+
+            product_a = self.Product()
             product_a.code = 'AAA'
             product_a.name = 'Awesome Alternative Allowance'
             product_a.start_date = datetime.date.today()
-            product_a.options = [coverage_a, coverage_b]
+            product_a.options = [
+                coverage_a, coverage_b, coverage_c, coverage_d]
+            product_a.eligibility_mgr = [brm_d]
             product_a.save()
 
             transaction.cursor.commit()
+
+    def test0020Pricing_Line_Results(self):
+        '''
+            Tests pricing lines
+        '''
+        p1 = PricingResultLine(value=10, name='Alpha', desc=[])
+        p2 = PricingResultLine(value=30, name='Beta', desc=[])
+
+        p_add = p1 + p2
+
+        self.assertEqual(p_add.value, 40)
+        self.assertEqual(p_add.name, '')
+        self.assertEqual(len(p_add.desc), 2)
+        self.assertEqual(p_add.desc, [p1, p2])
+
+        p1 += p2
+
+        self.assertEqual(p1.value, 40)
+        self.assertEqual(p1.name, 'Alpha')
+        self.assertEqual(len(p1.desc), 1)
+
+        sub_p = p1.desc[0]
+
+        self.assertEqual(sub_p.value, p2.value)
+        self.assertEqual(sub_p.name, p2.name)
+        self.assertEqual(sub_p.desc, p2.desc)
+
+        self.assertEqual(sub_p, p2)
+
+        p1 += p2
 
 
 def suite():
