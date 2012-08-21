a5a9160683b5d03b6870d8d33a71036bf76af9ec
Cédric Krier <ced@b2ck.com>
2012-08-21 14:29:47 +0200

    Backed out changeset 6e26d6c2e388

--- coop_utils/utils.py
+++ coop_utils/utils.py
@@ -1,12 +1,9 @@
 import ConfigParser
 import datetime
-from itertools import chain
 import os
 
 from trytond.pool import Pool
 from trytond.model import Model
-from trytond.transaction import Transaction
-
 from trytond.model import fields as fields
 # Needed for proper encoding / decoding of objects as strings
 from trytond.protocols.jsonrpc import JSONEncoder, object_hook
@@ -631,54 +628,3 @@ def get_coop_config(section, option):
         config = ConfigParser.ConfigParser()
         config.read(os.path.join(coop_utils, 'coop.cfg'))
         return config.get(section, option)
-
-
-class One2ManyDomain(fields.One2Many):
-    def get(self, ids, model, name, values=None):
-        '''
-        Return target records ordered.
-        '''
-        pool = Pool()
-        Relation = pool.get(self.model_name)
-        if self.field in Relation._fields:
-            field = Relation._fields[self.field]
-        else:
-            field = Relation._inherit_fields[self.field][2]
-        res = {}
-        for i in ids:
-            res[i] = []
-        ids2 = []
-        for i in range(0, len(ids), Transaction().cursor.IN_MAX):
-            sub_ids = ids[i:i + Transaction().cursor.IN_MAX]
-            if field._type == 'reference':
-                references = ['%s,%s' % (model.__name__, x) for x in sub_ids]
-                clause = [(self.field, 'in', references)]
-            else:
-                clause = [(self.field, 'in', sub_ids)]
-            clause.append(self.domain)
-            ids2.append(map(int, Relation.search(clause, order=self.order)))
-
-        cache = Transaction().cursor.get_cache(Transaction().context)
-        cache.setdefault(self.model_name, {})
-        ids3 = []
-        for i in chain(*ids2):
-            if i in cache[self.model_name] \
-                    and self.field in cache[self.model_name][i]:
-                res[cache[self.model_name][i][self.field].id].append(i)
-            else:
-                ids3.append(i)
-
-        if ids3:
-            for i in Relation.read(ids3, [self.field]):
-                if field._type == 'reference':
-                    _, id_ = i[self.field].split(',')
-                    id_ = int(id_)
-                else:
-                    id_ = i[self.field]
-                res[id_].append(i['id'])
-
-        index_of_ids2 = dict((i, index)
-            for index, i in enumerate(chain(*ids2)))
-        for id_, val in res.iteritems():
-            res[id_] = tuple(sorted(val, key=lambda x: index_of_ids2[x]))
-        return res
