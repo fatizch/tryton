0c863cec734159f312d0e17bb1bb8acf85f072bb
Romain Seon <romain.seon@coopengo.com>
2012-08-23 11:52:20 +0200

    first version of generic architecture for proteus test case

---
+++ insurance_product/test_case/proteus_test_case.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+def launch_test_case():
+    from proteus import Model
+    from decimal import Decimal
+
+    Product = Model.get('ins_product.product')
+    coverage = Model.get('ins_product.coverage')
+    brm = Model.get('ins_product.business_rule_manager')
+    gbr = Model.get('ins_product.generic_business_rule')
+    pricing = Model.get('ins_product.pricing_rule')
+
+    gbr_a = gbr()
+    gbr_a.kind = 'ins_product.pricing_rule'
+    gbr_a.start_date = datetime.date.today()
+    gbr_a.end_date = datetime.date.today() + \
+                                    datetime.timedelta(days=10)
+    prm_a = gbr_a.pricing_rule[0]
+    prm_a.price = Decimal(12.0)
+    prm_a.per_sub_elem_price = Decimal(1.0)
+
+    gbr_b = gbr()
+    gbr_b.kind = 'ins_product.pricing_rule'
+    gbr_b.start_date = datetime.date.today() + \
+                                    datetime.timedelta(days=11)
+    gbr_b.end_date = datetime.date.today() + \
+                                    datetime.timedelta(days=20)
+    prm_b = gbr_b.pricing_rule[0]
+    prm_b.price = k(15.0)
+
+    brm_a = brm()
+    brm_a.business_rules.append(gbr_a)
+    brm_a.business_rules.append(gbr_b)
+
+    coverage_a = coverage()
+    coverage_a.code = 'ALP'
+    coverage_a.name = 'Alpha Coverage'
+    coverage_a.start_date = datetime.date.today()
+
+    gbr_c = gbr()
+    gbr_c.kind = 'ins_product.pricing_rule'
+    gbr_c.start_date = datetime.date.today()
+    gbr_c.end_date = datetime.date.today() + \
+                                    datetime.timedelta(days=10)
+    prm_c = gbr_c.pricing_rule[0]
+    prm_c.price = Decimal(30.0)
+
+    brm_b = brm()
+    brm_b.business_rules.append(gbr_c)
+
+    brm_b.save()
+    brm_a.save()
+
+    coverage_b = coverage()
+    coverage_b.code = 'BET'
+    coverage_b.name = 'Beta Coverage'
+    coverage_b.start_date = datetime.date.today() + \
+                                    datetime.timedelta(days=5)
+
+#    coverage_b.pricing_mgr = []
+    coverage_b.pricing_mgr.append(brm_b)
+
+    coverage_a.pricing_mgr.append(brm_a)
+
+    coverage_a.save()
+    coverage_b.save()
+
+    product_a = Product()
+    product_a.code = 'AAA'
+    product_a.name = 'Awesome Alternative Allowance'
+    product_a.start_date = datetime.date.today()
+    product_a.options.append(coverage_a)
+    product_a.options.append(coverage_b)
+    product_a.save()
---
+++ test_case/proteus_test_case.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import datetime
+import os
+import ConfigParser
+
+from proteus import Model, Wizard
+from proteus import config as pconfig
+
+
+DIR = os.path.abspath(os.path.join(os.path.normpath(__file__), '..'))
+
+CODE_TEMPLATE = '''def execute_test():
+    from %s import launch_test_case
+
+    launch_test_case()
+
+execute_test()
+'''
+
+
+def install_modules(config, modules_to_install):
+    Module = Model.get('ir.module.module')
+    modules = Module.find([
+        ('name', 'in', modules_to_install),
+        ('state', '!=', 'installed'),
+    ])
+    Module.install([x.id for x in modules], config.context)
+    modules = [x.name for x in Module.find([('state', '=', 'to install')])]
+    Wizard('ir.module.module.install_upgrade').execute('upgrade')
+
+    ConfigWizardItem = Model.get('ir.module.module.config_wizard.item')
+    for item in ConfigWizardItem.find([('state', '!=', 'done')]):
+        item.state = 'done'
+        item.save()
+
+    return modules
+
+
+def get_cfg_as_dict(cfg, section, items_as_list=None):
+    '''this function get a config file as input and convert the section into
+    a dictionnary.
+    All items given in the items_as_list will be converted as list
+    [config]
+    database_type = sqlite
+    modules:
+        insurance_party
+        insurance_product
+
+    get_cfg_as_dict(cfg, 'config', ['modules'])'''
+
+    cfg_parser = ConfigParser.ConfigParser()
+    with open(cfg) as fp:
+        cfg_parser.readfp(fp)
+    cfg_dict = dict(cfg_parser.items(section))
+
+    #Setting the items as list
+    if items_as_list:
+        for key in items_as_list:
+            if key in cfg_dict:
+                cfg_dict[key] = cfg_dict[key].strip().splitlines()
+
+    #Setting boolean values
+    for (key, value) in cfg_dict.items():
+        try:
+            if value.upper() == 'TRUE':
+                cfg_dict[key] = True
+            if value.upper() == 'FALSE':
+                cfg_dict[key] = False
+        except:
+            pass
+
+    return cfg_dict
+
+
+def get_test_cfg(test_config_file):
+    cfg_dict = get_cfg_as_dict(test_config_file, 'options', ['modules'])
+    cfg_dict['config_file'] = os.path.abspath(
+            os.path.join(DIR, cfg_dict['config_file'], 'trytond.conf'))
+
+    trytond_cfg_dict = get_cfg_as_dict(cfg_dict['config_file'], 'options')
+
+    return dict(cfg_dict.items() + trytond_cfg_dict.items())
+
+
+def delete_db_if_necessary(cfg_dict):
+    if cfg_dict['create_db']:
+        db = os.path.join(cfg_dict['data_path'],
+            cfg_dict['database_name'] + '.' + cfg_dict['db_type'])
+        if os.path.isfile(db):
+            print 'Deleting DB : %s' % db
+            os.remove(db)
+
+
+def get_module_depends(module):
+    from trytond.modules import get_module_info
+
+    res = set()
+    info = get_module_info(module)
+    for dependency in info.get('depends', []):
+        res |= set(get_module_depends(dependency))
+    res.add(module)
+    return list(res)
+
+
+def is_coop_module(module):
+    return 'coop_utils' in get_module_depends(module)
+
+
+def get_modules_to_update(from_modules):
+    from trytond.modules import create_graph
+
+    modules_set = set()
+    for cur_module in from_modules:
+        modules_set |= set(get_module_depends(cur_module))
+    graph = create_graph(list(modules_set))[0]
+    return [x.name for x in graph if is_coop_module(x.name)]
+
+
+def install_or_update_modules(from_modules):
+    modules = get_modules_to_update(from_modules)
+    for cur_module in modules:
+        print '=' * 80
+        cur_file = os.path.abspath(
+            os.path.join(DIR, '..', cur_module, 'test_case',
+                'proteus_test_case.py'))
+        if not os.path.isfile(cur_file):
+            print 'Missing test case file for module %s' % cur_module
+            continue
+        print 'Running test case for module % s' % cur_module
+        code = CODE_TEMPLATE % ('trytond.modules.' + cur_module + '.test_case')
+        exec code
+
+
+def launch_proteus_test_case(test_config_file):
+    cfg_dict = get_test_cfg(test_config_file)
+
+    delete_db_if_necessary(cfg_dict)
+
+    config = pconfig.set_trytond(
+        database_name=cfg_dict['database_name'],
+        user=cfg_dict['user'],
+        database_type=cfg_dict['db_type'],
+        language=cfg_dict['language'],
+        password=cfg_dict['password'],
+        config_file=cfg_dict['config_file'],
+        )
+
+    modules = install_modules(config, cfg_dict['modules'])
+    for module in cfg_dict['modules']:
+        if module in modules:
+            print 'Module %s installed' % module
+        else:
+            print 'Module %s already installed' % module
+
+    install_or_update_modules(cfg_dict['modules'])
+
+
+if __name__ == '__main__':
+    launch_proteus_test_case('test.cfg')
