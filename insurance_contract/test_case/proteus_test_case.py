8f7ff45ba26b0c951399e0751299320ee2f26b6d
Jean CAVALLO <jean.cavallo@coopengo.com>
2012-08-27 17:08:20 +0200

    Updated contract Test Case

---
+++ insurance_contract/test_case/proteus_test_case.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from proteus import Model, Wizard
+
+
+def launch_test_case(cfg_dict):
+    Party = Model.get('party.party')
+    Product = Model.get('ins_product.product')
+    on_party, = Party.find(limit=1)
+    on_product, = Product.find([('code', '=', 'AAA')], limit=1)
+    wizard = Wizard('ins_contract.subs_process')
+    wizard._config._context['from_session'] = wizard.session_id
+    wizard._config.context['from_session'] = wizard.session_id
+    wizard.form.start_date += datetime.timedelta(days=2)
+    wizard.form.subscriber = on_party
+    wizard.form.product = on_product
+    wizard.execute('steps_next')
+    wizard.form.options[0].start_date += \
+        datetime.timedelta(days=-4)
+    wizard.form.options[0].start_date += \
+        datetime.timedelta(days=5)
+    wizard.form.options[1].start_date += \
+        datetime.timedelta(days=-1)
+    wizard.form.options[1].start_date += \
+        datetime.timedelta(days=1)
+    wizard.execute('steps_next')
+    wizard.execute('steps_next')
+    wizard.execute('steps_complete')
