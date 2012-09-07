4e8bf2b66c0aa17b1117a61ec0153d50e0db6866
Romain Seon <romain.seon@coopengo.com>
2012-09-07 12:07:16 +0200

    Do not recreate products if they already exists or with a specific option

--- insurance_product/test_case/proteus_test_case.py
+++ insurance_product/test_case/proteus_test_case.py
@@ -7,37 +7,76 @@ from decimal import Decimal
 from proteus import Model
 
 
-def get_models():
-    res = {}
-    res['Product'] = Model.get('ins_product.product')
-    res['TreeElement'] = Model.get('rule_engine.tree_element')
-    res['Context'] = Model.get('rule_engine.context')
-    res['RuleEngine'] = Model.get('rule_engine')
-    res['TestCase'] = Model.get('rule_engine.test_case')
-    res['TestCaseValue'] = Model.get('rule_engine.test_case.value')
-    return res
+def update_cfg_dict_with_models(cfg_dict):
+    cfg_dict['Currency'] = Model.get('currency.currency')
+    cfg_dict['Coverage'] = Model.get('ins_product.coverage')
+    cfg_dict['Product'] = Model.get('ins_product.product')
+    cfg_dict['TreeElement'] = Model.get('rule_engine.tree_element')
+    cfg_dict['Context'] = Model.get('rule_engine.context')
+    cfg_dict['RuleEngine'] = Model.get('rule_engine')
+    cfg_dict['TestCase'] = Model.get('rule_engine.test_case')
+    cfg_dict['TestCaseValue'] = Model.get('rule_engine.test_case.value')
+    cfg_dict['Insurer'] = Model.get('party.insurer')
+    return cfg_dict
 
 
-def create_product(models, code, name, options=None):
-    if not is_in_db(models, 'Product', code):
-        product = models['Product']()
-        product.code = code
-        product.name = name
-        product.start_date = datetime.date.today()
-        if options:
-            product.options[:] = options
+def get_or_create_product(cfg_dict, code, name, options=None):
+    product = get_object_from_db(cfg_dict, 'Product', 'code', code)
+    if product:
         return product
+    product = cfg_dict['Product']()
+    product.code = code
+    product.name = name
+    product.start_date = datetime.date.today()
+    if options:
+        product.options[:] = options
+    return product
 
 
-def is_in_db(models, model, code):
-    return len(models[model].find([('code', '=', code)], limit=1)) > 0
-
-
-def create_AAA_Product(models, code, name):
-    product_a = create_product(models, code, name)
-    if not product_a:
+def get_object_from_db(cfg_dict, model, key=None, value=None, domain=None,
+        force_search=False):
+    if not force_search and cfg_dict['re_create_if_already_exists']:
         return None
-    coverage = Model.get('ins_product.coverage')
+    if not domain:
+        domain = []
+    if key and value:
+        domain.append((key, '=', value))
+    instances = cfg_dict[model].find(domain, limit=1)
+    if instances:
+        return instances[0]
+
+
+def try_to_save_object(cfg_dict, cur_object):
+    if not cfg_dict['re_create_if_already_exists']:
+        cur_object.save()
+    #if we try to save one object which already exists, we could have error
+    #with constraints
+    try:
+        cur_object.save()
+    except:
+        print 'Exception raised when trying to save', cur_object
+
+
+def get_or_create_coverage(cfg_dict, code, name, date=None):
+    coverage = get_object_from_db(cfg_dict, 'Coverage', 'code', code)
+    if coverage:
+        return coverage
+    coverage = cfg_dict['Coverage']()
+    coverage.code = code
+    coverage.name = name
+    if date:
+        coverage.start_date = date
+    else:
+        coverage.start_date = datetime.date.today()
+    coverage.insurer = get_object_from_db(cfg_dict, 'Insurer',
+        force_search=True)
+    return coverage
+
+
+def create_AAA_Product(cfg_dict, code, name):
+    product_a = get_or_create_product(cfg_dict, code, name)
+    if product_a.id > 0:
+        return product_a
     brm = Model.get('ins_product.business_rule_manager')
     gbr = Model.get('ins_product.generic_business_rule')
 
@@ -63,11 +102,7 @@ def create_AAA_Product(models, code, name):
     brm_a.business_rules.append(gbr_a)
     brm_a.business_rules.append(gbr_b)
 
-    coverage_a = coverage()
-    coverage_a.code = 'ALP'
-    coverage_a.name = 'Alpha Coverage'
-    coverage_a.start_date = datetime.date.today()
-
+    coverage_a = get_or_create_coverage(cfg_dict, 'ALP', 'Alpha Coverage')
     gbr_c = gbr()
     gbr_c.kind = 'ins_product.pricing_rule'
     gbr_c.start_date = datetime.date.today()
@@ -79,29 +114,26 @@ def create_AAA_Product(models, code, name):
     brm_b = brm()
     brm_b.business_rules.append(gbr_c)
 
-    brm_b.save()
-    brm_a.save()
+    try_to_save_object(cfg_dict, brm_b)
+    try_to_save_object(cfg_dict, brm_a)
 
-    coverage_b = coverage()
-    coverage_b.code = 'BET'
-    coverage_b.name = 'Beta Coverage'
-    coverage_b.start_date = datetime.date.today() + \
-                                    datetime.timedelta(days=5)
+    coverage_b = get_or_create_coverage(cfg_dict, 'BET', 'Beta Coverage',
+        datetime.date.today() + datetime.timedelta(days=5))
 
 #    coverage_b.pricing_mgr = []
     coverage_b.pricing_mgr.append(brm_b)
 
     coverage_a.pricing_mgr.append(brm_a)
 
-    coverage_a.save()
-    coverage_b.save()
+    try_to_save_object(cfg_dict, coverage_a)
+    try_to_save_object(cfg_dict, coverage_b)
 
     product_a.options.append(coverage_a)
     product_a.options.append(coverage_b)
-    product_a.save()
+    try_to_save_object(cfg_dict, product_a)
 
 
-def get_or_create_tree_element(models, cur_type, description, name=None,
+def get_or_create_tree_element(cfg_dict, cur_type, description, name=None,
         namespace=None):
     cur_domain = []
     if cur_type == 'function':
@@ -109,42 +141,43 @@ def get_or_create_tree_element(models, cur_type, description, name=None,
         cur_domain.append(('name', '=', name))
     if cur_type == 'folder':
         cur_domain.append(('name', '=', name))
-    tree_elements = models['TreeElement'].find(cur_domain)
-    if len(tree_elements) > 0:
-        return tree_elements[0]
-    te = models['TreeElement']()
+    tree_element = get_object_from_db(cfg_dict, 'TreeElement',
+        domain=cur_domain)
+    if tree_element:
+        return tree_element
+    te = cfg_dict['TreeElement']()
     te.type = cur_type
     te.name = name
     te.description = description
     te.namespace = namespace
-    te.save()
+    try_to_save_object(cfg_dict, te)
     return te
 
 
-def append_inexisting_elements(cur_object, list_name, cur_list):
+def append_inexisting_elements(cfg_dict, cur_object, list_name, cur_list):
     cur_list = getattr(cur_object, list_name, [])
     for child in cur_list:
         if not child in cur_list:
             cur_list.append(child)
-    cur_object.save()
+    try_to_save_object(cfg_dict, cur_object)
     return cur_object
 
 
-def get_or_create_context(models, name):
-    contexts = models['Context'].find([('name', '=', name)])
-    if len(contexts) > 0:
-        return contexts[0]
-    ct = models['Context']()
+def get_or_create_context(cfg_dict, name):
+    ct = get_object_from_db(cfg_dict, 'Context', 'name', name)
+    if ct:
+        return ct
+    ct = cfg_dict['Context']()
     ct.name = name
-    ct.save()
+    try_to_save_object(cfg_dict, ct)
     return ct
 
 
-def create_rule(models, ct, name):
-    rules = models['RuleEngine'].find([('name', '=', name)])
-    if len(rules) > 0:
-        return rules[0]
-    rule = models['RuleEngine']()
+def get_or_create_rule(cfg_dict, ct, name):
+    rule = get_object_from_db(cfg_dict, 'RuleEngine', 'name', name)
+    if rule:
+        return rule
+    rule = cfg_dict['RuleEngine']()
     rule.name = name
     rule.context = ct
     rule.code = '''
@@ -154,20 +187,20 @@ if years_between(birthdate, today()) > 40:
     return False
 return True'''
 
-    tcv = models['TestCaseValue']()
+    tcv = cfg_dict['TestCaseValue']()
     tcv.name = 'get_person_birthdate'
     tcv.value = 'datetime.date(2000, 11, 02)'
 
-    tc = models['TestCase']()
+    tc = cfg_dict['TestCase']()
     tc.description = 'Test'
     tc.values.append(tcv)
     tc.expected_result = '(True, [], [])'
 
-    tcv1 = models['TestCaseValue']()
+    tcv1 = cfg_dict['TestCaseValue']()
     tcv1.name = 'get_person_birthdate'
     tcv1.value = 'datetime.date(1950, 11, 02)'
 
-    tc1 = models['TestCase']()
+    tc1 = cfg_dict['TestCase']()
     tc1.description = 'Test1'
     tc1.values.append(tcv1)
     tc1.expected_result = '(False, ["Subscriber too old (max: 40)"], [])'
@@ -175,45 +208,45 @@ return True'''
     rule.test_cases.append(tc)
     rule.test_cases.append(tc1)
 
-    rule.save()
+    try_to_save_object(cfg_dict, rule)
 
     return rule
 
 
-def create_rule_engine_data(models):
-    te1 = get_or_create_tree_element(models, 'function', 'Name',
+def create_rule_engine_data(cfg_dict):
+    te1 = get_or_create_tree_element(cfg_dict, 'function', 'Name',
         'get_person_name', 'ins_product.rule_sets.person')
-    te2 = get_or_create_tree_element(models, 'function', 'Birthday',
+    te2 = get_or_create_tree_element(cfg_dict, 'function', 'Birthday',
         'get_person_birthdate', 'ins_product.rule_sets.person')
-    te = get_or_create_tree_element(models, 'folder', 'Person')
-    append_inexisting_elements(te, 'children', [te1, te2])
+    te = get_or_create_tree_element(cfg_dict, 'folder', 'Person')
+    append_inexisting_elements(cfg_dict, te, 'children', [te1, te2])
 
-    te3 = get_or_create_tree_element(models, 'function', 'Years between',
+    te3 = get_or_create_tree_element(cfg_dict, 'function', 'Years between',
         'years_between', 'rule_engine.tools_functions')
-    te5 = get_or_create_tree_element(models, 'function', 'Today', 'today',
+    te5 = get_or_create_tree_element(cfg_dict, 'function', 'Today', 'today',
         'rule_engine.tools_functions')
 
-    te6 = get_or_create_tree_element(models, 'function', 'Add message',
+    te6 = get_or_create_tree_element(cfg_dict, 'function', 'Add message',
         'message', 'rule_engine.tools_functions')
 
-    te4 = get_or_create_tree_element(models, 'folder', 'Tools')
-    append_inexisting_elements(te4, 'children', [te3, te5, te6])
+    te4 = get_or_create_tree_element(cfg_dict, 'folder', 'Tools')
+    append_inexisting_elements(cfg_dict, te4, 'children', [te3, te5, te6])
 
-    ct = get_or_create_context(models, 'test_context')
-    append_inexisting_elements(ct, 'allowed_elements', [te, te4])
+    ct = get_or_create_context(cfg_dict, 'test_context')
+    append_inexisting_elements(cfg_dict, ct, 'allowed_elements', [te, te4])
 
-    return create_rule(models, ct, 'test_rule')
+    return get_or_create_rule(cfg_dict, ct, 'test_rule')
 
 
-def create_BBB_product(models, code, name):
-    product_b = create_product(models, code, name)
-    if not product_b:
-        return None
+def create_BBB_product(cfg_dict, code, name):
+    product_b = get_or_create_product(cfg_dict, code, name)
+    if product_b.id > 0:
+        return product_b
     coverage = Model.get('ins_product.coverage')
     brm = Model.get('ins_product.business_rule_manager')
     gbr = Model.get('ins_product.generic_business_rule')
 
-    rule = create_rule_engine_data(models)
+    rule = create_rule_engine_data(cfg_dict)
 
     coverage_a, = coverage.find([('code', '=', 'ALP')], limit=1)
     coverage_b, = coverage.find([('code', '=', 'BET')], limit=1)
@@ -237,7 +270,7 @@ def create_BBB_product(models, code, name):
 
     coverage_c.eligibility_mgr.append(brm_c)
 
-    coverage_c.save()
+    try_to_save_object(cfg_dict, coverage_c)
 
     # Coverage D
     gbr_g = gbr()
@@ -258,7 +291,7 @@ def create_BBB_product(models, code, name):
 
     coverage_d.eligibility_mgr.append(brm_f)
 
-    coverage_d.save()
+    try_to_save_object(cfg_dict, coverage_d)
 
     # Product Eligibility Manager
     gbr_e = gbr()
@@ -278,19 +311,24 @@ def create_BBB_product(models, code, name):
     product_b.options.append(coverage_c)
     product_b.options.append(coverage_d)
     product_b.eligibility_mgr.append(brm_d)
-    product_b.save()
+    try_to_save_object(cfg_dict, product_b)
+
+
+def get_or_create_currency(cfg_dict):
+    currency = get_object_from_db(cfg_dict, 'Currency', 'code', 'EUR',
+        force_search=True)
+    if currency:
+        return currency
+    currency = cfg_dict['Currency']()
+    currency.name = 'Euro'
+    currency.symbol = u'€'
+    currency.code = 'EUR'
+    try_to_save_object(cfg_dict, currency)
+    return currency
 
 
 def launch_test_case(cfg_dict):
-    currency = Model.get('currency.currency')
-    #We need to create the currency manually because it's needed
-    #on the default currency for product and coverage
-    euro = currency()
-    euro.name = 'Euro'
-    euro.symbol = u'€'
-    euro.code = 'EUR'
-    euro.save()
-
-    models = get_models()
-    create_AAA_Product(models, 'AAA', 'Awesome Alternative Allowance')
-    create_BBB_product(models, 'BBB', 'Big Bad Bully')
+    cfg_dict = update_cfg_dict_with_models(cfg_dict)
+    get_or_create_currency(cfg_dict)
+    create_AAA_Product(cfg_dict, 'AAA', 'Awesome Alternative Allowance')
+    create_BBB_product(cfg_dict, 'BBB', 'Big Bad Bully')
