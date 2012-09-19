e92b2a2850c4201fd7029665722b294322bda08e
Romain Seon <romain.seon@coopengo.com>
2012-09-19 18:23:29 +0200

    Fusionning tax flat_value and rate_value to value + add a few UI enhancements for tax

--- coop_account/tax.py
+++ coop_account/tax.py
@@ -1,8 +1,6 @@
 from trytond.model import fields as fields
 from trytond.pool import Pool
 
-from trytond.pyson import Eval
-
 from trytond.modules.coop_utils import CoopSQL, CoopView, utils as utils
 
 
@@ -24,9 +22,43 @@ class TaxDesc(CoopSQL, CoopView):
         'coop_account.tax_version',
         'my_tax_desc',
         'Versionned Rates')
-
+    description = fields.Text('Description')
+    current_value = fields.Function(fields.Char('Current Value'),
+        'get_current_value')
     # To do : CTD0069
     # Check there is no overlapping of versions before save
+
+    def get_previous_version(self, at_date):
+        prev_version = None
+        for version in self.versions:
+            if version.start_date > at_date:
+                return prev_version
+            prev_version = version
+        return prev_version
+
+    def append_version(self, version):
+        rank = 0
+        prev_version = self.get_previous_version(version.start_date)
+        if prev_version:
+            prev_version.end_date = version.start_date - 1
+            rank = self.versions.index(prev_version) + 1
+        self.versions.insert(rank, version)
+        return self
+
+    def get_current_value(self, name):
+        vers = utils.get_good_version_at_date(self, 'versions', utils.today())
+        if vers:
+            return vers.rec_name
+        return ''
+
+    def get_rec_name(self, name):
+        res = ''
+        if self.code:
+            res = self.code
+        elif self.name:
+            res = self.name
+        res += ' (%s)' % self.get_current_value(name)
+        return res
 
 
 class TaxVersion(CoopSQL, CoopView):
@@ -44,23 +76,14 @@ class TaxVersion(CoopSQL, CoopView):
         [('flat', 'Flat'), ('rate', 'Rate')],
         'Rating mode',
         required=True)
-    flat_value = fields.Numeric(
-        'Flat rate',
-        states={'invisible': (Eval('kind') != 'flat')})
-    rate_value = fields.Numeric(
-        'Appliable Rate',
-        states={'invisible': (Eval('kind') != 'rate')})
+    value = fields.Numeric('Value')
 
     @staticmethod
     def default_kind():
         return 'rate'
 
     @staticmethod
-    def default_flat_value():
-        return 0
-
-    @staticmethod
-    def default_rate_value():
+    def default_value():
         return 0
 
     @staticmethod
@@ -73,10 +96,16 @@ class TaxVersion(CoopSQL, CoopView):
 
     def apply_tax(self, base):
         if self.kind == 'rate':
-            return base * self.rate_value / 100
+            return base * self.value / 100
         elif self.kind == 'flat':
-            return self.flat_value
+            return self.value
         return 0
+
+    def get_rec_name(self, name):
+        res = '%.2f' % self.value
+        if self.kind == 'rate':
+            res += ' %'
+        return res
 
 
 class TaxManager(CoopSQL, CoopView):
@@ -98,7 +127,7 @@ class TaxManager(CoopSQL, CoopView):
 
     def get_rec_name(self, name):
         taxes = self.give_appliable_taxes({'date': utils.today()})
-        return '; '.join([tax.my_tax_desc.name for tax in taxes])
+        return '; '.join([tax.my_tax_desc.rec_name for tax in taxes])
 
 
 class ManagerTaxRelation(CoopSQL):
