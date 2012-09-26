ffd006dd81d78a5733e28357d371fda98320a41f
Jean CAVALLO <jean.cavallo@coopengo.com>
2012-09-26 15:31:29 +0200

    Moved One2ManyDomain in model.py

--- coop_utils/__init__.py
+++ coop_utils/__init__.py
@@ -2,7 +2,6 @@ from trytond.pool import Pool
 from .utils import *
 from .model import *
 from .many2one_form import *
-from .one2many_domain import *
 from .business import *
 from .session import *
 
