75473802cbaddd33068239c0e9625e6222ac4e3f
Romain Seon <romain.seon@coopengo.com>
2012-10-02 10:20:14 +0200

    When updating all modules, update also tryton modules

--- scripts/updatedatabase.sh
+++ scripts/updatedatabase.sh
@@ -5,57 +5,7 @@ fi
 if test -z $1 ; then
     echo "Update all modules"
     echo ""
-    echo "Update module coop_utils"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u coop_utils
-    echo ""
-    echo "Update module coop_party"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u coop_party
-    echo ""
-    echo "Update module coop_party_fr"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u coop_party_fr
-    echo ""
-    echo "Update module insurance_party"
-    echo ""
-    $TRYTOND_PATH/tr    ytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_party
-    echo ""
-    echo "Update module insurance_party_fr"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_party_fr
-    echo ""
-    echo "Update module party_bank"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u party_bank
-    echo ""
-    echo "Update module insurance_product"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_product
-    echo ""
-    echo "Update module insurance_process"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_process
-    echo ""
-    echo "Update module insurance_contract"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_contract
-    echo ""
-    echo "Update module insurance_collective"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u insurance_collective
-    echo ""
-    echo "Update module loan product"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u loan_product
-    echo ""
-    echo "Update module P&C product"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u property_product
-    echo ""
-    echo "Update module loan product"
-    echo ""
-    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u health_product
+    $TRYTOND_PATH/trytond -d $DATABASE_NAME -c $TRYTOND_CONF -u all
 else
     echo ""
     echo "Update module $1"
