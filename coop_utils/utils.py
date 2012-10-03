1658507fb8f7fe34b938fa2e63340e440bc438ac
Romain Seon <romain.seon@coopengo.com>
2012-10-03 18:07:03 +0200

    Translate model_name when presenting descendents (warning bug with accentued chars)

--- coop_utils/utils.py
+++ coop_utils/utils.py
@@ -40,18 +40,6 @@ def get_descendents(from_class, names_only=False):
     return res
 
 
-def get_descendents_name(from_class):
-    result = []
-    for model_name, model in Pool().iterobject():
-        if issubclass(model, from_class):
-            if model.__doc__:
-                result.append((model_name, model.__doc__.splitlines()[0]))
-            else:
-                raise Exception(
-                    'Model %s does not have a docstring !' % model_name)
-    return result
-
-
 def get_module_name(cls):
     return cls.__name__.split('.')[0]
 
