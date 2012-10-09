03a13e5cfa33ae67b989d1d1fbe6ac37d2ea22e9
Jean CAVALLO <jean.cavallo@coopengo.com>
2012-10-09 17:15:02 +0200

    Added multiprocess toolset (beta) for proteus test cases

--- life_contract/test_case/proteus_test_case.py
+++ life_contract/test_case/proteus_test_case.py
@@ -4,28 +4,36 @@
 import datetime
 
 from proteus import Model, Wizard
+from proteus_tools import multiprocess_this
 
 
-def launch_test_case(cfg_dict):
-    Party = Model.get('party.person')
-    Product = Model.get('ins_product.product')
-    on_party, = Party.find(limit=1)
-    on_product, = Product.find([('code', '=', 'AAA')], limit=1)
+def create_contract(on_party, on_product, date_delta):
+    print '%s, %s, %s' % (on_party.party.name, on_product.name,
+        date_delta)
     wizard = Wizard('ins_contract.subs_process')
     wizard._config._context['from_session'] = wizard.session_id
     wizard._config.context['from_session'] = wizard.session_id
-    wizard.form.start_date += datetime.timedelta(days=2)
+    wizard.form.start_date += datetime.timedelta(days=date_delta)
     wizard.form.subscriber = on_party.party
     wizard.form.product = on_product
     wizard.execute('steps_next')
-    wizard.form.options[0].start_date += \
-        datetime.timedelta(days=-4)
-    wizard.form.options[0].start_date += \
-        datetime.timedelta(days=5)
-    wizard.form.options[1].start_date += \
-        datetime.timedelta(days=-1)
-    wizard.form.options[1].start_date += \
-        datetime.timedelta(days=1)
     wizard.execute('steps_next')
     wizard.execute('steps_next')
     wizard.execute('steps_complete')
+
+
+def launch_test_case(cfg_dict):
+    the_date = datetime.date.today()
+    Party = Model.get('party.person')
+    Product = Model.get('ins_product.product')
+    on_product, = Product.find([('code', '=', 'AAA')], limit=1)
+    on_parties = Party.find()
+    the_args = []
+    for party in on_parties:
+        the_args.append([
+            party,
+            on_product,
+            len(the_args)])
+
+    multiprocess_this(create_contract, the_args, cfg_dict,
+        datetime.datetime.now() + datetime.timedelta(days=20))
---
+++ test_case/proteus_tools.py
@@ -0,0 +1,52 @@
+import multiprocessing
+
+from proteus import config as pconfig
+
+
+def get_config(cfg_dict, connexion_date):
+    config = pconfig.set_trytond(
+        database_name=cfg_dict['database_name'],
+        user=cfg_dict['user'],
+        database_type=cfg_dict['db_type'],
+        language=cfg_dict['language'],
+        password=cfg_dict['password'],
+        config_file=cfg_dict['config_file'],
+    )
+    config.set_context({'client_defined_date': connexion_date})
+    return config
+
+
+def multi_launch(the_fun, the_args, connexion_date):
+    while the_args:
+        the_fun(*(the_args.pop()))
+
+
+def npop(the_list, n):
+    res = []
+    for i in range(n):
+        if len(the_list) > 0:
+            res.append(the_list.pop())
+        else:
+            break
+    return res
+
+
+def multiprocess_this(fun, the_args, cfg_dict, connexion_date):
+#    num_processes = multiprocessing.cpu_count()
+    num_processes = 1
+
+    grouping = int((len(the_args) - 1) / num_processes) + 1
+
+    print 'Num processes : %s' % num_processes
+    print 'Grouping : %s' % grouping
+    print 'Number of args : %s\n' % len(the_args)
+
+    get_config(cfg_dict, connexion_date)
+
+    for n in range(num_processes):
+        p = multiprocessing.Process(target=multi_launch, args=(
+            fun,
+            npop(the_args, grouping),
+            connexion_date))
+        p.start()
+        print 'Number of args remaining : %s' % len(the_args)
