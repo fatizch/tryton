3135476a2ccfa56c0662ad1edd3be049e01e8678
Jean CAVALLO <jean.cavallo@coopengo.com>
2012-10-18 13:53:10 +0200

    Removed low-level debug info as default behaviour for client launch

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -11,7 +11,8 @@ echo $SEP
 $TRYTOND_PATH/trytond -c $TRYTOND_CONF &
 echo "Starting tryton client"
 echo $SEP
-env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF -l DEBUG &
+env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF &
+# env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF -l DEBUG &
 sleep 2
 echo $SEP
 echo "Client ready"
