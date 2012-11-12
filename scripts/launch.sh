f038ff9664531460e71d86b3eecf122a9269e2bb
Romain Seon <romain.seon@coopengo.com>
2012-11-12 11:02:41 +0100

    First migration of xml from database to file : module coop_country

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -11,7 +11,7 @@ echo $SEP
 $TRYTOND_PATH/trytond -c $TRYTOND_CONF &
 echo "Starting tryton client"
 echo $SEP
-env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF &
+env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF -d &
 # env UBUNTU_MENUPROXY= $TRYTON_PATH/tryton -c $TRYTON_CONF -l DEBUG &
 sleep 2
 echo $SEP
