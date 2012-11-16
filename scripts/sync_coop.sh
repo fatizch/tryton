876784b152fdada41276ddf0217b2a76ff0eacf7
Romain Seon <romain.seon@coopengo.com>
2012-11-16 15:05:06 +0100

    update sync script

--- scripts/sync_coop.sh
+++ scripts/sync_coop.sh
@@ -5,5 +5,7 @@ cd $ENV_FOLDER/tryton-workspace/tryton
 hg pull -u
 cd $ENV_FOLDER/tryton-workspace/trytond
 hg pull -u
+cd $ENV_FOLDER/tryton-workspace/proteus
+hg pull -u
 cd $ENV_FOLDER/tryton-workspace/coopbusiness/scripts
 ./configure_directory $ENV_FOLDER
