688ad20c62ada0163d8f7b8f01cd01fbcc9af09f
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-01-22 17:01:01 +0100

    Coop_process module should not save process classes

--- coop_process/__init__.py
+++ coop_process/__init__.py
@@ -5,8 +5,8 @@ from .coop_process import *
 def register():
     Pool.register(
         StepTransition,
-        module='process', type_='model')
+        module='coop_process', type_='model')
 
     Pool.register(
         GenerateGraph,
-        module='process', type_='report')
+        module='coop_process', type_='report')
