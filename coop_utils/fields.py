c695a6170df730bee7f5b6511e028f7c3b382248
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-03-19 18:06:05 +0100

    Do not take into account Pyson domains on One2ManyDomain getter

--- coop_utils/fields.py
+++ coop_utils/fields.py
@@ -1,6 +1,7 @@
 from itertools import chain
 
 from trytond.model import fields as tryton_fields
+from trytond.pyson import PYSON
 from trytond.pool import Pool
 from trytond.transaction import Transaction
 
@@ -81,7 +82,37 @@ class One2ManyDomain(One2Many):
                 clause = [(self.field, 'in', references)]
             else:
                 clause = [(self.field, 'in', sub_ids)]
-            clause.append(self.domain)
+
+            def clean_domain(the_domain):
+                if the_domain[0] in ('OR', 'AND'):
+                    final_domain = the_domain[0]
+                    for elem in the_domain[1:]:
+                        good_domain = clean_domain(elem)
+                        if not good_domain:
+                            final_domain.append(True)
+                        else:
+                            final_domain.append(good_domain)
+                elif isinstance(the_domain[0], (tuple, list)):
+                    final_domain = []
+                    for elem in the_domain:
+                        good_domain = clean_domain(elem)
+                        if not good_domain:
+                            final_domain.append(True)
+                        else:
+                            final_domain.append(good_domain)
+                else:
+                    has_pyson = False
+                    for value in the_domain:
+                        if isinstance(value, PYSON):
+                            has_pyson = True
+                            break
+                    if not has_pyson:
+                        final_domain = the_domain
+                    else:
+                        final_domain = None
+                return final_domain
+
+            clause.append(clean_domain(self.domain))
             targets.append(Relation.search(clause, order=self.order))
         targets = list(chain(*targets))
 
