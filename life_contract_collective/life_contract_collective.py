034276521e68223eae2fbffee57e1bb5cca6d0bd
Romain Seon <romain.seon@coopengo.com>
2013-03-27 20:49:13 +0100

    remove deprecated states

--- life_contract_collective/life_contract_collective.py
+++ life_contract_collective/life_contract_collective.py
@@ -12,11 +12,3 @@ class GroupCoveredElement():
 
     __name__ = 'ins_contract.covered_element'
     __metaclass__ = PoolMeta
-
-    @classmethod
-    def __setup__(cls):
-        super(GroupCoveredElement, cls).__setup__()
-        utils.update_states(cls, 'name',
-            {'invisible': Eval('_parent_contract', {}).get('kind') != 'group'})
-        utils.update_states(cls, 'person',
-            {'invisible': Eval('_parent_contract', {}).get('kind') == 'group'})
