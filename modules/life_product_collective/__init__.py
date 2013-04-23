35903d4a022a9878cf8e4308946a39d8fbc8f6f7
Romain Seon <romain.seon@coopengo.com>
2013-04-23 17:59:35 +0200

    Remove deprecated module life_product_collective

--- modules/life_product_collective/__init__.py
+++ modules/life_product_collective/__init__.py
@@ -1,11 +1,10 @@
-from trytond.pool import Pool
-from .gbp_creation import *
 
 
 def register():
-    Pool.register(
-        module='life_product_collective', type_='model')
+    # Pool.register(
+    #     module='life_product_collective', type_='model')
 
-    Pool.register(
-        # GBPWizard,
-        module='life_product_collective', type_='wizard')
+    # Pool.register(
+    #     # GBPWizard,
+    #     module='life_product_collective', type_='wizard')
+    pass
