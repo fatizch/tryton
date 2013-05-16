20694a910ed06dfa4a1e81e50704826a88d3eb7a
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-05-16 10:21:14 +0200

    Backout sur la révision : 52d27a98ef4f

--- modules/life_product/test_case/proteus_test_case.py
+++ modules/life_product/test_case/proteus_test_case.py
@@ -291,7 +291,6 @@ def create_AAA_Product(cfg_dict, code, name):
 
     product_a.coverages.append(coverage_a)
     product_a.coverages.append(coverage_b)
-    product_a.item_descriptors.append(item_desc)
 
     product_a.contract_generator = get_or_create_generator(
         cfg_dict, 'ins_product.product')
