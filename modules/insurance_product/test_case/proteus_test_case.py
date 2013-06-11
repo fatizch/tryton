556f3002c0c981b87783614c971f3a7cf9c0777c
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-11 11:40:47 +0200

    Remove now useless migration test_case

--- modules/insurance_product/test_case/proteus_test_case.py
+++ modules/insurance_product/test_case/proteus_test_case.py
@@ -7,29 +7,9 @@ import proteus_tools
 
 
 def update_models(cfg_dict):
-    cfg_dict['EligibilityRule'] = Model.get('ins_product.eligibility_rule')
-
-
-def migrate_eligibility_rules(cfg_dict):
-    for rule in cfg_dict['EligibilityRule'].find(
-        [
-            'OR',
-            [('subscriber_classes', '=', 'party.person')],
-            [('subscriber_classes', '=', 'party.party')],
-            [('subscriber_classes', '=', 'party.company')],
-        ]):
-        if rule.subscriber_classes == 'party.person':
-            rule.subscriber_classes = 'person'
-        elif rule.subscriber_classes == 'party.society':
-            rule.subscriber_classes = 'company'
-        elif rule.subscriber_classes == 'society':
-            rule.subscriber_classes = 'company'
-        elif rule.subscriber_classes == 'party.party':
-            rule.subscriber_classes = 'all'
-        rule.save()
+    pass
 
 
 def launch_test_case(cfg_dict):
     update_models(cfg_dict)
     proteus_tools.set_global_search('ins_product.product')
-    migrate_eligibility_rules(cfg_dict)
