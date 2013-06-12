dd44ec1b7b71e09cf01adcd2219fa01432b601ee
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-12 13:20:37 +0200

    Fixed a bug when creating the rule combination context

--- modules/insurance_product/test_case/proteus_test_case.py
+++ modules/insurance_product/test_case/proteus_test_case.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from proteus import Model
+
 from trytond.modules.rule_engine.test_case import create_or_update_folder
 from trytond.modules.rule_engine.test_case import append_folder_to_context
 from trytond.modules.rule_engine.test_case import get_or_create_context
 
 
+def update_cfg(cfg_dict):
+    cfg_dict['Context'] = Model.get('rule_engine.context')
+    cfg_dict['TreeElement'] = Model.get('rule_engine.tree_element')
+
+
 def launch_test_case(cfg_dict):
+    update_cfg(cfg_dict)
     default_context = get_or_create_context(cfg_dict, 'Default Context')
     person_folder = create_or_update_folder(cfg_dict,
         'ins_product.rule_sets.person')
@@ -19,11 +27,13 @@ def launch_test_case(cfg_dict):
     option_folder = create_or_update_folder(cfg_dict,
         'ins_product.rule_sets.option')
     append_folder_to_context(default_context, option_folder)
-    rule_combination_folder = create_or_update_folder(cfg_dict,
-        'ins_product.rule_sets.rule_combination')
-    append_folder_to_context(default_context, rule_combination_folder)
     covered_data_folder = create_or_update_folder(cfg_dict,
         'ins_product.rule_sets.covered_data')
     append_folder_to_context(default_context, covered_data_folder)
+    rule_combination_folder = create_or_update_folder(cfg_dict,
+        'ins_product.rule_sets.rule_combination')
+    append_folder_to_context(default_context, rule_combination_folder)
     rule_combi_context = get_or_create_context(cfg_dict, 'Rule Combination')
+    rule_combination_folder = create_or_update_folder(cfg_dict,
+        'ins_product.rule_sets.rule_combination')
     append_folder_to_context(rule_combi_context, rule_combination_folder)
