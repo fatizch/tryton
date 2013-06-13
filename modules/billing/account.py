9847ba11918dd251658060a210827ccfb7164a5b
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-13 10:22:26 +0200

    Fixed a bug in second_origin definition

--- modules/billing/account.py
+++ modules/billing/account.py
@@ -19,19 +19,20 @@ class Move:
 class MoveLine:
     __name__ = 'account.move.line'
 
-    second_origin = fields.Reference('Second Origin', selection='get_origin')
+    second_origin = fields.Reference('Second Origin',
+        selection='get_second_origin')
 
     @classmethod
-    def _get_origin(cls):
+    def _get_second_origin(cls):
         return [
             'offered.product',
             'offered.coverage',
             ]
 
     @classmethod
-    def get_origin(cls):
+    def get_second_origin(cls):
         Model = Pool().get('ir.model')
-        models = cls._get_origin()
+        models = cls._get_second_origin()
         models = Model.search([
                 ('model', 'in', models),
                 ])
