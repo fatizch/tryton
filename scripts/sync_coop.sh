f17c27086215959ae3057778996ee3f55f990638
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-18 18:46:57 +0200

    Do not ask for confirmation when syncing

--- scripts/sync_coop.sh
+++ scripts/sync_coop.sh
@@ -2,18 +2,14 @@
 SEP="-----------------------------------------------------------------"
 . ./print_status.sh
 echo $SEP
-read -p "Are you sure you want to sync (y/n) ? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    cd $ENV_FOLDER/tryton-workspace/coopbusiness
-    hg pull -u
-    cd $ENV_FOLDER/tryton-workspace/tryton
-    hg pull -u
-    cd $ENV_FOLDER/tryton-workspace/trytond
-    hg pull -u
-    cd $ENV_FOLDER/tryton-workspace/proteus
-    hg pull -u
-    cd $ENV_FOLDER/tryton-workspace/trytond/trytond/modules
-    cd $ENV_FOLDER
-    ./tryton-workspace/coopbusiness/scripts/configure_directory.sh $ENV_FOLDER
-fi
+cd $ENV_FOLDER/tryton-workspace/coopbusiness
+hg pull -u
+cd $ENV_FOLDER/tryton-workspace/tryton
+hg pull -u
+cd $ENV_FOLDER/tryton-workspace/trytond
+hg pull -u
+cd $ENV_FOLDER/tryton-workspace/proteus
+hg pull -u
+cd $ENV_FOLDER/tryton-workspace/trytond/trytond/modules
+cd $ENV_FOLDER
+./tryton-workspace/coopbusiness/scripts/configure_directory.sh $ENV_FOLDER
