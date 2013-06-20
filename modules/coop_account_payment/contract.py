520124e82ac5ae1178f13df646b7f0673d60b4c4
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-20 16:37:02 +0200

    Add paymentl lines on contract

--- modules/coop_account_payment/contract.py
+++ modules/coop_account_payment/contract.py
@@ -19,6 +19,13 @@ class Contract():
         'get_paid_today')
     due_today = fields.Function(fields.Numeric('Due today'),
         'get_due_today')
+    payment_lines = fields.Function(
+        fields.One2Many('account.payment', None, 'Payment Lines',
+            domain=[
+                ('line.move.origin', '=', (__name__, Eval('id', 0))),
+                ('state', 'in', ('approved', 'processing', 'succeeded'))],
+            on_change_with=['id'], loading='lazy'),
+        'on_change_with_payment_lines')
 
     @classmethod
     def __setup__(cls):
@@ -41,3 +48,7 @@ class Contract():
             ('maturity_date', '<=', Date.today())])
         result = sum(map(lambda x: x.payment_amount, lines))
         return result
+
+    def on_change_with_payment_lines(self, name=None):
+        return map(lambda x: x.id, sorted(utils.get_domain_instances(self,
+            'payment_lines'), key=lambda x: x.date, reverse=True))
