14233a80c9e74c8ce79eae28e8cd105d043f4272
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-06-25 15:49:29 +0200

    Add prorata calculus mode on payment_rule_lines

--- modules/billing/payment_rule.py
+++ modules/billing/payment_rule.py
@@ -1,10 +1,10 @@
-import copy
 import datetime
+from decimal import Decimal
+from dateutil.relativedelta import relativedelta
 
 from trytond.pool import Pool
 from trytond.pyson import Eval
 from trytond.modules.coop_utils import fields, model, date
-from trytond.modules.account_invoice import PaymentTermLine
 from trytond.modules.insurance_product.business_rule.pricing_rule import \
     PRICING_FREQUENCY
 
@@ -19,29 +19,216 @@ REMAINING_POSITION = [
     ('', ''),
     ('first_calc', 'First calculated'),
     ('last_calc', 'Last calculated'),
+    ('custom', 'Custom Lines'),
     ]
 
 
-# Temp : inherit from PaymentTermLine rather than copy-pasting it
-class PaymentRuleLine(PaymentTermLine, model.CoopSQL, model.CoopView):
+class PaymentRuleLine(model.CoopSQL, model.CoopView):
     'Payment Rule Line'
 
     __name__ = 'billing.payment_rule_line'
 
+    sequence = fields.Integer('Sequence',
+        order_field='(%(table)s.sequence IS NULL) %(order)s, '
+        '%(table)s.sequence %(order)s',
+        help='Use to order lines in ascending order')
+    payment_rule = fields.Many2One('billing.payment_rule', 'Payment Term',
+        required=True, ondelete="CASCADE")
+    type = fields.Selection([
+            ('fixed', 'Fixed'),
+            ('percent_on_total', 'Percentage on Total'),
+            ('prorata', 'Prorata'),
+            ], 'Type', required=True,
+        on_change=['type'])
+    percentage = fields.Numeric('Percentage', digits=(16, 8),
+        states={
+            'invisible': Eval('type', '') != 'percent_on_total',
+            'required': Eval('type', '') == 'percent_on_total'
+            }, on_change=['percentage'], depends=['type'])
+    divisor = fields.Numeric('Divisor', digits=(16, 8),
+        states={
+            'invisible': Eval('type', '') != 'percent_on_total',
+            'required': Eval('type', '') == 'percent_on_total',
+            }, on_change=['divisor'], depends=['type'])
+    amount = fields.Numeric('Amount', digits=(16, Eval('currency_digits', 2)),
+        states={
+            'invisible': Eval('type', '') != 'fixed',
+            'required': Eval('type', '') == 'fixed',
+            }, depends=['type', 'currency_digits'])
+    currency = fields.Many2One('currency.currency', 'Currency',
+        states={
+            'invisible': Eval('type', '') != 'fixed',
+            'required': Eval('type', '') == 'fixed',
+            }, depends=['type'])
+    currency_digits = fields.Function(fields.Integer('Currency Digits',
+            on_change_with=['currency']), 'on_change_with_currency_digits')
+    day = fields.Integer('Day of Month')
+    month = fields.Selection([
+            (None, ''),
+            ('1', 'January'),
+            ('2', 'February'),
+            ('3', 'March'),
+            ('4', 'April'),
+            ('5', 'May'),
+            ('6', 'June'),
+            ('7', 'July'),
+            ('8', 'August'),
+            ('9', 'September'),
+            ('10', 'October'),
+            ('11', 'November'),
+            ('12', 'December'),
+            ], 'Month', sort=False)
+    weekday = fields.Selection([
+            (None, ''),
+            ('0', 'Monday'),
+            ('1', 'Tuesday'),
+            ('2', 'Wednesday'),
+            ('3', 'Thursday'),
+            ('4', 'Friday'),
+            ('5', 'Saturday'),
+            ('6', 'Sunday'),
+            ], 'Day of Week', sort=False)
+    months = fields.Integer('Number of Months', required=True)
+    weeks = fields.Integer('Number of Weeks', required=True)
+    days = fields.Integer('Number of Days', required=True)
+    add_calculated_period = fields.Boolean('Add calculated period',
+        on_change=['add_calculated_period'])
+    number_of_periods = fields.Numeric('Number of periods', digits=(16, 2),
+        states={
+            'invisible': ~Eval('add_calculated_period'),
+            'required': ~~Eval('add_calculated_period')})
+    is_remainder = fields.Boolean('Is remainder',
+        states={'invisible': Eval('_parent_payment_rule', {}).get('kind', '')
+            != 'custom'})
+
     @classmethod
-    def __setup__(cls):
-        super(PaymentRuleLine, cls).__setup__()
-        cls.payment = copy.copy(cls.payment)
-        cls.payment.model = 'billing.payment_rule'
-        setattr(cls, 'payment', cls.payment)
+    def default_add_calculated_period(cls):
+        return False
+
+    @classmethod
+    def default_number_of_periods(cls):
+        return 1
+
+    @classmethod
+    def default_is_remainder(cls):
+        return False
+
+    @staticmethod
+    def default_currency_digits():
+        return 2
+
+    @staticmethod
+    def default_type():
+        return 'fixed'
+
+    @staticmethod
+    def default_months():
+        return 0
+
+    @staticmethod
+    def default_weeks():
+        return 0
+
+    @staticmethod
+    def default_days():
+        return 0
+
+    def on_change_add_calculated_period(self):
+        res = {}
+        if (hasattr(self, 'add_calculated_period') and
+                self.add_calculated_period):
+            res['number_of_periods'] = 1
+        return res
+
+    def on_change_type(self):
+        res = {}
+        if self.type != 'fixed':
+            res['amount'] = Decimal('0.0')
+            res['currency'] = None
+        if self.type != 'percent_on_total':
+            res['percentage'] = Decimal('0.0')
+            res['divisor'] = Decimal('0.0')
+        return res
+
+    def on_change_percentage(self):
+        if not self.percentage:
+            return {'divisor': 0.0}
+        return {
+            'divisor': self.round(Decimal('100.0') / self.percentage,
+                self.__class__.divisor.digits[1]),
+            }
+
+    def on_change_divisor(self):
+        if not self.divisor:
+            return {'percentage': 0.0}
+        return {
+            'percentage': self.round(Decimal('100.0') / self.divisor,
+                self.__class__.percentage.digits[1]),
+            }
+
+    def on_change_with_currency_digits(self, name=None):
+        if self.currency:
+            return self.currency.digits
+        return 2
+
+    def get_delta(self):
+        return {
+            'day': self.day,
+            'month': int(self.month) if self.month else None,
+            'days': self.days,
+            'weeks': self.weeks,
+            'months': self.months,
+            'weekday': int(self.weekday) if self.weekday else None,
+            }
+
+    def get_date(self, date):
+        return date + relativedelta(**self.get_delta())
+
+    def get_flat_value(self, start_date, end_date, amount, currency):
+        Currency = Pool().get('currency.currency')
+        if self.type == 'fixed':
+            return Currency.compute(self.currency, self.amount, currency)
+        elif self.type == 'percent_on_total':
+            return currency.round(amount * self.percentage / Decimal('100'))
+        elif self.type == 'prorata':
+            payment_rule = self.payment_rule
+            my_start_date = self.get_date(start_date)
+            if payment_rule.with_sync_date:
+                temp_date = date.add_frequency(payment_rule.base_frequency,
+                    my_start_date)
+                if payment_rule.base_frequency == 'yearly':
+                    final_date = datetime.date(temp_date.year,
+                        payment_rule.sync_date.month,
+                        payment_rule.sync_date.day)
+                else:
+                    final_date = datetime.date(temp_date.year, temp_date.month,
+                        payment_rule.sync_date.day)
+                if final_date <= end_date:
+                    my_end_date = final_date
+                else:
+                    my_end_date = end_date
+            else:
+                my_end_date = date.add_frequency(payment_rule.base_frequency,
+                    my_start_date)
+            my_end_date = date.add_day(my_end_date, -1)
+            period = date.number_of_days_between(my_start_date, my_end_date)
+            total_period = date.number_of_days_between(start_date, end_date)
+            return currency.round(amount * period / total_period)
+        return 0
+
+    @staticmethod
+    def round(number, digits):
+        quantize = Decimal(10) ** -Decimal(digits)
+        return Decimal(number).quantize(quantize)
 
     def get_line_info(self, start_date):
         return {
             'date': self.get_date(start_date),
-            'remaining': self.kind == 'remaining',
-            'freq_amount': 0,
+            'remaining': self.is_remainder,
+            'freq_amount': self.number_of_periods if self.add_calculated_period
+            else 0,
             'line': self,
-        }
+            }
 
 
 class PaymentRule(model.CoopSQL, model.CoopView):
@@ -59,9 +246,9 @@ class PaymentRule(model.CoopSQL, model.CoopView):
     force_line_at_start = fields.Boolean('Force start date line',
         states={'invisible': ~Eval('with_sync_date') or ~~Eval('start_lines')})
     sync_date = fields.Date('Sync Date', states={
-        'invisible': ~Eval('with_sync_date'),
-        'required': ~~Eval('with_sync_date')})
-    start_lines = fields.One2Many('billing.payment_rule_line', 'payment',
+            'invisible': ~Eval('with_sync_date'),
+            'required': ~~Eval('with_sync_date')})
+    start_lines = fields.One2Many('billing.payment_rule_line', 'payment_rule',
         'Start Lines')
 
     @classmethod
@@ -87,10 +274,10 @@ class PaymentRule(model.CoopSQL, model.CoopView):
         dates = [line.get_line_info(start_date) for line in self.start_lines]
         if not dates and self.with_sync_date and self.force_line_at_start:
             dates.append({
-                'date': start_date,
-                'remaining': self.remaining_position == 'first_calc',
-                'freq_amount': 1,
-                'line': None})
+                    'date': start_date,
+                    'remaining': self.remaining_position == 'first_calc',
+                    'freq_amount': 1,
+                    'line': None})
         last_date = dates[-1]['date'] if len(dates) else start_date
         if self.with_sync_date:
             temp_date = date.add_frequency(self.base_frequency, last_date)
@@ -102,10 +289,10 @@ class PaymentRule(model.CoopSQL, model.CoopView):
                     self.sync_date.day)
             if final_date <= end_date:
                 dates.append({
-                    'date': last_date,
-                    'remaining': False,
-                    'freq_amount': 1,
-                    'line': None})
+                        'date': final_date,
+                        'remaining': False,
+                        'freq_amount': 1,
+                        'line': None})
                 last_date = final_date
         else:
             last_date = date.add_day(last_date, 1)
@@ -115,10 +302,10 @@ class PaymentRule(model.CoopSQL, model.CoopView):
                 last_date)
             if last_date <= end_date:
                 dates.append({
-                    'date': last_date,
-                    'remaining': False,
-                    'freq_amount': 1,
-                    'line': None})
+                        'date': last_date,
+                        'remaining': False,
+                        'freq_amount': 1,
+                        'line': None})
 
         if self.remaining_position == 'last_calc':
             dates[-1]['remaining'] = True
@@ -131,7 +318,8 @@ class PaymentRule(model.CoopSQL, model.CoopView):
         remainder = amount
         flat_total = 0
         for line in (l for l in lines_dates if l['line']):
-            line_amount = line['line'].get_value(0, 0, currency)
+            line_amount = line['line'].get_flat_value(start_date, end_date,
+                amount, currency)
             flat_total += line_amount
             res[line['date']] += line_amount
             remainder -= line_amount
@@ -145,4 +333,5 @@ class PaymentRule(model.CoopSQL, model.CoopView):
             remaining_line = next(l for l in lines_dates if l['remaining'])
             res[remaining_line['date']] += currency.round(remainder)
 
-        return sorted(list(res.iteritems()), key=lambda x: x[0])
+        return sorted(list((x for x in res.iteritems() if x[1])),
+            key=lambda x: x[0])
