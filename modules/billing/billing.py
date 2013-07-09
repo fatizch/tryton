37256aff696b54928656bc7287baede41c845bf4
Romain Seon <romain.seon@coopengo.com>
2013-07-09 09:54:26 +0200

    Fix wrong import for module date

--- modules/billing/billing.py
+++ modules/billing/billing.py
@@ -708,7 +708,7 @@ class Contract():
         new_period_end = coop_date.add_frequency(
             self.get_product_frequency(last_date), last_date)
         if self.next_renewal_date:
-            new_period_end = min(new_period_end, date.add_day(
+            new_period_end = min(new_period_end, coop_date.add_day(
                 self.next_renewal_date, -1))
         if self.end_date and new_period_end > self.end_date:
             return (new_period_start, self.end_date)
