7f369d3f89f5140e57d0344cc853ac5043726ff2
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-07-18 16:30:26 +0200

    Billing manager must heck contract's status rather than its own

--- modules/billing/billing.py
+++ modules/billing/billing.py
@@ -389,7 +389,7 @@ class BillingManager(model.CoopSQL, model.CoopView):
         })
 
     def check_payment_bank_acount(self):
-        if self.status == 'quote':
+        if self.contract.status == 'quote':
             return
         if self.payment_mode == 'direct_debit':
             if not self.payment_bank_account:
