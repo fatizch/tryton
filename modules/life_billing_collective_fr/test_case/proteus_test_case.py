e16bbb7ddcdfca13f2d3a394b0ff0dcdba0c3949
Romain Seon <romain.seon@coopengo.com>
2013-09-18 14:36:09 +0200

    Fix billing collective migration script

--- modules/life_billing_collective_fr/test_case/proteus_test_case.py
+++ modules/life_billing_collective_fr/test_case/proteus_test_case.py
@@ -8,7 +8,7 @@ def update_cfg(cfg_dict):
 def migrate_rate_line(cfg_dict):
     Contract = cfg_dict['Contract']
     Contract.button_calculate_rates(Contract.find(
-            [('rates.start_date', '=', None)]))
+            [('rates.start_date', '=', None)]), {})
 
 
 def launch_test_case(cfg_dict):
