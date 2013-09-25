59862e5c96fb7450ff49bcd9a10bece850712a59
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-09-25 10:17:58 +0200

    Update test case to create default suspense account

--- modules/life_billing_collective_fr/test_case/proteus_test_case.py
+++ modules/life_billing_collective_fr/test_case/proteus_test_case.py
@@ -1,8 +1,22 @@
 from proteus import Model
+import proteus_tools
 
 
 def update_cfg(cfg_dict):
     cfg_dict['Contract'] = Model.get('contract.contract')
+    cfg_dict['Account'] = Model.get('account.account')
+    cfg_dict['AccountType'] = Model.get('account.account.type')
+    cfg_dict['AccountConfiguration'] = Model.get('account.configuration')
+    cfg_dict['Company'] = Model.get('company.company')
+
+
+def create_methods(cfg_dict):
+    res = {}
+    res['Account'] = proteus_tools.generate_creation_method(
+        cfg_dict, 'Account', sel_keys=['name', 'company'])
+    res['AccountType'] = proteus_tools.generate_creation_method(
+        cfg_dict, 'AccountType', sel_keys=['name', 'company'])
+    return res
 
 
 def migrate_rate_line(cfg_dict):
@@ -12,5 +26,21 @@ def migrate_rate_line(cfg_dict):
 
 
 def launch_test_case(cfg_dict):
+    meths = create_methods(cfg_dict)
     update_cfg(cfg_dict)
     migrate_rate_line(cfg_dict)
+    company, = cfg_dict['Company'].find([('party.name', '=', 'Coop')])
+    account_config = cfg_dict['AccountConfiguration'].find([])[0]
+    if not account_config.default_suspense_account:
+        default_suspense_kind = meths['AccountType'](
+            {'name': 'Suspense', 'company': company},
+        )
+        default_account = meths['Account'](
+            {
+                'name': 'Default Suspense Account',
+                'kind': 'other',
+                'type': default_suspense_kind,
+            },
+            {'company': company.id})
+        account_config.default_suspense_account = default_account
+        account_config.save()
