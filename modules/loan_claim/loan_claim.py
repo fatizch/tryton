67278356696e5c3eb8ca6f3e877d853525a73ec2
Romain Seon <romain.seon@coopengo.com>
2013-10-14 10:24:25 +0200

    Do not display loan on delivered service if not loan

--- modules/loan_claim/loan_claim.py
+++ modules/loan_claim/loan_claim.py
@@ -15,20 +15,24 @@ class LoanClaimDeliveredService():
     __name__ = 'ins_contract.delivered_service'
     __metaclass__ = PoolMeta
 
+    is_loan = fields.Function(
+        fields.Boolean('Is loan', states={'invisible': True}),
+        'get_is_loan')
     loan = fields.Many2One('ins_contract.loan', 'Loan',
         domain=[('contract', '=', Eval('contract'))],
-        depends=['contract'])
+        states={'invisible': ~Eval('is_loan')},
+        depends=['contract', 'is_loan'])
 
     def get_loan(self):
         return self.loan
 
-    def is_loan(self):
+    def get_is_loan(self, name):
         return self.subscribed_service.is_loan
 
     def init_dict_for_rule_engine(self, cur_dict):
         super(LoanClaimDeliveredService, self).init_dict_for_rule_engine(
             cur_dict)
-        if not self.is_loan():
+        if not self.is_loan:
             return
         cur_dict['loan'] = self.loan
         cur_dict['share'] = self.loan.get_loan_share(self.loss.covered_person)
@@ -43,6 +47,6 @@ class LoanIndemnification():
     def init_from_delivered_service(self, delivered_service):
         super(LoanIndemnification, self).init_from_delivered_service(
             delivered_service)
-        if not delivered_service.is_loan():
+        if not delivered_service.is_loan:
             return
         self.beneficiary = delivered_service.loan.lender.party
