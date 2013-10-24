8bc9c93c7b6d778695d5ff5991ab5a909b4b9444
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-10-24 16:33:20 +0200

    Fix issue #111 : make bank.account exportable, add searcher on field number

--- modules/coop_bank/bank.py
+++ modules/coop_bank/bank.py
@@ -2,6 +2,7 @@ from ibanlib import iban
 
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
+from trytond.model import fields as tryton_fields
 
 from trytond.modules.coop_utils import utils, fields, export
 from trytond.modules.coop_utils import coop_string
@@ -67,12 +68,27 @@ class BankAccount(export.ExportImportMixin):
 
     start_date = fields.Date('Start Date')
     end_date = fields.Date('End Date')
-    numbers_as_char = fields.Function(
-        fields.Char('Numbers'),
-        'get_numbers_as_char')
     number = fields.Function(
         fields.Char('Main Account Number'),
-        'get_main_bank_account_number')
+        'get_main_bank_account_number',
+        searcher='search_main_bank_account_number')
+
+    @classmethod
+    def _export_keys(cls):
+        return set(['number'])
+
+    @classmethod
+    def search_main_bank_account_number(cls, name, clause):
+        pool = Pool()
+        account = pool.get('bank.account').__table__()
+        number = pool.get('bank.account.number').__table__()
+        _, operator, value = clause
+        Operator = tryton_fields.SQL_OPERATORS[operator]
+        query_table = account.join(number, condition=(
+                account.id == number.account))
+        query = query_table.select(account.id, where=Operator(
+                number.number, getattr(cls, name).sql_format(value)))
+        return [('id', 'in', query)]
 
     @staticmethod
     def default_currency():
@@ -112,7 +128,7 @@ class BankAccount(export.ExportImportMixin):
         return ['numbers', ('bank', 'light'), ('currency', 'light')]
 
     def get_main_bank_account_number(self, name):
-        ibans = [x.number for x in self.numbers if x.kind == 'iban']
+        ibans = [x for x in self.numbers if x.type == 'iban']
         if ibans:
             return ibans[-1].number
         elif self.numbers:
