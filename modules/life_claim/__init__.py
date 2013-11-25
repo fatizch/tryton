eed43527340f6713498dc8217308ace454b7f704
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-11-25 17:59:35 +0100

    Move life claim test cases in life_claim module

--- modules/life_claim/__init__.py
+++ modules/life_claim/__init__.py
@@ -1,13 +1,18 @@
 from trytond.pool import Pool
 from .life_claim import *
 from .life_contract import *
+from .test_case import *
 
 
 def register():
     Pool.register(
+        # from life_contract
         LifeOption,
+        LifeClaimDeliveredService,
+        # from life_claim
         LifeClaim,
         LifeLoss,
-        LifeClaimDeliveredService,
         LifeIndemnification,
+        # from test_module
+        TestCaseModel,
         module='life_claim', type_='model')
