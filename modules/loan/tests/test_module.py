67ae43609e47844d367643f3b039ab340f9f3a87
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-11-26 16:48:45 +0100

    Update loan unittests

--- modules/loan/tests/test_module.py
+++ modules/loan/tests/test_module.py
@@ -1,55 +1,34 @@
 #-*- coding:utf-8 -*-
-import sys
-import os
 from datetime import date
 from decimal import Decimal
-
 import unittest
+
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
 
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
-MODULE_NAME = os.path.basename(os.path.abspath(
-        os.path.join(os.path.normpath(__file__), '..', '..')))
+from trytond.modules.coop_utils import test_framework
 
 
-class ModuleTestCase(unittest.TestCase):
+class ModuleTestCase(test_framework.CoopTestCase):
     '''
     Test Coop module.
     '''
+    @classmethod
+    def get_module_name(cls):
+        return 'loan'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(MODULE_NAME)
-        self.Loan = POOL.get('loan.loan')
-        self.Currency = POOL.get('currency.currency')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view(MODULE_NAME)
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    @classmethod
+    def get_models(cls):
+        return {
+            'Loan': 'loan.loan',
+            'Currency': 'currency.currency',
+        }
 
     def test0010loan_basic_data(self):
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT) as transaction:
-            currency, = self.Currency.create([{
-                        'name': 'Euro',
-                        'code': 'EUR',
-                        'symbol': u'€',
-            }])
-            transaction.cursor.commit()
+        currency, = self.Currency.create([{
+                    'name': 'Euro',
+                    'code': 'EUR',
+                    'symbol': u'€',
+        }])
 
     def assert_payment(self, loan, at_date, number, begin_balance, amount,
             principal, interest, end_balance):
@@ -64,56 +43,56 @@ class ModuleTestCase(unittest.TestCase):
         self.assert_(loan.currency.is_zero(
                 payment.end_balance - end_balance))
 
+    @test_framework.prepare_test('loan.test0010loan_basic_data')
     def test0020loan_creation(self):
         '''
         Test basic loan
         '''
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            currency, = self.Currency.search([], limit=1)
-            loan = self.Loan()
-            loan.kind = 'fixed_rate'
-            loan.rate = Decimal(0.04)
-            loan.funds_release_date = date(2012, 7, 1)
-            loan.first_payment_date = date(2012, 7, 15)
-            loan.payment_frequency = 'month'
-            loan.amount = 100000
-            loan.number_of_payments = 180
-            loan.currency = currency
-            loan.payment_amount = loan.on_change_with_payment_amount()
-            self.assert_(
-                currency.is_zero(loan.payment_amount - Decimal(739.69)))
+        currency, = self.Currency.search([], limit=1)
+        loan = self.Loan()
+        loan.kind = 'fixed_rate'
+        loan.rate = Decimal(0.04)
+        loan.funds_release_date = date(2012, 7, 1)
+        loan.first_payment_date = date(2012, 7, 15)
+        loan.payment_frequency = 'month'
+        loan.amount = 100000
+        loan.number_of_payments = 180
+        loan.currency = currency
+        loan.payment_amount = loan.on_change_with_payment_amount()
+        self.assert_(
+            currency.is_zero(loan.payment_amount - Decimal(739.69)))
 
-            loan.calculate_increments('partially', 12)
-            self.assert_(len(loan.increments) == 2)
-            increment_1 = loan.get_increment(loan.first_payment_date)
-            self.assert_(increment_1)
-            self.assert_(increment_1.defferal == 'partially')
-            self.assert_(increment_1.number_of_payments == 12)
-            self.assert_(
-                currency.is_zero(increment_1.payment_amount - Decimal(333.33)))
-            increment_2 = loan.get_increment(date(2013, 7, 15))
-            self.assert_(increment_2)
-            self.assert_(
-                currency.is_zero(increment_2.payment_amount - Decimal(778.35)))
+        loan.calculate_increments('partially', 12)
+        self.assert_(len(loan.increments) == 2)
+        increment_1 = loan.get_increment(loan.first_payment_date)
+        self.assert_(increment_1)
+        self.assert_(increment_1.defferal == 'partially')
+        self.assert_(increment_1.number_of_payments == 12)
+        self.assert_(
+            currency.is_zero(increment_1.payment_amount - Decimal(333.33)))
+        increment_2 = loan.get_increment(date(2013, 7, 15))
+        self.assert_(increment_2)
+        self.assert_(
+            currency.is_zero(increment_2.payment_amount - Decimal(778.35)))
 
-            loan.early_payments = []
-            loan.payments = loan.calculate_payments()
-            self.assert_(len(loan.payments) == loan.number_of_payments)
+        loan.early_payments = []
+        loan.payments = loan.calculate_payments()
+        self.assert_(len(loan.payments) == loan.number_of_payments)
 
-            self.assert_payment(loan, date(2013, 7, 14), 12, loan.amount,
-                Decimal(333.33), Decimal(0.0), Decimal(333.33), loan.amount)
+        self.assert_payment(loan, date(2013, 7, 14), 12, loan.amount,
+            Decimal(333.33), Decimal(0.0), Decimal(333.33), loan.amount)
 
-            self.assert_payment(loan, date(2013, 7, 15), 13, loan.amount,
-                Decimal(778.35), Decimal(445.02), Decimal(333.33),
-                Decimal(99554.98))
+        self.assert_payment(loan, date(2013, 7, 15), 13, loan.amount,
+            Decimal(778.35), Decimal(445.02), Decimal(333.33),
+            Decimal(99554.98))
 
-            self.assert_payment(loan, date(2021, 1, 15), 103,
-                Decimal(53381.95), Decimal(778.35), Decimal(600.41),
-                Decimal(177.94), Decimal(52781.54))
+        self.assert_payment(loan, date(2021, 1, 15), 103,
+            Decimal(53381.95), Decimal(778.35), Decimal(600.41),
+            Decimal(177.94), Decimal(52781.54))
 
-            self.assert_payment(loan, date(2027, 6, 15), 180,
-                Decimal(774.70), Decimal(777.28), Decimal(774.70),
-                Decimal(2.58), Decimal(0.0))
+        self.assert_payment(loan, date(2027, 6, 15), 180,
+            Decimal(774.70), Decimal(777.28), Decimal(774.70),
+            Decimal(2.58), Decimal(0.0))
 
 
 def suite():
