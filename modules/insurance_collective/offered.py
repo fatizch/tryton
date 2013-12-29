508b8a3b7532504a46b9dc984ecabf3ca2198e87
Romain Séon <romain.seon@coopengo.com>
2013-12-29 18:31:34 +0100

    collective refactoring

--- modules/insurance_collective/offered.py
+++ modules/insurance_collective/offered.py
@@ -6,19 +6,15 @@ from trytond.pyson import Eval
 
 from trytond.modules.coop_utils import utils, fields
 
-
+__metaclass__ = PoolMeta
 __all__ = [
-    'GroupProduct',
-    'GroupCoverage',
-    'GroupBenefit',
-]
+    'Product',
+    'OptionDescription',
+    ]
 
 
-class GroupProduct():
-    'Group Product'
-
+class Product:
     __name__ = 'offered.product'
-    __metaclass__ = PoolMeta
 
     is_group = fields.Boolean('Group Product',
         states={'invisible': Eval('kind') != 'insurance'})
@@ -29,34 +25,18 @@ class GroupProduct():
             cls, 'coverages', [('is_group', '=', Eval('is_group'))])
         cls.coverages = copy.copy(cls.coverages)
         cls.coverages.depends.append('is_group')
-        super(GroupProduct, cls).__setup__()
+        super(Product, cls).__setup__()
 
 
-class GroupCoverage():
-    'Group Coverage'
-
+class OptionDescription:
     __name__ = 'offered.option.description'
-    __metaclass__ = PoolMeta
 
     is_group = fields.Boolean('Group Coverage',
         states={'invisible': Eval('kind') != 'insurance'})
 
     @classmethod
     def __setup__(cls):
-        super(GroupCoverage, cls).__setup__()
+        super(OptionDescription, cls).__setup__()
         utils.update_domain(cls, 'coverages_in_package',
             [('is_group', '=', Eval('is_group'))])
         utils.update_depends(cls, 'coverages_in_package', ['is_group'])
-
-
-class GroupBenefit():
-    'Benefit'
-
-    __name__ = 'benefit'
-    __metaclass__ = PoolMeta
-
-    @classmethod
-    def get_beneficiary_kind(cls):
-        res = super(GroupBenefit, cls).get_beneficiary_kind()
-        res.append(['affiliated', 'Affiliated'])
-        return res
