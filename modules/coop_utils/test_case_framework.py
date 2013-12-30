64786e2d8056a7def657f978c5e32b7a2c857489
Jean CAVALLO <jean.cavallo@coopengo.com>
2013-12-30 15:38:35 +0100

    Set a user when running compagny related test_cases, for ir_property searching to work properly

--- modules/coop_utils/test_case_framework.py
+++ modules/coop_utils/test_case_framework.py
@@ -154,8 +154,13 @@ class TestCaseModel(ModelSingleton, model.CoopSQL, model.CoopView):
 
     @classmethod
     def run_test_case(cls, test_case):
+        cur_user = Transaction().user
         with Transaction().new_cursor(), Transaction().set_user(0):
-            cls.run_test_case_method(test_case)
+            if cur_user:
+                with Transaction().set_context(user=cur_user):
+                    cls.run_test_case_method(test_case)
+            else:
+                    cls.run_test_case_method(test_case)
             Transaction().cursor.commit()
 
     @classmethod
