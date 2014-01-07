9f886bdb4a07a15a0499bc42ea4affd76224c59a
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-01-07 11:32:20 +0100

    Refactor property product module

--- modules/property_product/__init__.py
+++ modules/property_product/__init__.py
@@ -4,5 +4,6 @@ from .product import *
 
 def register():
     Pool.register(
+        # From product
         Coverage,
         module='property_product', type_='model')
--- modules/property_product/product.py
+++ modules/property_product/product.py
@@ -1,24 +1,15 @@
-#-*- coding:utf-8 -*-
-import copy
-
 from trytond.pool import PoolMeta
-
 from trytond.modules.coop_utils import utils
 
+__metaclass__ = PoolMeta
 __all__ = ['Coverage']
 
 
-class Coverage():
-    'Coverage'
-
+class Coverage:
     __name__ = 'offered.option.description'
-    __metaclass__ = PoolMeta
 
     @classmethod
     def __setup__(cls):
         super(Coverage, cls).__setup__()
-        cls.family = copy.copy(cls.family)
-        if not cls.family.selection:
-            cls.family.selection = []
-        utils.append_inexisting(cls.family.selection,
-            ('pc', 'Property & Casualty'))
+        utils.update_selection(cls, 'selection', (
+                ('pc', 'Property & Casualty'),))
