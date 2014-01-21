1ebe86b360f5acba0d6548be5461e23f6e37713d
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-01-21 16:51:50 +0100

    Framework for per-date offered validation

--- modules/offered/offered.py
+++ modules/offered/offered.py
@@ -289,6 +289,27 @@ class Offered(model.CoopView, GetResult, Templated):
     def init_dict_for_rule_engine(self, args):
         pass
 
+    @classmethod
+    def get_dated_fields(cls):
+        return [x for x in cls._fields.keys() if x.endswith('rules')]
+
+    @classmethod
+    def validate(cls, instances):
+        versioned_fields = cls.get_dated_fields()
+        for instance in instances:
+            values = []
+            for field in versioned_fields:
+                values += getattr(instance, field)
+            dates = set(map(lambda x: x.start_date, values))
+            for date in dates:
+                data = dict([
+                        (x, utils.get_good_version_at_date(instance, x, date))
+                        for x in versioned_fields])
+                instance.validate_consistency(data)
+
+    def validate_consistency(self, data):
+        pass
+
 
 class Product(model.CoopSQL, Offered):
     'Product'
