aa26436ee8714817fa7dccf1c8e7f4fb93a73994
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-02-25 11:02:23 +0100

    Test loan contract subscription and extra-premium calculation

--- modules/loan/tests/test_module.py
+++ modules/loan/tests/test_module.py
@@ -6,6 +6,7 @@ import unittest
 import trytond.tests.test_tryton
 
 from trytond.modules.cog_utils import test_framework
+from trytond.transaction import Transaction
 
 
 class ModuleTestCase(test_framework.CoopTestCase):
@@ -24,7 +25,8 @@ class ModuleTestCase(test_framework.CoopTestCase):
 
     @classmethod
     def get_test_cases_to_run(cls):
-        return ['fiscal_year_test_case']
+        return ['fiscal_year_test_case', 'extra_premium_kind_test_case',
+            'configure_accounting_test_case']
 
     @classmethod
     def get_models(cls):
@@ -41,6 +43,8 @@ class ModuleTestCase(test_framework.CoopTestCase):
             'DistNetwork': 'distribution.network',
             'CommercialProduct': 'distribution.commercial_product',
             'OfferedPaymentMethod': 'offered.product-billing.payment.method',
+            'ExtraPremiumKind': 'extra_premium.kind',
+            'ExtraPremium': 'contract.covered_data.extra_premium',
             }
 
     def test0010loan_basic_data(self):
@@ -117,7 +121,6 @@ class ModuleTestCase(test_framework.CoopTestCase):
 
         loan.save()
 
-    @test_framework.prepare_test('company_cog.test0001_testCompanyCreation')
     def test0025_CreateAccountKind(self):
         product_account_kind = self.AccountKind()
         product_account_kind.name = 'Product Account Kind'
@@ -152,7 +155,7 @@ class ModuleTestCase(test_framework.CoopTestCase):
         disability_account.company = company
         disability_account.save()
 
-    def test0028_CreatPersonItemDesc(self):
+    def test0028_CreatePersonItemDesc(self):
         item_desc = self.ItemDescription()
         item_desc.code = 'person_item_desc'
         item_desc.name = 'Person Item Description'
@@ -160,35 +163,61 @@ class ModuleTestCase(test_framework.CoopTestCase):
         item_desc.save()
 
     @test_framework.prepare_test('loan.test0026_CreateAccounts',
-        'loan.test0028_CreatPersonItemDesc',
+        'loan.test0028_CreatePersonItemDesc',
         'billing_individual.test0016_PaymentMethod')
     def test0030_LoanCoverageCreation(self):
-        today = self.Date.today()
+        main_date = date(2014, 1, 1)
         company, = self.Company.search([('party.name', '=', 'Coop')])
         item_desc, = self.ItemDescription.search([
                 ('code', '=', 'person_item_desc')])
+
+        # Death Coverage
+        pricing_comp_death = self.PremiumRuleComponent()
+        pricing_comp_death.config_kind = 'simple'
+        pricing_comp_death.fixed_amount = 200
+        pricing_comp_death.kind = 'base'
+        pricing_comp_death.code = 'Main'
+        pricing_comp_death.rated_object_kind = 'sub_item'
+        premium_rule_death = self.Pricing()
+        premium_rule_death.sub_item_components = [pricing_comp_death]
+        premium_rule_death.start_date = main_date
         death = self.OptionDescription()
         death.name = 'Death'
         death.code = 'DH'
         death.family = 'loan'
-        death.start_date = today
+        death.start_date = main_date
         death.company = company
         death.account_for_billing = self.Account.search([
                 ('name', '=', 'Death Option Account')])[0]
         death.item_desc = item_desc
         death.kind = 'insurance'
+        death.premium_rules = [premium_rule_death]
         death.save()
+
+        # Disability Coverage
+        pricing_comp_disability = self.PremiumRuleComponent()
+        pricing_comp_disability.config_kind = 'simple'
+        pricing_comp_disability.fixed_amount = 50
+        pricing_comp_disability.kind = 'base'
+        pricing_comp_disability.code = 'Main'
+        pricing_comp_disability.rated_object_kind = 'sub_item'
+        premium_rule_disability = self.Pricing()
+        premium_rule_disability.sub_item_components = [pricing_comp_disability]
+        premium_rule_disability.start_date = main_date
         disability = self.OptionDescription()
         disability.name = 'Disability'
         disability.code = 'DY'
         disability.family = 'loan'
-        disability.start_date = today
+        disability.start_date = main_date
         disability.company = company
         disability.account_for_billing = self.Account.search([
                 ('name', '=', 'Disability Option Account')])[0]
         disability.item_desc = item_desc
         disability.kind = 'insurance'
+        disability.premium_rules = [premium_rule_disability]
         disability.save()
+
+        # Loan Product
         loan_contract_sequence_code = self.SequenceType()
         loan_contract_sequence_code.name = 'Product sequence'
         loan_contract_sequence_code.code = 'product_sequence'
@@ -204,7 +233,7 @@ class ModuleTestCase(test_framework.CoopTestCase):
         loan = self.Product()
         loan.name = 'Loan Product'
         loan.code = 'LOAN'
-        loan.start_date = today
+        loan.start_date = main_date
         loan.company = company
         loan.kind = 'insurance'
         loan.contract_generator = loan_contract_sequence
@@ -228,67 +257,97 @@ class ModuleTestCase(test_framework.CoopTestCase):
         commercial_product = self.CommercialProduct()
         commercial_product.name = 'Loan Commercial Product'
         commercial_product.code = 'loan_commercial_product'
-        commercial_product.start_date = self.Date.today()
         commercial_product.description = 'Test Description'
         commercial_product.product = self.Product.search([
                 ('code', '=', 'LOAN')])[0]
+        commercial_product.start_date = commercial_product.product.start_date
         commercial_product.dist_networks = self.DistNetwork.search([])
         commercial_product.save()
 
     @test_framework.prepare_test(
         'loan.test0020loan_creation',
         'contract_insurance.test0001_testPersonCreation',
-        'loan.test0032_LoanCommercialProduct',
-        'billing_individual.test0010_payment_term_first_following_month')
+        'loan.test0032_LoanCommercialProduct')
     def test0040_LoanContractSubscription(self):
         company, = self.Company.search([('party.name', '=', 'Coop')])
-        commercial_product = self.CommercialProduct.search([])
-        self.assertEqual(len(commercial_product), 1)
-        commercial_product = commercial_product[0]
-        self.assertEqual(commercial_product.code, 'loan_commercial_product')
-        today = self.Date.today()
+        with Transaction().set_context(user=1):
+            commercial_product = self.CommercialProduct.search([])
+            self.assertEqual(len(commercial_product), 1)
+            commercial_product = commercial_product[0]
+            self.assertEqual(commercial_product.code,
+                'loan_commercial_product')
+            main_date = date(2014, 2, 25)
 
-        contract = self.Contract()
-        contract.init_from_offered(commercial_product.product, today)
-        contract.company = company
-        contract.save()
-        contract.subscriber, = self.Party.search([('name', '=', 'DOE')])
-        contract.save()
-        contract.check_product_not_null()
-        contract.check_subscriber_not_null()
-        contract.check_start_date_valid()
-        contract.check_product_eligibility()
-        self.assertEqual(len(contract.covered_elements), 0)
-        contract.set_subscriber_as_covered_element()
-        self.assertEqual(len(contract.covered_elements), 1)
-        contract.save()
-        contract.loans = self.Loan.search([])
-        self.assertEqual(len(contract.loans), 1)
-        contract.set_contract_end_date_from_loans()
-        self.assertEqual(contract.end_date, date(2027, 7, 14))
-        contract.save()
-        contract.init_options()
-        contract.init_covered_elements()
-        contract.init_extra_data()
-        contract.save()
-        self.assertEqual(len(contract.covered_elements[0].covered_data), 2)
-        contract.check_options_eligibility()
-        contract.check_at_least_one_covered()
-        contract.check_sub_elem_eligibility()
-        contract.check_option_dates()
-        contract.update_coverage_amounts_if_needed()
-        contract.check_covered_amounts()
-        contract.update_agreements()
+            contract = self.Contract()
+            contract.init_from_offered(commercial_product.product, main_date)
+            contract.company = company
+            contract.save()
+            contract.subscriber, = self.Party.search([('name', '=', 'DOE')])
+            contract.save()
+            contract.check_product_not_null()
+            contract.check_subscriber_not_null()
+            contract.check_start_date_valid()
+            contract.check_product_eligibility()
+            self.assertEqual(len(contract.covered_elements), 0)
+            contract.set_subscriber_as_covered_element()
+            self.assertEqual(len(contract.covered_elements), 1)
+            contract.save()
+            contract.loans = self.Loan.search([])
+            self.assertEqual(len(contract.loans), 1)
+            contract.set_contract_end_date_from_loans()
+            self.assertEqual(contract.end_date, date(2027, 7, 14))
+            contract.save()
+            contract.init_options()
+            contract.init_covered_elements()
+            contract.init_extra_data()
+            contract.save()
+            self.assertEqual(len(contract.covered_elements[0].covered_data), 2)
+            contract.check_options_eligibility()
+            contract.check_at_least_one_covered()
+            contract.check_sub_elem_eligibility()
+            contract.check_option_dates()
+            contract.update_coverage_amounts_if_needed()
+            contract.check_covered_amounts()
+            contract.update_agreements()
+            contract.calculate_prices()
+            contract.save()
+            self.assertEqual(contract.billing_datas[0].payment_method,
+                self.PaymentMethod.search([
+                        ('code', '=', 'test_payment_method')])[0])
+            contract.check_billing_data()
+            contract.activate_contract()
+            contract.finalize_contract()
+            contract.save()
+            self.assertEqual(contract.status, 'active')
+
+    @test_framework.prepare_test('loan.test0040_LoanContractSubscription')
+    def test0041_TestPremiumModification(self):
+        contract = self.Contract.search([
+                ('start_date', '=', date(2014, 2, 25)),
+                ('subscriber.name', '=', 'DOE'),
+                ('offered.code', '=', 'LOAN')])[0]
+        self.assertEqual(contract.prices[6].amount, Decimal('200'))
+        covered_data = contract.covered_elements[0].covered_data[0]
+        extra_premium = self.ExtraPremium()
+        extra_premium.covered_data = covered_data
+        extra_premium.motive = self.ExtraPremiumKind.search([
+                ('code', '=', 'medical_risk')])[0]
+        extra_premium.start_date = date(2014, 2, 25)
+        extra_premium.end_date = date(2015, 2, 24)
+        extra_premium.calculation_kind = 'flat'
+        extra_premium.flat_amount = 10000
+        extra_premium.save()
         contract.calculate_prices()
-        contract.save()
-        self.assertEqual(contract.billing_datas[0].payment_method,
-            self.PaymentMethod.search([
-                    ('code', '=', 'test_payment_method')])[0])
-        contract.check_billing_data()
-        contract.activate_contract()
-        contract.finalize_contract()
-        contract.save()
-        self.assertEqual(contract.status, 'active')
+        self.assertEqual(contract.prices[6].amount, Decimal('10200'))
+        line = contract.prices[6].all_lines[0]
+        self.assertEqual(line.on_object, covered_data)
+        self.assertEqual(len(line.all_lines), 1)
+        self.assertEqual(line.all_lines[0].on_object.__name__, 'loan.share')
+        self.assertEqual(len(line.all_lines[0].all_lines), 2)
+        self.assertEqual(line.all_lines[0].all_lines[-1].on_object,
+            extra_premium.motive)
+        self.assertEqual(line.all_lines[0].all_lines[-1].amount,
+            Decimal('10000'))
 
 
 def suite():
