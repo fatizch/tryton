2cac5b8d06973784a4ded0192d25883a8291cd98
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-04-01 09:42:00 +0200

    Remove readonly states on offered -> company

--- modules/offered/offered.py
+++ modules/offered/offered.py
@@ -167,8 +167,7 @@ class Offered(model.CoopView, GetResult, Templated):
         context={'extra_data_kind': 'product'},
         domain=[('kind', '=', 'product')])
     company = fields.Many2One('company.company', 'Company', required=True,
-        ondelete='RESTRICT', states={'readonly': Eval('id', -1) >= 0},
-        depends=['id'])
+        ondelete='RESTRICT')
 
     @classmethod
     def __setup__(cls):
