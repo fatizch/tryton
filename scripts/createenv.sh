72c0d9d810eabec43d1839863d5d0ea5b7a01fed
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-11-26 17:00:44 +0100

    scripts : Update createnv to add simpleeval dependency,  remove pytz and useless text
    review http://rietveld.coopengo.com/7270001

--- scripts/createenv.sh
+++ scripts/createenv.sh
@@ -37,8 +37,6 @@ else
     source bin/activate
     echo Installing shared pip packages
     echo $SEP
-    # pytz does not install properly through pip
-    easy_install pytz
     pip install dateutils
     # pyflakes 0.5.0 for rule_engine validation, to upgrade
     pip install pyparsing==1.5.7 pyflakes==0.5.0
@@ -46,7 +44,7 @@ else
     echo Installing server pip packages
     echo $SEP
     # Genshi 0.6 for relatorio compatibility
-    pip install lxml polib genshi==0.6 relatorio python-ldap pywebdav vobject pydot ibanlib argcomplete python-stdnum python-sql raven Mock
+    pip install lxml polib genshi==0.6 relatorio python-ldap pywebdav vobject pydot ibanlib argcomplete python-stdnum python-sql raven Mock simpleeval
     echo $SEP
     echo Installing client packages
     echo $SEP
@@ -81,14 +79,6 @@ else
     # Batchs managing
     pip install celery flower
     echo $SEP
-    echo Installation complete, remember to add
-    echo    '[extensions]'
-    echo    'hgnested='
-    echo    'hgreview='
-    echo    ''
-    echo    '[review]'
-    echo    'server = http://codereview.tryton.org'
-    echo    'send_email = False (True if you are sure of what you are doing)'
-    echo to your home hgrc file before calling coopinstall.sh
+    echo Installation complete
     echo $SEP
 fi
