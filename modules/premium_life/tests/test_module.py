997a782b01e3875ddb55adc84e6e925004f2899a
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-12-17 19:33:20 +0100

    premium_life : Move tests from offered_life
    review http://rietveld.coopengo.com/490001

--- modules/premium_life/tests/test_module.py
+++ modules/premium_life/tests/test_module.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime
+from mock import Mock
 
 import trytond.tests.test_tryton
 from trytond.modules.cog_utils import test_framework
@@ -10,6 +12,35 @@ class ModuleTestCase(test_framework.CoopTestCase):
     @classmethod
     def get_module_name(cls):
         return 'premium_life'
+
+    @classmethod
+    def get_models(cls):
+        return {
+            'PremiumDates': 'offered.product.premium_dates',
+            }
+
+    def test0011_premium_date_configuration(self):
+        premium_configuration = self.PremiumDates(
+            yearly_on_new_eve=False,
+            yearly_on_start_date=False,
+            yearly_custom_date=None,
+            yearly_each_covered_anniversary_date=True)
+        contract = Mock()
+        contract.start_date = datetime.date(2014, 03, 01)
+        contract.end_date = datetime.date(2016, 12, 31)
+
+        party = Mock()
+        party.birth_date = datetime.date(1976, 10, 21)
+        covered_element = Mock()
+        covered_element.party = party
+        covered_element.is_person = True
+        contract.covered_elements = []
+        contract.covered_elements.append(covered_element)
+
+        dates = premium_configuration.get_dates_for_contract(contract)
+        dates = sorted(list(set(dates)))
+        self.assertEqual(dates, [datetime.date(2014, 10, 21),
+             datetime.date(2015, 10, 21), datetime.date(2016, 10, 21)])
 
 
 def suite():
