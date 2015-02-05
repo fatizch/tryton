a4c0b0d87c33de431a4a2f600cd287093c989886
Romain Séon <romain.seon@coopengo.com>
2015-02-05 14:34:20 +0100

    account_invoice_cog: New icons and colors for invoices review: http://rietveld.coopengo.com/3790001/

--- modules/account_invoice_cog/invoice.py
+++ modules/account_invoice_cog/invoice.py
@@ -1,5 +1,5 @@
 from trytond.pool import PoolMeta
-from trytond.modules.cog_utils import export
+from trytond.modules.cog_utils import export, fields
 
 __metaclass__ = PoolMeta
 __all__ = [
@@ -11,6 +11,25 @@ class Invoice(export.ExportImportMixin):
     __name__ = 'account.invoice'
     _func_key = 'number'
 
+    icon = fields.Function(
+        fields.Char('Icon'),
+        'on_change_with_icon')
+
     @classmethod
     def is_master_object(cls):
         return True
+
+    @fields.depends('state', 'amount_to_pay_today')
+    def on_change_with_icon(self, name=None):
+        if self.state == 'cancel':
+            return 'invoice_cancel'
+        elif self.state == 'paid':
+            return 'invoice_paid'
+        elif self.state == 'draft':
+            return 'invoice_draft'
+        elif self.amount_to_pay_today > 0:
+            return 'invoice_unpaid'
+        elif self.state == 'posted':
+            return 'invoice_post'
+        else:
+            return 'invoice'
