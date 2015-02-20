8573889823f272a45c36dfce1a58c5b06d16081e
Frederic Langevin <frederic.langevin@coopengo.com>
2015-02-20 16:29:45 +0100

    commission_insurance: Allow to calculate commission pattern rule from coverage http://rietveld.coopengo.com/860001

--- modules/commission_insurance/contract.py
+++ modules/commission_insurance/contract.py
@@ -55,44 +55,46 @@ class Contract:
         invoice.agent = self.agent
         return invoice
 
-    def find_insurer_agent_domain(self, line):
+    def find_insurer_agent_domain(self, coverage=None, line=None):
         domain = [('type_', '=', 'principal')]
         if self.agent and self.agent.plan.insurer_plan:
             domain.append(('plan', '=', self.agent.plan.insurer_plan))
-        coverage = getattr(line.details[0], 'rated_entity', None)
+        if not coverage and line:
+            coverage = getattr(line.details[0], 'rated_entity', None)
         if coverage and getattr(coverage, 'insurer', None):
             domain.append(('party', '=', coverage.insurer.party))
         else:
             return None
         return domain
 
-    def get_insurer_pattern(self, line):
+    def get_insurer_pattern(self, coverage=None, line=None):
         pattern = {}
         if self.agent and self.agent.plan.insurer_plan:
             pattern['plan'] = self.agent.plan.insurer_plan
-        coverage = getattr(line.details[0], 'rated_entity', None)
+        if not coverage and line:
+            coverage = getattr(line.details[0], 'rated_entity', None)
         if coverage:
             pattern['option'] = coverage
             if coverage.insurer:
                 pattern['party'] = coverage.insurer.party
         return pattern
 
-    def find_insurer_agent(self, line):
+    def find_insurer_agent(self, coverage=None, line=None):
         pool = Pool()
         Agent = pool.get('commission.agent')
-        domain = self.find_insurer_agent_domain(line)
+        domain = self.find_insurer_agent_domain(coverage, line)
         if not domain:
             return
         agents = Agent.search(domain)
         for agent in agents:
             for plan_line in agent.plan.lines:
-                if plan_line.match(self.get_insurer_pattern(line)):
+                if plan_line.match(self.get_insurer_pattern(coverage, line)):
                     return agent
 
     def finalize_invoices_lines(self, lines):
         super(Contract, self).finalize_invoices_lines(lines)
         for line in lines:
-            line.principal = self.find_insurer_agent(line)
+            line.principal = self.find_insurer_agent(line=line)
         return lines
 
     @fields.depends('agent')
