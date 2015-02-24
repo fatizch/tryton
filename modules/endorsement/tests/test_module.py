cee54e7d5fbb712e836233724ececdbe43a6825a
Jeremy Mousset <jeremy.mousset@coopengo.com>
2015-02-24 17:59:22 +0100

    endorsement : fix tests after changeset 31a0b77a897a http://rietveld.coopengo.com/9740001/

--- modules/endorsement/tests/test_module.py
+++ modules/endorsement/tests/test_module.py
@@ -128,7 +128,8 @@ class ModuleTestCase(test_framework.CoopTestCase):
         possible_views = [
             x[0] for x in self.EndorsementPart.get_possible_views()]
         self.assertEqual(possible_views, ['simple_contract_modification',
-                'dummy_step', 'change_start_date'])
+                'dummy_step', 'change_start_date',
+                'change_contract_extra_data'])
 
     @test_framework.prepare_test(
         'endorsement.test0001_check_possible_views',
