343945d7ecc03768650227c3687f09d58e834e6a
Jeremy Mousset <jeremy.mousset@coopengo.com>
2015-04-16 10:38:19 +0200

    endorsement_set_process : fix process finder http://rietveld.coopengo.com/10260001/

--- modules/endorsement_set_process/process.py
+++ modules/endorsement_set_process/process.py
@@ -1,6 +1,7 @@
 from trytond.pool import Pool, PoolMeta
 
 from trytond.modules.process_cog import ProcessFinder, ProcessStart
+from trytond.modules.cog_utils import fields
 
 
 __metaclass__ = PoolMeta
@@ -26,14 +27,17 @@ class EndorsementSetApplyFindProcess(ProcessStart):
 
     __name__ = 'endorsement.set.apply.find_process'
 
+    effective_date = fields.Date('Effective Date', required=True)
+    contract_set = fields.Many2One('contract.set', 'Contract Set',
+        required=True)
+    endorsement_set = fields.Many2One('endorsement.set', 'Endorsement Set')
+    endorsement_definition = fields.Many2One('endorsement.definition',
+        'Endorsement Definition', required=True)
+
     @classmethod
     def default_model(cls):
         Model = Pool().get('ir.model')
         return Model.search([('model', '=', 'endorsement.set')])[0].id
-
-    @classmethod
-    def build_process_domain(cls):
-        return []
 
 
 class EndorsementSetApply(ProcessFinder):
@@ -47,9 +51,18 @@ class EndorsementSetApply(ProcessFinder):
 
     @classmethod
     def get_parameters_view(cls):
-        return 'process_cog.process_parameters_form'
+        return \
+            'endorsement_set_process.endorsement_set_apply_find_process_form'
 
     def init_main_object_from_process(self, obj, process_param):
+        pool = Pool()
+        Endorsement = pool.get('endorsement')
+        ContractEndorsement = pool.get('endorsement.contract')
         res, errs = super(EndorsementSetApply,
             self).init_main_object_from_process(obj, process_param)
+        obj.endorsements = [Endorsement(
+                effective_date=process_param.effective_date,
+                definition=process_param.endorsement_definition,
+                contract_endorsements=[ContractEndorsement(contract=contract)])
+                for contract in process_param.contract_set.contracts]
         return res, errs
