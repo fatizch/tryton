f8a89f34df196d69212652cb26a8bb094c962d30
Frederic Langevin <frederic.langevin@coopengo.com>
2015-04-20 09:48:31 +0200

    contract_insurance_health_fr: Add subscriber hc system rule engine data Fix #1684: https://redmine.coopengo.com/issues/1684 http://rietveld.coopengo.com/12110001

--- modules/contract_insurance_health_fr/rule_engine.py
+++ modules/contract_insurance_health_fr/rule_engine.py
@@ -18,6 +18,17 @@ class RuleEngineRuntime:
         cls.append_error(args, 'Cannot find the hc_system')
 
     @classmethod
+    def _re_health_subscriber_hc_system(cls, args):
+        if 'contract' in args:
+            person = args['contract'].subscriber
+        else:
+            person = cls.get_person(args)
+        if person.health_complement:
+            hc_system = person.health_complement[0].hc_system
+            return hc_system.code if hc_system else ''
+        cls.append_error(args, 'Cannot find the hc_system')
+
+    @classmethod
     def _re_health_department(cls, args):
         person = cls.get_person(args)
         if person.health_complement:
