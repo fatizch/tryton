7b774e010223d92520f2f927c662d22d6ba3f90c
Jeremy Mousset <jeremy.mousset@coopengo.com>
2015-04-21 18:14:05 +0200

    contract_insurance : date migration script should not return if no options or extra_premiums http://rietveld.coopengo.com/10290001/

--- modules/contract_insurance/migrate_dates.py
+++ modules/contract_insurance/migrate_dates.py
@@ -38,16 +38,15 @@ def migrate_dates():
                 'select id, start_date from contract_option')
             to_save = []
             values = dict(transaction.cursor.fetchall())
-            if not values:
-                return
-            to_save = []
-            for option_id, prev_start_date in values.iteritems():
-                option = Option(option_id)
-                start_date = option.parent_contract.start_date
-                if prev_start_date != start_date:
-                    option.manual_start_date = prev_start_date
-                    to_save.append(option)
-            Option.save(to_save)
+            if values:
+                to_save = []
+                for option_id, prev_start_date in values.iteritems():
+                    option = Option(option_id)
+                    start_date = option.parent_contract.start_date
+                    if prev_start_date != start_date:
+                        option.manual_start_date = prev_start_date
+                        to_save.append(option)
+                Option.save(to_save)
         except Exception as e:
             transaction.cursor.rollback()
             raise e
@@ -62,25 +61,25 @@ def migrate_dates():
                 'contract_option_extra_premium')
             request_res = {item[0]: (item[1], item[2]) for
                 item in transaction.cursor.fetchall()}
-            if not request_res:
-                return
-            to_save = []
-            for extra_premium_id, dates in request_res.iteritems():
-                extra_premium = ExtraPremium(extra_premium_id)
-                delta = relativedelta(dates[1], dates[0])
-                months = delta.months + 12 * delta.years
-                extra_premium.duration_unit = 'month'
-                if months:
-                    extra_premium.duration = months + 1
-                start_date = extra_premium.option.parent_contract.start_date
-                if dates[0] != start_date:
-                    extra_premium.manual_start_date = dates[0]
-                if months and (dates[0] + relativedelta(months=months + 1,
-                            days=-1)) != dates[1]:
-                    extra_premium.manual_end_date = dates[1]
-                    extra_premium.duration = months + 2
-                to_save.append(extra_premium)
-            ExtraPremium.save(to_save)
+            if request_res:
+                to_save = []
+                for extra_premium_id, dates in request_res.iteritems():
+                    extra_premium = ExtraPremium(extra_premium_id)
+                    delta = relativedelta(dates[1], dates[0])
+                    months = delta.months + 12 * delta.years
+                    extra_premium.duration_unit = 'month'
+                    if months:
+                        extra_premium.duration = months + 1
+                    start_date = \
+                        extra_premium.option.parent_contract.start_date
+                    if dates[0] != start_date:
+                        extra_premium.manual_start_date = dates[0]
+                    if months and (dates[0] + relativedelta(months=months + 1,
+                                days=-1)) != dates[1]:
+                        extra_premium.manual_end_date = dates[1]
+                        extra_premium.duration = months + 2
+                    to_save.append(extra_premium)
+                ExtraPremium.save(to_save)
         except Exception as e:
             transaction.cursor.rollback()
             raise e
