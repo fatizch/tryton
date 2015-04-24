4eda556c75dc2455b19fce79b52574fee5f3606b
Jeremy Mousset <jeremy.mousset@coopengo.com>
2015-04-24 16:50:41 +0200

    endorsement_party_process : add module fix #1695 http://rietveld.coopengo.com/14080001/

---
+++ modules/endorsement_party_process/__init__.py
@@ -0,0 +1,12 @@
+from trytond.pool import Pool
+from .process import *
+
+
+def register():
+    Pool.register(
+        Process,
+        EndorsementPartyFindProcess,
+        module='endorsement_party_process', type_='model')
+    Pool.register(
+        EndorsementPartyStartProcess,
+        module='endorsement_party_process', type_='wizard')
---
+++ modules/endorsement_party_process/process.py
@@ -0,0 +1,70 @@
+from trytond.modules.process_cog import ProcessFinder, ProcessStart
+from trytond.modules.cog_utils import fields
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
+
+__metaclass__ = PoolMeta
+
+__all__ = [
+    'Process',
+    'EndorsementPartyFindProcess',
+    'EndorsementPartyStartProcess',
+    ]
+
+
+class Process:
+    __name__ = 'process'
+    __metaclass__ = PoolMeta
+
+    @classmethod
+    def __setup__(cls):
+        super(Process, cls).__setup__()
+        cls.kind.selection.append(('party_endorsement', 'Party Endorsement'))
+
+
+class EndorsementPartyFindProcess(ProcessStart):
+    'Endorsement Party Process Finder'
+
+    __name__ = 'endorsement_party.start.find_process'
+
+    effective_date = fields.Date('Effective Date', required=True)
+    definition = fields.Many2One('endorsement.definition',
+        'Endorsement Definition', required=True, domain=[
+            ('is_party', '=', True)])
+    party = fields.Many2One('party.party', 'Party', required=True)
+
+    @classmethod
+    def default_model(cls):
+        pool = Pool()
+        Model = pool.get('ir.model')
+        return Model.search([('model', '=', 'endorsement')])[0].id
+
+    @classmethod
+    def build_process_domain(cls):
+        return [('on_model', '=', Eval('model')),
+            ('kind', '=', 'party_endorsement')]
+
+
+class EndorsementPartyStartProcess(ProcessFinder):
+    'Endorsement Start Process'
+
+    __name__ = 'endorsement_party.start_process'
+
+    @classmethod
+    def get_parameters_model(cls):
+        return 'endorsement_party.start.find_process'
+
+    @classmethod
+    def get_parameters_view(cls):
+        return 'endorsement_party_process.' + \
+            'endorsement_party_start_process_find_process_view_form'
+
+    def init_main_object_from_process(self, obj, process_param):
+        pool = Pool()
+        PartyEndorsement = pool.get('endorsement.party')
+        res, errs = super(EndorsementPartyStartProcess,
+            self).init_main_object_from_process(obj, process_param)
+        obj.effective_date = process_param.effective_date
+        obj.definition = process_param.definition
+        obj.party_endorsements = [PartyEndorsement(party=process_param.party)]
+        return res, errs
