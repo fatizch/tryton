c9fa8c7aab59a18853e11b26eef0df89eee11478
Frederic Langevin <frederic.langevin@coopengo.com>
2015-10-02 11:49:00 +0200

    contract: add product code as business data in rule engine http://rietveld.coopengo.com/15360001

--- modules/contract/rule_engine.py
+++ modules/contract/rule_engine.py
@@ -34,6 +34,10 @@ class RuleEngineRuntime:
         return args['contract'].initial_start_date
 
     @classmethod
+    def _re_product_code(cls, args):
+        return args['contract'].product.code
+
+    @classmethod
     def _re_get_contract_start_date(cls, args):
         return args['contract'].start_date
 
