111e1470fd0aa738fd57e0671a9b083cd0bed0df
Frederic Langevin <frederic.langevin@coopengo.com>
2015-10-29 14:57:19 +0100

    commission_insurance_recovery: add paid invoice end date business data Fix #2724: https://redmine.coopengo.com/issues/2724 http://rietveld.coopengo.com/16520001

--- modules/commission_insurance_recovery/rule_engine.py
+++ modules/commission_insurance_recovery/rule_engine.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from sql.aggregate import Sum
+from sql.aggregate import Sum, Max
 
 from trytond.pool import PoolMeta, Pool
 from trytond.transaction import Transaction
@@ -22,7 +22,26 @@ class RuleEngineRuntime:
         commission = pool.get('commission').__table__()
 
         cursor.execute(*commission.select(Sum(commission.amount),
-            where=((commission.commissioned_option == args['option'].id) and
-                commission.agent == args['agent'].id)))
+            where=((commission.commissioned_option == args['option'].id) &
+                commission.agent == args['agent'].id) &
+                commission.invoice_line != None))
         res = cursor.fetchall()
         return res[0][0] or Decimal(0)
+
+    @classmethod
+    @check_args('option')
+    def re_last_paid_invoice_end_date(cls, args):
+        pool = Pool()
+        cursor = Transaction().cursor
+        contract_invoice = pool.get('contract.invoice').__table__()
+        invoice_line = pool.get('account.invoice.line').__table__()
+        details = pool.get('account.invoice.line.detail').__table__()
+
+        query_table = contract_invoice.join(invoice_line, condition=(
+                contract_invoice.invoice == invoice_line.invoice)
+            ).join(details, condition=(
+                invoice_line.id == details.invoice_line))
+        cursor.execute(*query_table.select(Max(contract_invoice.end),
+            where=((details.option == args['option'].id))))
+        res = cursor.fetchall()
+        return res[0][0]
