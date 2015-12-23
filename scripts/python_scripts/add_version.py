31b5b3a0a5bef466e50216c9908eee28a39c20b3
Frederic Langevin <frederic.langevin@coopengo.com>
2015-12-23 14:07:32 +0100

    Prepare Coog Release 1.6.0

--- scripts/python_scripts/add_version.py
+++ scripts/python_scripts/add_version.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import os
 
-version_line = 'Version 1.3 - 2015-01-22\n'
-module_dir = os.path.join(os.environ.get('VIRTUAL_ENV'), 'tryton-workspace',
-    'coopbusiness', 'modules')
+version_line = 'Version 1.6 - 2015-12-23\n'
+module_dir = os.path.join(os.environ.get('VIRTUAL_ENV'), 'coog', 'modules')
 
 for module in sorted(os.listdir(module_dir)):
     if not os.path.isfile(os.path.join(module_dir, module, 'CHANGELOG')):
@@ -12,4 +11,6 @@ for module in sorted(os.listdir(module_dir)):
         lines = changelog.read()
     with open(os.path.join(module_dir, module, 'CHANGELOG'), 'w') as changelog:
         changelog.write(version_line)
+        if lines[0:7] == 'Version':
+            changelog.write('\n')
         changelog.write(lines)
