1912dcccec72865ad27702b94224babb582b3db9
Ali Kefia <ali.kefia@coopengo.com>
2016-01-07 14:09:22 +0100

    bin: print today's redmine logged time

---
+++ bin/.py/redmine-te-parse.py
@@ -0,0 +1,19 @@
+import sys
+import json
+import HTMLParser
+h = HTMLParser.HTMLParser()
+
+data = sys.stdin.read()
+entries = json.loads(data)['time_entries']
+total = 0
+for entry in entries:
+    hours = entry['hours']
+    project = h.unescape(entry['project']['name'])
+    activity = h.unescape(entry['activity']['name'])
+    issue = entry.get('issue', {'id': 0})['id']
+    comment = h.unescape(entry.get('comments', ''))
+    print u'{4}: {0: <10} {1: <30} {2: <5} {3}'.format(project, activity,
+        issue, comment, hours)
+    total += hours
+
+print '\nYou have logged %s hours' % total
