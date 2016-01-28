b4fd19c7de354f11555ee73d1e9f3f29c34259bc
Jérémy Mousset <jeremy.mousset@coopengo.com>
2016-01-28 16:14:26 +0100

    endorsement_party : do not add twice the same contract in get_contracts_from_object http://rietveld.coopengo.com/13750001/

--- modules/endorsement_party/event.py
+++ modules/endorsement_party/event.py
@@ -13,10 +13,8 @@ class EventTypeAction:
         contracts = super(EventTypeAction,
             self).get_contracts_from_object(object_)
         if object_.__name__ == 'endorsement':
-            contracts.extend([x.contract for x
-                    in object_.contract_endorsements])
             if object_.party_endorsements:
                 for party_endorsement in object_.party_endorsements:
                     contracts.extend(
                         party_endorsement.party.get_all_contracts())
-        return contracts
+        return list(set(contracts))
