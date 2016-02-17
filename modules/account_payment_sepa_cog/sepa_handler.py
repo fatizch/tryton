7d54a54a03f34d950676654ec078e8309531d8a1
Frederic Langevin <frederic.langevin@coopengo.com>
2016-02-17 14:46:35 +0100

    account_payment_sepa_cog: Fix #3438: Set today as reject date if no date defined in CAMT054 file http://rietveld.coopengo.com/16970003

--- modules/account_payment_sepa_cog/sepa_handler.py
+++ modules/account_payment_sepa_cog/sepa_handler.py
@@ -3,6 +3,8 @@ from dateutil.parser import parse
 
 from trytond.modules.account_payment_sepa.payment import CAMT054
 
+from trytond.modules.cog_utils import utils
+
 __all__ = ['CAMT054Coog']
 
 
@@ -11,7 +13,7 @@ class CAMT054Coog(CAMT054):
     def set_return_information(self, payment, element):
         super(CAMT054Coog, self).set_return_information(payment, element)
         date_value = self.date_value(element)
-        payment.sepa_bank_reject_date = date_value
+        payment.sepa_bank_reject_date = date_value or utils.today()
 
     def date_value(self, element):
         date = super(CAMT054Coog, self).date_value(element)
