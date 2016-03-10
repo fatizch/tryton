f1c5d1a11cafd08d52c1d04c7b372c414a55a968
Romain Seon <romain.seon@coopengo.com>
2016-03-10 17:06:23 +0100

    account_payment_cog: Restore deleted commit Fix #3575

--- modules/account_payment_cog/payment.py
+++ modules/account_payment_cog/payment.py
@@ -503,8 +503,8 @@ class Payment(export.ExportImportMixin, Printable):
             if len(all_payments) != len(
                     [x for x in payments if x.merged_id]):
                 cls.raise_user_error('missing_payments')
-            fields = cls.payments_fields_to_update_after_fail(reject_reason)
-            cls.write(payments, fields)
+        fields = cls.payments_fields_to_update_after_fail(reject_reason)
+        cls.write(payments, fields)
 
     @classmethod
     @ModelView.button_action('account_payment_cog.manual_payment_fail_wizard')
