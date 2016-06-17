0c6338c528337be4a41d3b1a87ed04cbac23558d
Frederic Langevin <frederic.langevin@coopengo.com>
2016-06-17 09:10:50 +0200

    cog_utils: Allow to export/import notes

--- modules/cog_utils/note.py
+++ modules/cog_utils/note.py
@@ -1,14 +1,23 @@
-
 from trytond.pool import PoolMeta
+
+from trytond.modules.cog_utils import export
 
 __all__ = ['Note']
 __metaclass__ = PoolMeta
 
-class Note:
+
+class Note(export.ExportImportMixin):
     'Note'
     __name__ = 'ir.note'
+    _func_key = 'id'
 
     @classmethod
     def __setup__(cls):
         super(Note, cls).__setup__()
         cls._order = [('create_date', 'DESC')]
+
+    @classmethod
+    def add_func_key(cls, values):
+        # importing a note will always create a new one
+        # override add_func_key since it's required during import
+        values['_func_key'] = 0
