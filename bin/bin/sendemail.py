b762d72d6fe476ddc0bcb33ee6a0a7ae8e7f350a
Ali Kefia <ali.kefia@coopengo.com>
2016-06-29 10:46:15 +0200

    test: improve mail format

--- bin/bin/sendemail.py
+++ bin/bin/sendemail.py
@@ -8,16 +8,19 @@ from sendgrid.helpers.mail import *
 
 def main():
     parser = argparse.ArgumentParser(description='Coog utils to send mail')
-    parser.add_argument('--expeditor', '-f', required=True, help='From address')
-    parser.add_argument('--destination', '-t', required=True, help='To address')
+    parser.add_argument('--fromemail', '-fe', required=True, help='From email')
+    parser.add_argument('--fromname', '-fn', help='From name')
+    parser.add_argument('--toemail', '-te', required=True, help='To email')
+    parser.add_argument('--toname', '-tn', help='To name')
     parser.add_argument('--subject', '-s', required=True, help='Subject')
     arguments = parser.parse_args()
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email(arguments.expeditor)
-    to_email = Email(arguments.destination)
+
+    f = Email(email=arguments.fromemail, name=arguments.fromname)
+    t = Email(email=arguments.toemail, name=arguments.toname)
     subject = arguments.subject
-    content = Content('text/plain', sys.stdin.read())
-    mail = Mail(from_email, subject, to_email, content)
+    content = Content('text/html', sys.stdin.read())
+    mail = Mail(f, subject, t, content)
     response = sg.client.mail.send.post(request_body=mail.get())
     if response.status_code >= 300:
         print response.body
