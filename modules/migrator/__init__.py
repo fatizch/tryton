b54f8530cd1234ccba6e03c38461221d555ef3bc
Frederic Langevin <frederic.langevin@coopengo.com>
2016-07-21 11:32:23 +0200

    migrator: Add import - fix following 2cf8885

--- modules/migrator/__init__.py
+++ modules/migrator/__init__.py
@@ -1,5 +1,10 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from .migrator import Migrator
+
+__all__ = [
+    'Migrator'
+    ]
 
 
 def register():
