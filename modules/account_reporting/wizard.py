90a5c6ae9c6a8b2529ef7866d4a7acf3bb4f5b53
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2016-08-22 16:39:10 +0200

    e account_reporting: Hotfix - Test KO for b94aae56 (Unused import)

--- modules/account_reporting/wizard.py
+++ modules/account_reporting/wizard.py
@@ -1,6 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import datetime
 from itertools import groupby
 from sql.aggregate import Sum
 from sql.functions import DateTrunc
