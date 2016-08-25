d63c904a983a977fc29d476683cf914b9bd2ed09
Pierre Jackman <pierre.jackman@coopengo.com>
2016-08-25 11:21:53 +0200

    claim_indemnification: add missing module in __all__

--- modules/claim_indemnification/wizard.py
+++ modules/claim_indemnification/wizard.py
@@ -22,6 +22,7 @@ __all__ = [
     'IndemnificationRegularisation',
     'ExtraDatasDisplayers',
     'FillExtraData',
+    'SelectService',
     'IndemnificationValidateElement',
     'IndemnificationControlElement',
     'IndemnificationAssistantView',
