fatizch/tryton

commit 8533a3eb25012dce6d0f8b2da743aa2e52707c7b

offered_distribution: Getter should use appliable_condition_date to retrieve the commercial product (#39)
Fix: #4979

modules/offered_distribution/contract.py
@@ -28,7 +28,7 @@ class Contract:
         if not self.dist_network:
             return None
         com_products = utils.get_good_versions_at_date(self.dist_network,
-            'all_com_products', self.start_date)
+            'all_com_products', self.appliable_condition_date)
         com_product = [x for x in com_products if x.product == self.product]
         if com_product:
             return com_product[0].id

commit f37500c2103b74b96e1ecd9f168a1d602a91ee1d

offered_distribution: Fix typo introduced by 8533a3eb25012dce6d0f8b2da743aa2e52707c7b

modules/offered_distribution/contract.py
@@ -28,7 +28,7 @@ class Contract:
         if not self.dist_network:
             return None
         com_products = utils.get_good_versions_at_date(self.dist_network,
-            'all_com_products', self.appliable_condition_date)
+            'all_com_products', self.appliable_conditions_date)
         com_product = [x for x in com_products if x.product == self.product]
         if com_product:
             return com_product[0].id