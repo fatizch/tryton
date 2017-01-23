f50c9a2226ed807c6e8c931d2e4c9ce169e6c0ef
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2017-01-23 11:25:55 +0100

    report_engine: iterate on report requests per template (#233)
    Ref #5075

--- modules/report_engine/batch.py
+++ modules/report_engine/batch.py
@@ -1,8 +1,10 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import logging
+from itertools import groupby
 
 from trytond.pool import Pool
+from trytond.transaction import Transaction
 
 from trytond.modules.coog_core import batch
 
@@ -21,20 +23,26 @@ class ReportProductionRequestTreatmentBatch(batch.BatchRoot):
 
     @classmethod
     def get_batch_main_model_name(cls):
-        return 'report_production.request'
+        return 'report.template'
 
     @classmethod
-    def get_batch_search_model(cls):
-        return 'report_production.request'
-
-    @classmethod
-    def get_batch_domain(cls):
-        return [('treated', '=', False)]
+    def select_ids(cls):
+        pool = Pool()
+        cursor = Transaction().connection.cursor()
+        main_table = pool.get('report_production.request').__table__()
+        query = main_table.select(main_table.report_template,
+            where=(main_table.treated == False),
+            group_by=[main_table.report_template])
+        cursor.execute(*query)
+        return cursor.fetchall()
 
     @classmethod
     def execute(cls, objects, ids):
         pool = Pool()
+        ProductionRequest = pool.get('report_production.request')
         ReportProductionRequest = pool.get('report_production.request')
+        objects = ProductionRequest.search([('report_template', 'in', ids),
+                ('treated', '=', False)])
         reports, attachments = ReportProductionRequest.treat_requests(objects)
         cls.logger.info('Produced %s reports, and created %s attachments' %
             (len(reports), len(attachments)))
