72797c6b0ff0ee4636a18a7a9dc13a6878048dd5
Pierre Jackman <pierre.jackman@coopengo.com>
2017-03-07 17:34:46 +0100

    bin: automatically load batches, if none are defined, when starting celery (#393)
    - add python script which connects to server and gets a list of batch
    names
    - modify coog-celery launcher script to auto load batch names

---
+++ bin/bin/load_batches.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+from trytond.pool import Pool
+from trytond.modules.coog_core import batch
+from trytond.transaction import Transaction
+
+import tryton_init
+
+database = os.environ.get('DB_NAME', None)
+with Transaction().start(database, 0, readonly=True):
+    for name, kls in Pool().iterobject():
+        if issubclass(kls, batch.BatchRoot):
+            print name
