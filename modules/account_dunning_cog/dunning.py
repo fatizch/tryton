d38e147ff46c9fc87783e6f91c0c4319d090d477
Pierre Jackman <pierre.jackman@coopengo.com>
2017-03-29 09:56:29 +0200

    account_dunning_cog: fix typo

--- modules/account_dunning_cog/dunning.py
+++ modules/account_dunning_cog/dunning.py
@@ -66,7 +66,7 @@ class Dunning(export.ExportImportMixin):
                 dunning)
         if update_dates:
             cls.write(*sum([
-                        [values, {'state': 'done', 'last_proces_date': date}]
+                        [values, {'state': 'done', 'last_process_date': date}]
                         for date, values in update_dates.iteritems()], []))
         cls.notify_dunning_per_level(dunnings)
 
