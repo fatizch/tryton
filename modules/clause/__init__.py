572a31b3fe5b8554fbee717550867737f61823ab
Jean Cavallo <jean.cavallo@coopengo.com>
2017-04-03 17:33:18 +0200

    clause : Update __init__ file declaration

--- modules/clause/__init__.py
+++ modules/clause/__init__.py
@@ -1,12 +1,13 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .clause import *
-from .test_case import *
+
+import clause
+import test_case
 
 
 def register():
     Pool.register(
-        Clause,
-        TestCaseModel,
+        clause.Clause,
+        test_case.TestCaseModel,
         module='clause', type_='model')
