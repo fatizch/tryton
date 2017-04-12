7801574cedda2cebd25d0ee7d403f30686ffd8b5
Frederic Langevin <frederic.langevin@coopengo.com>
2017-04-12 11:56:55 +0200

    claim_group_life_fr: Add ijss_before_part_time business data (#474)

--- modules/claim_group_life_fr/rule_engine.py
+++ modules/claim_group_life_fr/rule_engine.py
@@ -1,6 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
 from trytond.pool import PoolMeta
 
 from trytond.modules.rule_engine import check_args
@@ -22,3 +21,26 @@ class RuleEngineRuntime:
         return sum([coog_date.number_of_days_between(
                     x.start_date, x.end_date)
                 for x in args['loss'].hospitalisation_periods])
+
+    @classmethod
+    @check_args('indemnification', 'indemnification_detail_start_date')
+    def _re_ijss_before_part_time(cls, args):
+        delivered = args['indemnification'].service
+        at_date = args['indemnification_detail_start_date']
+        ijss = {e.date: e.extra_data_values['ijss']
+            if 'ijss' in e.extra_data_values else 0
+            for e in delivered.extra_datas
+            if (e.date or delivered.loss.start_date) < at_date}
+        dates = sorted(ijss.keys(), reverse=True)
+        part_times = [x for x in delivered.loss.deduction_periods
+            if x.deduction_kind.code == 'part_time' and x.start_date
+            and x.start_date < at_date]
+
+        def part_time_period_at_date(date):
+            for part_time in part_times:
+                if part_time.start_date <= date and part_time.end_date >= date:
+                    return part_time
+
+        for date in dates:
+            if not part_time_period_at_date(date):
+                return ijss[date]
