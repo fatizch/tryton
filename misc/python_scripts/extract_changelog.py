40c29af695cbda7bcd874944033aa781075d9a63
Frederic Langevin <frederic.langevin@coopengo.com>
2017-04-25 11:33:16 +0200

    misc: Fix extract_changelog script to handle use of features_log instead of changelog

--- misc/python_scripts/extract_changelog.py
+++ misc/python_scripts/extract_changelog.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 import os
+import sys
+
+try:
+    language = sys.argv[1]
+except:
+    sys.stderr.write('''
+Usage :
+    extract_chnagelog.py <language>
+''')
+    sys.exit()
 
 module_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
     '..', '..', 'modules')
@@ -7,10 +17,12 @@ module_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 for module in sorted(os.listdir(module_dir)):
     print ''
     print 'Module', module
-    if not os.path.isfile(os.path.join(module_dir, module, 'CHANGELOG')):
+    if not os.path.isfile(os.path.join(module_dir, module, 'doc', language,
+            'features_log')):
         print '   NO CHANGELOG FOUND'
         continue
-    with open(os.path.join(module_dir, module, 'CHANGELOG'), 'r') as changelog:
+    with open(os.path.join(module_dir, module, 'doc', language,
+            'features_log'), 'r') as changelog:
         for line in changelog.readlines():
             if not line.strip():
                 continue
