1bc564aa5dd2d716e91dfc13cf4481f8c1327585
Pierre Jackman <pierre.jackman@coopengo.com>
2017-06-14 10:42:39 +0200

    report_engine: import Printable in report_engine init (#829)

--- modules/report_engine/__init__.py
+++ modules/report_engine/__init__.py
@@ -1,6 +1,7 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
+from report_engine import Printable
 
 import report_engine
 import ir
