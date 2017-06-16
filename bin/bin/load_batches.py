9c45c9f8da0212e448699ee63b956a4890c03358
Pierre Jackman <pierre.jackman@coopengo.com>
2017-06-16 18:25:48 +0200

    bin: init pool before searching for Batches (#854)

--- bin/bin/load_batches.py
+++ bin/bin/load_batches.py
@@ -6,8 +6,9 @@ from trytond.transaction import Transaction
 
 database = os.environ.get('DB_NAME', None)
 with Transaction().start(database, 0, readonly=True):
-    pool = Pool(database_name=database)
-    #  pool.init()
+    # Why do we do need to this all of a sudden?
+    pool = Pool()
+    pool.init()
     for name, kls in pool.iterobject():
         if issubclass(kls, batch.BatchRoot):
             print name
