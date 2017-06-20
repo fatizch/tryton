d5a292dc9253bd5a4826b2ed3c852913769dd93a
Jean Cavallo <jean.cavallo@hotmail.fr>
2017-06-20 16:58:55 +0200

    commission_insurance : Manually set parameters in batch chain (#871)
    Fix #6445

--- modules/commission_insurance/__init__.py
+++ modules/commission_insurance/__init__.py
@@ -3,47 +3,47 @@
 from trytond.pool import Pool
 
 import contract
-from .commission import *
-from .invoice import *
-from .party import *
-from .payment import *
-from .account import *
-from .batch import *
+import commission
+import invoice
+import party
+import payment
+import account
+import batch
 
 
 def register():
     Pool.register(
         contract.Contract,
         contract.ContractOption,
-        AggregatedCommission,
-        Commission,
-        PlanLines,
-        Plan,
-        PlanRelation,
-        PlanLinesCoverageRelation,
-        PlanCalculationDate,
-        Agent,
-        InvoiceLine,
-        Invoice,
-        Party,
-        CreateAgentsParties,
-        CreateAgentsAsk,
-        CreateInvoiceAsk,
-        SelectNewBroker,
-        Configuration,
-        Fee,
-        MoveLine,
-        OpenCommissionsSynthesisStart,
-        OpenCommissionsSynthesisShow,
-        OpenCommissionSynthesisYearLine,
-        CreateCommissionInvoiceBatch,
-        PostCommissionInvoiceBatch,
+        commission.AggregatedCommission,
+        commission.Commission,
+        commission.PlanLines,
+        commission.Plan,
+        commission.PlanRelation,
+        commission.PlanLinesCoverageRelation,
+        commission.PlanCalculationDate,
+        commission.Agent,
+        invoice.InvoiceLine,
+        invoice.Invoice,
+        party.Party,
+        commission.CreateAgentsParties,
+        commission.CreateAgentsAsk,
+        commission.CreateInvoiceAsk,
+        commission.SelectNewBroker,
+        payment.Configuration,
+        account.Fee,
+        account.MoveLine,
+        commission.OpenCommissionsSynthesisStart,
+        commission.OpenCommissionsSynthesisShow,
+        commission.OpenCommissionSynthesisYearLine,
+        batch.CreateCommissionInvoiceBatch,
+        batch.PostCommissionInvoiceBatch,
         module='commission_insurance', type_='model')
     Pool.register(
-        CreateInvoice,
-        CreateAgents,
-        ChangeBroker,
-        FilterCommissions,
-        OpenCommissionsSynthesis,
-        FilterAggregatedCommissions,
+        commission.CreateInvoice,
+        commission.CreateAgents,
+        commission.ChangeBroker,
+        commission.FilterCommissions,
+        commission.OpenCommissionsSynthesis,
+        commission.FilterAggregatedCommissions,
         module='commission_insurance', type_='wizard')
