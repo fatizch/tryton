ec8c96a38b2a0eb9bda13e44a3a9e1e444ab9a59
Pierre Jackman <pierre.jackman@coopengo.com>
2017-06-22 11:05:39 +0200

    event_log: limit access rights for event_logs (#870)
    Fix #6361

--- modules/event_log/__init__.py
+++ modules/event_log/__init__.py
@@ -1,12 +1,12 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .event_log import *
+import event_log
 
 
 def register():
     Pool.register(
-        EventLog,
-        Trigger,
-        Event,
+        event_log.EventLog,
+        event_log.Trigger,
+        event_log.Event,
         module='event_log', type_='model')
