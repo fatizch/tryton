c6c3468b79b32f478b4ca4d6ab383702d582a963
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-11 09:55:15 +0200

    bank_fr : Update init file for pep8

--- modules/bank_fr/__init__.py
+++ modules/bank_fr/__init__.py
@@ -1,14 +1,15 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .bank import *
-from .test_case import *
+
+import bank
+import test_case
 
 
 def register():
     Pool.register(
-        Bank,
-        Agency,
-        BankAccount,
-        TestCaseModel,
+        bank.Bank,
+        bank.Agency,
+        bank.BankAccount,
+        test_case.TestCaseModel,
         module='bank_fr', type_='model')
