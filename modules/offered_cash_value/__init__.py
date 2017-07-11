8f655b3626d513fd9afb2c57843756a36738e3b5
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-11 09:54:09 +0200

    offered_cash_value : Update init file for pep8

--- modules/offered_cash_value/__init__.py
+++ modules/offered_cash_value/__init__.py
@@ -1,13 +1,13 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .offered import *
+
+import offered
 
 
 def register():
     Pool.register(
-        # From file offered :
-        CashValueRule,
-        Product,
-        OptionDescription,
+        offered.CashValueRule,
+        offered.Product,
+        offered.OptionDescription,
         module='offered_cash_value', type_='model')
