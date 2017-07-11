77507582efd18e898d9602765f6a1ba516e1b34c
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-11 09:56:16 +0200

    table: update __init__ file for pep8

--- modules/table/__init__.py
+++ modules/table/__init__.py
@@ -1,25 +1,22 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import table
+import test_case
 from trytond.pool import Pool
-from .table import *
-from .test_case import *
 
 
 def register():
     Pool.register(
-        # From table
-        TableDefinition,
-        TableDefinitionDimension,
-        TableDefinitionDimensionOpenAskType,
-        TableCell,
-        TableOpen2DAskDimensions,
-        Table2D,
-        # From test_case
-        TestCaseModel,
+        table.TableDefinition,
+        table.TableDefinitionDimension,
+        table.TableDefinitionDimensionOpenAskType,
+        table.TableCell,
+        table.TableOpen2DAskDimensions,
+        table.Table2D,
+        test_case.TestCaseModel,
         module='table', type_='model')
 
     Pool.register(
-        # From table
-        TableDefinitionDimensionOpen,
-        TableOpen2D,
+        table.TableDefinitionDimensionOpen,
+        table.TableOpen2D,
         module='table', type_='wizard')
