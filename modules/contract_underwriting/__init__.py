7ab26647bc61832fb37af752ec61fdb7175b5b13
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-11 10:14:03 +0200

    contract_underwriting: update __init__ file for pep8

--- modules/contract_underwriting/__init__.py
+++ modules/contract_underwriting/__init__.py
@@ -1,26 +1,25 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import contract
+import offered
+import extra_data
+import report_engine
+import rule_engine
 from trytond.pool import Pool
-
-from .offered import *
-from .extra_data import *
-from .report_engine import *
-from .contract import *
-from .rule_engine import *
 
 
 def register():
     Pool.register(
-        UnderwritingDecision,
-        UnderwritingDecisionUnderwritingDecision,
-        Contract,
-        ContractUnderwriting,
-        ContractUnderwritingOption,
-        Product,
-        OptionDescription,
-        ExtraData,
-        ReportTemplate,
-        UnderwritingRule,
-        UnderwritingRuleUnderwritingDecision,
-        RuleEngine,
+        offered.UnderwritingDecision,
+        offered.UnderwritingDecisionUnderwritingDecision,
+        contract.Contract,
+        contract.ContractUnderwriting,
+        contract.ContractUnderwritingOption,
+        offered.Product,
+        offered.OptionDescription,
+        extra_data.ExtraData,
+        report_engine.ReportTemplate,
+        offered.UnderwritingRule,
+        offered.UnderwritingRuleUnderwritingDecision,
+        rule_engine.RuleEngine,
         module='contract_underwriting', type_='model')
