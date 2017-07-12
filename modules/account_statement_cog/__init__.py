068ef4cfe06dad45a485b32aba56f04d441c7e8e
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-12 10:00:28 +0200

    account_statement_cog: update __init__ file for pep8

--- modules/account_statement_cog/__init__.py
+++ modules/account_statement_cog/__init__.py
@@ -1,31 +1,32 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .statement import *
-from .test_case import *
-from .journal import *
-from .move import *
+
+import journal
+import move
+import statement
+import test_case
 import wizard
 import party
 
 
 def register():
     Pool.register(
-        Line,
-        Statement,
-        LineGroup,
-        TestCaseModel,
-        Journal,
-        Move,
-        MoveLine,
-        CancelLineGroupStart,
-        CancelMotive,
-        JournalCancelMotiveRelation,
+        statement.Line,
+        statement.Statement,
+        statement.LineGroup,
+        test_case.TestCaseModel,
+        journal.Journal,
+        move.Move,
+        move.MoveLine,
+        statement.CancelLineGroupStart,
+        journal.CancelMotive,
+        journal.JournalCancelMotiveRelation,
         wizard.PaymentInformations,
         wizard.StartCreateStatement,
         module='account_statement_cog', type_='model')
     Pool.register(
-        CancelLineGroup,
+        statement.CancelLineGroup,
         wizard.CreateStatement,
         party.PartyReplace,
         module='account_statement_cog', type_='wizard')
