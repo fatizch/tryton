882269b23d7aa4fa8bc14118458d5525b33fdde5
MikeSquall <michael.arbib@coopengo.com>
2017-07-12 12:12:35 +0200

    endorsement_party: update __init__ file for pep8

--- modules/endorsement_party/__init__.py
+++ modules/endorsement_party/__init__.py
@@ -1,40 +1,40 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .endorsement import *
-from .offered import *
-from .wizard import *
-from .event import *
+import endorsement
+import offered
+import wizard
+import event
 import party
 
 
 def register():
     Pool.register(
-        EndorsementDefinition,
-        EndorsementPart,
-        EndorsementPartyField,
-        EndorsementAddressField,
-        EndorsementRelationField,
-        Party,
-        Relation,
-        Address,
-        EndorsementParty,
-        EndorsementPartyAddress,
-        EndorsementPartyRelation,
-        Endorsement,
-        ChangePartyBirthDate,
-        AddressDisplayer,
-        ChangePartyAddress,
-        ChangePartySSN,
-        PartyNameDisplayer,
-        ChangePartyName,
-        ChangePartyRelationship,
-        RelationDisplayer,
-        SelectEndorsement,
-        EventTypeAction,
+        offered.EndorsementDefinition,
+        offered.EndorsementPart,
+        offered.EndorsementPartyField,
+        offered.EndorsementAddressField,
+        offered.EndorsementRelationField,
+        endorsement.Party,
+        endorsement.Relation,
+        endorsement.Address,
+        endorsement.EndorsementParty,
+        endorsement.EndorsementPartyAddress,
+        endorsement.EndorsementPartyRelation,
+        endorsement.Endorsement,
+        wizard.ChangePartyBirthDate,
+        wizard.AddressDisplayer,
+        wizard.ChangePartyAddress,
+        wizard.ChangePartySSN,
+        wizard.PartyNameDisplayer,
+        wizard.ChangePartyName,
+        wizard.ChangePartyRelationship,
+        wizard.RelationDisplayer,
+        wizard.SelectEndorsement,
+        event.EventTypeAction,
         module='endorsement_party', type_='model')
 
     Pool.register(
-        StartEndorsement,
+        wizard.StartEndorsement,
         party.PartyReplace,
         module='endorsement_party', type_='wizard')
