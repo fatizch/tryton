8e40733a39e62aca095ca2742856a1f2f8ca5d69
adrien.benduc <adrien.benduc@coopengo.com>
2017-07-12 09:46:27 +0200

    country_hexaposte: Update init file for pep8

--- modules/country_hexaposte/__init__.py
+++ modules/country_hexaposte/__init__.py
@@ -1,14 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .zipcode import *
-from .test_case import *
-from .batch import *
+import zipcode
+import test_case
+import batch
 
 
 def register():
     Pool.register(
-        Zip,
-        TestCaseModel,
-        UpdateZipCodesFromHexaPost,
+        zipcode.Zip,
+        test_case.TestCaseModel,
+        batch.UpdateZipCodesFromHexaPost,
         module='country_hexaposte', type_='model')
