bdf2e5828913decebc9aba7dde910f57d73fad68
adrien.benduc <adrien.benduc@coopengo.com>
2017-07-12 09:51:05 +0200

    claim_credit: Update init file for pep8

--- modules/claim_credit/__init__.py
+++ modules/claim_credit/__init__.py
@@ -1,12 +1,12 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .service import *
-from .benefit import *
+import service
+import benefit
 
 
 def register():
     Pool.register(
-        ClaimService,
-        BenefitRule,
+        service.ClaimService,
+        benefit.BenefitRule,
         module='claim_credit', type_='model')
