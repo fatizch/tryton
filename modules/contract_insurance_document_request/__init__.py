ee643808cc52582a03c3f15597724d73d007cd86
MikeSquall <michael.arbib@coopengo.com>
2017-07-12 12:10:56 +0200

    contract_insurance_document_request: update __init__ file for pep8

--- modules/contract_insurance_document_request/__init__.py
+++ modules/contract_insurance_document_request/__init__.py
@@ -1,27 +1,27 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .offered import *
-from .contract import *
-from .document import *
-from .report_engine import *
-from .rule_engine import *
+import offered
+import contract
+import document
+import report_engine
+import rule_engine
 
 
 def register():
     Pool.register(
-        DocumentRule,
-        ReportTemplate,
-        RuleDocumentDescriptionRelation,
-        Product,
-        OptionDescription,
-        Contract,
-        DocumentRequest,
-        DocumentReception,
-        RuleEngine,
-        DocumentRequestLine,
+        offered.DocumentRule,
+        report_engine.ReportTemplate,
+        offered.RuleDocumentDescriptionRelation,
+        offered.Product,
+        offered.OptionDescription,
+        contract.Contract,
+        document.DocumentRequest,
+        document.DocumentReception,
+        rule_engine.RuleEngine,
+        document.DocumentRequestLine,
         module='contract_insurance_document_request', type_='model')
 
     Pool.register(
-        ReceiveDocument,
+        document.ReceiveDocument,
         module='contract_insurance_document_request', type_='wizard')
