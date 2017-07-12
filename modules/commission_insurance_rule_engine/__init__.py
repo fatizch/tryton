597265a2b490bf29496f3b0e2fd9016998141ec4
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-12 10:05:01 +0200

    commission_insurance_rule_engine : Update init file for pep8

--- modules/commission_insurance_rule_engine/__init__.py
+++ modules/commission_insurance_rule_engine/__init__.py
@@ -1,18 +1,19 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .commission import *
-from .rule_engine import *
-from .extra_data import *
+
+import commission
+import rule_engine
+import extra_data
 
 
 def register():
     Pool.register(
-        Plan,
-        PlanLines,
-        Agent,
-        RuleEngineRuntime,
-        RuleEngine,
-        ExtraData,
-        CommissionPlanExtraDataRelation,
+        commission.Plan,
+        commission.PlanLines,
+        commission.Agent,
+        rule_engine.RuleEngineRuntime,
+        rule_engine.RuleEngine,
+        extra_data.ExtraData,
+        extra_data.CommissionPlanExtraDataRelation,
         module='commission_insurance_rule_engine', type_='model')
