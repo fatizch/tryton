e0de9fa4ae4c53b6cc38d51b6860c236cee777ec
adrien.benduc <adrien.benduc@coopengo.com>
2017-07-12 09:44:23 +0200

    contract_loan_invoice: Update init file for pep8

--- modules/contract_loan_invoice/__init__.py
+++ modules/contract_loan_invoice/__init__.py
@@ -1,27 +1,27 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .contract import *
-from .offered import *
-from .invoice import *
-from .future_payments import *
+import contract
+import offered
+import invoice
+import future_payments
 
 
 def register():
     Pool.register(
-        Contract,
-        ExtraPremium,
-        Premium,
-        OptionDescriptionPremiumRule,
-        OptionDescription,
-        ProductPremiumDate,
-        Product,
-        InvoiceLineDetail,
-        InvoiceLine,
-        ShowAllInvoicesMain,
-        ShowAllInvoicesLine,
+        contract.Contract,
+        contract.ExtraPremium,
+        contract.Premium,
+        offered.OptionDescriptionPremiumRule,
+        offered.OptionDescription,
+        offered.ProductPremiumDate,
+        offered.Product,
+        invoice.InvoiceLineDetail,
+        invoice.InvoiceLine,
+        future_payments.ShowAllInvoicesMain,
+        future_payments.ShowAllInvoicesLine,
         module='contract_loan_invoice', type_='model')
 
     Pool.register(
-        ShowAllInvoices,
+        future_payments.ShowAllInvoices,
         module='contract_loan_invoice', type_='wizard')
