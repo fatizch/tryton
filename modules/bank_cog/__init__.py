7c228df8459a3d532a8cfde3b293eb88d2ced338
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-12 10:03:56 +0200

    bank_cog : Update init file for pep8

--- modules/bank_cog/__init__.py
+++ modules/bank_cog/__init__.py
@@ -1,25 +1,23 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
-from .bank import *
-from .test_case import *
+
+import party
+import bank
+import test_case
 
 
 def register():
     Pool.register(
-        # From Party
-        Party,
-        SynthesisMenuBankAccoount,
-        SynthesisMenu,
-        # From Bank
-        Bank,
-        BankAccount,
-        BankAccountNumber,
-        BankAccountParty,
-        # from test_case
-        TestCaseModel,
+        party.Party,
+        party.SynthesisMenuBankAccoount,
+        party.SynthesisMenu,
+        bank.Bank,
+        bank.BankAccount,
+        bank.BankAccountNumber,
+        bank.BankAccountParty,
+        test_case.TestCaseModel,
         module='bank_cog', type_='model')
     Pool.register(
-        SynthesisMenuOpen,
+        party.SynthesisMenuOpen,
         module='bank_cog', type_='wizard')
