0a70e5068b69b6f37150ef84b96eed6cf79c266b
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-12 10:06:27 +0200

    account_payment_clearing_contract: update __init__ file for pep8

--- modules/account_payment_clearing_contract/__init__.py
+++ modules/account_payment_clearing_contract/__init__.py
@@ -1,11 +1,10 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-
-from .payment import *
+import payment
 
 
 def register():
     Pool.register(
-        Payment,
+        payment.Payment,
         module='account_payment_clearing_contract', type_='model')
