c0dc967b13c31db2b05ef067e8ced535bc0d5f6c
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-12 09:56:46 +0200

    claim_indemnification_group: update __init__ file for pep8

--- modules/claim_indemnification_group/__init__.py
+++ modules/claim_indemnification_group/__init__.py
@@ -1,34 +1,35 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .benefit import *
-from .contract import *
-from .rule_engine import *
-from .claim import *
-from .wizard import *
+
+import benefit
+import claim
+import contract
+import rule_engine
+import wizard
 
 
 def register():
     Pool.register(
-        Benefit,
-        BenefitCompanyProduct,
-        BenefitRule,
-        BenefitRuleIndemnification,
-        BenefitRuleDeductible,
-        BenefitRuleRevaluation,
-        Option,
-        OptionVersion,
-        OptionBenefit,
-        RuleEngineRuntime,
-        ClaimService,
-        Indemnification,
-        IndemnificationDefinition,
-        TransferServicesContracts,
-        TransferServicesBenefits,
-        TransferServicesBenefitLine,
+        benefit.Benefit,
+        benefit.BenefitCompanyProduct,
+        benefit.BenefitRule,
+        benefit.BenefitRuleIndemnification,
+        benefit.BenefitRuleDeductible,
+        benefit.BenefitRuleRevaluation,
+        contract.Option,
+        contract.OptionVersion,
+        contract.OptionBenefit,
+        rule_engine.RuleEngineRuntime,
+        claim.ClaimService,
+        claim.Indemnification,
+        wizard.IndemnificationDefinition,
+        wizard.TransferServicesContracts,
+        wizard.TransferServicesBenefits,
+        wizard.TransferServicesBenefitLine,
         module='claim_indemnification_group', type_='model')
 
     Pool.register(
-        CreateIndemnification,
-        TransferServices,
+        wizard.CreateIndemnification,
+        wizard.TransferServices,
         module='claim_indemnification_group', type_='wizard')
