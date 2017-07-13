60bc7eb839f5ae3e5585c64cbf9b9f24015f03b3
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:33:54 +0200

    commission_multi_agents: update init file for pep8

--- modules/commission_multi_agents/__init__.py
+++ modules/commission_multi_agents/__init__.py
@@ -1,19 +1,19 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .commission import *
-from .invoice import *
-from .contract import *
+import commission
+import invoice
+import contract
 
 
 def register():
     Pool.register(
-        Commission,
-        Agent,
-        AgentAgentRelation,
-        Plan,
-        PlanAgentRelation,
-        InvoiceLine,
-        Contract,
-        ContractOption,
+        commission.Commission,
+        commission.Agent,
+        commission.AgentAgentRelation,
+        commission.Plan,
+        commission.PlanAgentRelation,
+        invoice.InvoiceLine,
+        contract.Contract,
+        contract.ContractOption,
         module='commission_multi_agents', type_='model')
