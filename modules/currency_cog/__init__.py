7fad9ed405ce98bca2fc6217cf8733d42ef3acc6
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-13 10:21:00 +0200

    currency_cog : Update init file for pep8

--- modules/currency_cog/__init__.py
+++ modules/currency_cog/__init__.py
@@ -1,16 +1,20 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .currency import *
-from .model import *
-from .test_case import *
+
+import currency
+import test_case
+
+from model import ModelCurrency
+
+__all__ = [
+    'ModelCurrency',
+    ]
 
 
 def register():
     Pool.register(
-        # From file currency
-        Currency,
-        CurrencyRate,
-        # From file test_case
-        TestCaseModel,
+        currency.Currency,
+        currency.CurrencyRate,
+        test_case.TestCaseModel,
         module='currency_cog', type_='model')
