179640b5aae223a964791f1d530e18d804c45ea9
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 10:06:27 +0200

    account_payment_dunning: update init file for pep8

--- modules/account_payment_dunning/__init__.py
+++ modules/account_payment_dunning/__init__.py
@@ -1,13 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .payment import *
+
+import payment
 
 
 def register():
     Pool.register(
-        PaymentJournal,
-        JournalFailureAction,
-        JournalFailureDunning,
-        Payment,
+        payment.PaymentJournal,
+        payment.JournalFailureAction,
+        payment.JournalFailureDunning,
+        payment.Payment,
         module='account_payment_dunning', type_='model')
