59eec26b508ebe59862188bdfa9855358e58e988
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:38:17 +0200

    claim_health: update init file for pep8

--- modules/claim_health/__init__.py
+++ modules/claim_health/__init__.py
@@ -1,26 +1,26 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .claim import *
-from .contract import *
-from .wizard import *
-from .benefit import *
+import claim
+import contract
+import wizard
+import benefit
 import party
 
 
 def register():
     Pool.register(
-        LossDescription,
-        MedicalActFamily,
-        MedicalActDescription,
-        HealthLoss,
-        Loss,
-        Claim,
-        ClaimService,
-        CoveredElement,
-        ModifyCoveredElement,
-        CoveredElementDisplayer,
-        ChangeContractSubscriber,
+        benefit.LossDescription,
+        benefit.MedicalActFamily,
+        benefit.MedicalActDescription,
+        claim.HealthLoss,
+        claim.Loss,
+        claim.Claim,
+        claim.ClaimService,
+        contract.CoveredElement,
+        wizard.ModifyCoveredElement,
+        wizard.CoveredElementDisplayer,
+        wizard.ChangeContractSubscriber,
         module='claim_health', type_='model')
     Pool.register(
         party.PartyReplace,
