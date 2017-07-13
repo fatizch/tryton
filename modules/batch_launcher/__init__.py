4f5755e12b90d5726d88164fce05559896cf5a6e
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:40:12 +0200

    batch_launcher: update init file for pep8

--- modules/batch_launcher/__init__.py
+++ modules/batch_launcher/__init__.py
@@ -1,14 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .batch_launcher import *
+import batch_launcher
 
 
 def register():
     Pool.register(
-        SelectBatch,
-        BatchParameter,
+        batch_launcher.SelectBatch,
+        batch_launcher.BatchParameter,
         module='batch_launcher', type_='model')
     Pool.register(
-        LaunchBatch,
+        batch_launcher.LaunchBatch,
         module='batch_launcher', type_='wizard')
