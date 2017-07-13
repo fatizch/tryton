1c76bdf2e2c7fefa895d2757df30950b5cf88dae
MikeSquall <michael.arbib@coopengo.com>
2017-07-13 10:21:19 +0200

    contract_coverage_amount_revaluation: update __init__ file for pep8

--- modules/contract_coverage_amount_revaluation/__init__.py
+++ modules/contract_coverage_amount_revaluation/__init__.py
@@ -1,16 +1,16 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .rule_engine import *
-from .offered import *
-from .contract import *
+import rule_engine
+import offered
+import contract
 
 
 def register():
     Pool.register(
-        RuleEngine,
-        OptionDescription,
-        CoverageAmountRule,
-        Contract,
-        ContractOption,
+        rule_engine.RuleEngine,
+        offered.OptionDescription,
+        offered.CoverageAmountRule,
+        contract.Contract,
+        contract.ContractOption,
         module='contract_coverage_amount_revaluation', type_='model')
