266a8301a00855bc299bfd9b25d0d695ba6c0441
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 10:04:54 +0200

    claim_eligibility: update init file for pep8

--- modules/claim_eligibility/__init__.py
+++ modules/claim_eligibility/__init__.py
@@ -1,21 +1,22 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .benefit import *
-from .claim import *
-from .wizard import *
+
+import benefit
+import claim
+import wizard
 
 
 def register():
     Pool.register(
-        Benefit,
-        BenefitEligibilityRule,
-        Claim,
-        ClaimService,
-        ClaimIndemnification,
-        ExtraData,
+        benefit.Benefit,
+        benefit.BenefitEligibilityRule,
+        claim.Claim,
+        claim.ClaimService,
+        claim.ClaimIndemnification,
+        claim.ExtraData,
         module='claim_eligibility', type_='model')
     Pool.register(
-        ManualValidationEligibility,
-        ManualRejectionEligibility,
+        wizard.ManualValidationEligibility,
+        wizard.ManualRejectionEligibility,
         module='claim_eligibility', type_='wizard')
