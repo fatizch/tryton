70434bf37dbb3309d5d6fb58687de62e87505fbe
MikeSquall <michael.arbib@coopengo.com>
2017-07-13 10:20:35 +0200

    claim_group_process: update __init__ file for pep8

--- modules/claim_group_process/__init__.py
+++ modules/claim_group_process/__init__.py
@@ -1,13 +1,13 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .claim import *
+import claim
 
 
 def register():
     Pool.register(
-        ClaimDeclareFindProcess,
+        claim.ClaimDeclareFindProcess,
         module='claim_group_process', type_='model')
     Pool.register(
-        ClaimDeclare,
+        claim.ClaimDeclare,
         module='claim_group_process', type_='wizard')
