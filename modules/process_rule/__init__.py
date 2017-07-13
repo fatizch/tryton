071d05885b8a594d711c27e5d1ff7af2da809e64
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-13 10:22:27 +0200

    process_rule : Update init file for pep8

--- modules/process_rule/__init__.py
+++ modules/process_rule/__init__.py
@@ -1,12 +1,13 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .rule_engine import *
-from .process import *
+
+import rule_engine
+import process
 
 
 def register():
     Pool.register(
-        RuleEngine,
-        ProcessAction,
+        rule_engine.RuleEngine,
+        process.ProcessAction,
         module='process_rule', type_='model')
