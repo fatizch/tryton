be030144d1f5fc8a450cdf9377af1845623bb479
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:14:52 +0200

    claim_life_process: update init file for pep8

--- modules/claim_life_process/__init__.py
+++ modules/claim_life_process/__init__.py
@@ -1,11 +1,10 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .claim import *
+import claim
 
 
 def register():
     Pool.register(
-        # From claim
-        Claim,
+        claim.Claim,
         module='claim_life_process', type_='model')
