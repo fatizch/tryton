bda367102bf8ef1203bc39200260c1a7121ab1fe
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:13:42 +0200

    contract_group_process: update init file for pep8

--- modules/contract_group_process/__init__.py
+++ modules/contract_group_process/__init__.py
@@ -1,19 +1,19 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .wizard import *
-from .process import *
-from .document import *
+import document
+import process
+import wizard
 
 
 def register():
     Pool.register(
-        ContractSubscribeFindProcess,
-        ContractGroupSubscribeFindProcess,
-        DocumentDescription,
+        process.ContractSubscribeFindProcess,
+        wizard.ContractGroupSubscribeFindProcess,
+        document.DocumentDescription,
         module='contract_group_process', type_='model')
     Pool.register(
-        ContractSubscribe,
-        ContractGroupSubscribe,
-        ReceiveDocument,
+        process.ContractSubscribe,
+        wizard.ContractGroupSubscribe,
+        document.ReceiveDocument,
         module='contract_group_process', type_='wizard')
