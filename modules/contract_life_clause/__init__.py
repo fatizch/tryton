880c67c27a29cd95a6df7cff3499236b9fee1805
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 10:14:51 +0200

    contract_life_clause: update init file for pep8

--- modules/contract_life_clause/__init__.py
+++ modules/contract_life_clause/__init__.py
@@ -1,15 +1,15 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .contract import *
+import contract
 import party
 
 
 def register():
     Pool.register(
-        Contract,
-        ContractOption,
-        Beneficiary,
+        contract.Contract,
+        contract.ContractOption,
+        contract.Beneficiary,
         module='contract_life_clause', type_='model')
     Pool.register(
         party.PartyReplace,
