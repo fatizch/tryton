a4e115b6b019e3275bc67affb819861acc4ebbe6
MikeSquall <michael.arbib@coopengo.com>
2017-07-13 10:21:39 +0200

    contract_set_insurance_invoice: update __init__ file for pep8

--- modules/contract_set_insurance_invoice/__init__.py
+++ modules/contract_set_insurance_invoice/__init__.py
@@ -1,24 +1,24 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .contract import *
-from .account import *
-from .wizard import *
-from .dunning import *
-from .move import *
-from .payment import *
+import contract
+import account
+import wizard
+import dunning
+import move
+import payment
 
 
 def register():
     Pool.register(
-        Contract,
-        ContractSet,
-        Fee,
-        Level,
-        PartyBalance,
-        Payment,
+        contract.Contract,
+        contract.ContractSet,
+        account.Fee,
+        dunning.Level,
+        move.PartyBalance,
+        payment.Payment,
         module='contract_set_insurance_invoice', type_='model')
     Pool.register(
-        DisplayContractSetPremium,
-        Renew,
+        contract.DisplayContractSetPremium,
+        wizard.Renew,
         module='contract_set_insurance_invoice', type_='wizard')
