b99d01731931eeb0f8cff812466aaab25bba8de9
adrien.benduc <adrien.benduc@coopengo.com>
2017-07-13 10:33:07 +0200

    contract_term_renewal: Update init file for pep8

--- modules/contract_term_renewal/__init__.py
+++ modules/contract_term_renewal/__init__.py
@@ -1,28 +1,28 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .rule_engine import *
-from .offered import *
-from .contract import *
-from .batch import *
-from .report_engine import *
-from .wizard import *
+import rule_engine
+import offered
+import contract
+import batch
+import report_engine
+import wizard
 
 
 def register():
     Pool.register(
-        RuleEngine,
-        Product,
-        ProductTermRenewalRule,
-        ActivationHistory,
-        Contract,
-        SelectDeclineRenewalReason,
-        RenewContracts,
-        ReportTemplate,
-        TerminateContract,
-        ConfirmRenew,
+        rule_engine.RuleEngine,
+        offered.Product,
+        offered.ProductTermRenewalRule,
+        contract.ActivationHistory,
+        contract.Contract,
+        contract.SelectDeclineRenewalReason,
+        batch.RenewContracts,
+        report_engine.ReportTemplate,
+        wizard.TerminateContract,
+        contract.ConfirmRenew,
         module='contract_term_renewal', type_='model')
     Pool.register(
-        DeclineRenewal,
-        Renew,
+        contract.DeclineRenewal,
+        contract.Renew,
         module='contract_term_renewal', type_='wizard')
