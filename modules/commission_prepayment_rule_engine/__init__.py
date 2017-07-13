74a237744ff6a4bba3069413ecdd45e329d21184
MikeSquall <michael.arbib@coopengo.com>
2017-07-13 10:20:58 +0200

    commission_prepayment_rule_engine: update __init__ file for pep8

--- modules/commission_prepayment_rule_engine/__init__.py
+++ modules/commission_prepayment_rule_engine/__init__.py
@@ -1,14 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .commission import *
-from .rule_engine import *
+import commission
+import rule_engine
 
 
 def register():
     Pool.register(
-        PrepaymentPaymentDateRule,
-        Plan,
-        PlanLines,
-        RuleEngineRuntime,
+        commission.PrepaymentPaymentDateRule,
+        commission.Plan,
+        commission.PlanLines,
+        rule_engine.RuleEngineRuntime,
         module='commission_prepayment_rule_engine', type_='model')
