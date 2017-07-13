e2e356cb1930386f4e63116a08ef89c3d60adc90
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-13 10:11:23 +0200

    contract_insurance_payment : Update init file for pep8

--- modules/contract_insurance_payment/__init__.py
+++ modules/contract_insurance_payment/__init__.py
@@ -2,26 +2,26 @@
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
 
-from .payment import *
-from .configuration import *
-from .offered import *
-from .move import *
-from .event import *
-from .contract import *
-from .report import *
+import payment
+import configuration
+import offered
+import move
+import event
+import contract
+import report
 
 
 def register():
     Pool.register(
-        Payment,
-        Journal,
-        Configuration,
-        Product,
-        JournalFailureAction,
-        BillingMode,
-        MoveLine,
-        EventLog,
-        Contract,
-        ReportTemplate,
-        MergedPaymentsByContracts,
+        payment.Payment,
+        payment.Journal,
+        configuration.Configuration,
+        offered.Product,
+        payment.JournalFailureAction,
+        offered.BillingMode,
+        move.MoveLine,
+        event.EventLog,
+        contract.Contract,
+        report.ReportTemplate,
+        payment.MergedPaymentsByContracts,
         module='contract_insurance_payment', type_='model')
