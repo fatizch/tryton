9d544ca8db072b1dbe47c134322a24d6a2ae7a69
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 10:10:19 +0200

    claim_process: update init file for pep8

--- modules/claim_process/__init__.py
+++ modules/claim_process/__init__.py
@@ -1,23 +1,23 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .claim import *
-from .document import *
+
+import claim
+import document
 
 
 def register():
     Pool.register(
-        # From claim
-        Claim,
-        Process,
-        ClaimDeclarationElement,
-        ProcessLossDescRelation,
-        ClaimDeclareFindProcess,
-        DocumentDescription,
+        claim.Claim,
+        claim.Process,
+        claim.ClaimDeclarationElement,
+        claim.ProcessLossDescRelation,
+        claim.ClaimDeclareFindProcess,
+        document.DocumentDescription,
         module='claim_process', type_='model')
 
     Pool.register(
-        CloseClaim,
-        ClaimDeclare,
-        ReceiveDocument,
+        claim.CloseClaim,
+        claim.ClaimDeclare,
+        document.ReceiveDocument,
         module='claim_process', type_='wizard')
