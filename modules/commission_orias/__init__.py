de66f5a147666cbacf94489bc066db1e2a57c10f
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:39:35 +0200

    commission_orias: update init file for pep8

--- modules/commission_orias/__init__.py
+++ modules/commission_orias/__init__.py
@@ -1,11 +1,11 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
+import party
 
 
 def register():
     Pool.register(
-        Party,
-        PartyIdentifier,
+        party.Party,
+        party.PartyIdentifier,
         module='commission_orias', type_='model')
