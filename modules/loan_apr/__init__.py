6735d9b186d397e16476620d3bbf8e694a7e63d7
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-13 17:46:04 +0200

    loan_apr : Update init file for pep8

--- modules/loan_apr/__init__.py
+++ modules/loan_apr/__init__.py
@@ -1,23 +1,24 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .offered import *
-from .contract import *
-from .loan import *
+
+import offered
+import contract
+import loan
 
 
 def register():
     Pool.register(
-        LoanAveragePremiumRule,
-        FeeRule,
-        Product,
-        Contract,
-        LoanShare,
-        Loan,
-        AveragePremiumRateLoanDisplayer,
-        DisplayLoanAveragePremiumValues,
+        offered.LoanAveragePremiumRule,
+        offered.FeeRule,
+        offered.Product,
+        contract.Contract,
+        contract.LoanShare,
+        loan.Loan,
+        contract.AveragePremiumRateLoanDisplayer,
+        contract.DisplayLoanAveragePremiumValues,
         module='loan_apr', type_='model')
 
     Pool.register(
-        DisplayLoanAveragePremium,
+        contract.DisplayLoanAveragePremium,
         module='loan_apr', type_='wizard')
