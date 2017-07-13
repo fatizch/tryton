d277d307e353ec6dfbd49f844b211ad16ec99738
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-13 17:04:10 +0200

    party_interlocutor: update init file for pep8

--- modules/party_interlocutor/__init__.py
+++ modules/party_interlocutor/__init__.py
@@ -1,14 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .interlocutor import *
+import interlocutor
 import party
 
 
 def register():
     Pool.register(
-        ContactInterlocutor,
-        PartyContactInterlocutorPartyContactMechanism,
+        interlocutor.ContactInterlocutor,
+        interlocutor.PartyContactInterlocutorPartyContactMechanism,
         module='party_interlocutor', type_='model')
     Pool.register(
         party.PartyReplace,
