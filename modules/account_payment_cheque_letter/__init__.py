5382dc1bc81cf66fa314512e581c1d852afe6a62
MikeSquall <michael.arbib@coopengo.com>
2017-07-17 15:38:55 +0200

    account_payment_cheque_letter: update __init__ file for pep8

--- modules/account_payment_cheque_letter/__init__.py
+++ modules/account_payment_cheque_letter/__init__.py
@@ -1,23 +1,23 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .payment import *
-from .report import *
-from .report_engine import *
+import payment
+import report
+import report_engine
 
 
 def register():
     Pool.register(
-        Payment,
-        MergedPayments,
-        Journal,
-        Group,
-        ProcessPaymentStart,
-        ReportTemplate,
+        payment.Payment,
+        payment.MergedPayments,
+        payment.Journal,
+        payment.Group,
+        payment.ProcessPaymentStart,
+        report.ReportTemplate,
         module='account_payment_cheque_letter', type_='model')
     Pool.register(
-        ProcessPayment,
+        payment.ProcessPayment,
         module='account_payment_cheque_letter', type_='wizard')
     Pool.register(
-        ReportGenerate,
+        report_engine.ReportGenerate,
         module='account_payment_cheque_letter', type_='report')
