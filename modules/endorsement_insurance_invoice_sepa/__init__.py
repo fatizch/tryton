08f657f4f2942c807eb7f031959959182dbeeedb
MikeSquall <michael.arbib@coopengo.com>
2017-07-17 15:38:55 +0200

    endorsement_insurance_invoice_sepa: update __init__ file for pep8

--- modules/endorsement_insurance_invoice_sepa/__init__.py
+++ modules/endorsement_insurance_invoice_sepa/__init__.py
@@ -1,14 +1,14 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .endorsement import *
-from .wizard import *
+import endorsement
+import wizard
 
 
 def register():
     Pool.register(
-        Contract,
-        Endorsement,
-        ChangeBillingInformation,
-        ChangeDirectDebitAccount,
+        endorsement.Contract,
+        endorsement.Endorsement,
+        wizard.ChangeBillingInformation,
+        wizard.ChangeDirectDebitAccount,
         module='endorsement_insurance_invoice_sepa', type_='model')
