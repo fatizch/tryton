86f31491a194148ced30afe2975e14fe69a8a652
MikeSquall <michael.arbib@coopengo.com>
2017-07-17 15:38:56 +0200

    endorsement_premium: update __init__ file for pep8

--- modules/endorsement_premium/__init__.py
+++ modules/endorsement_premium/__init__.py
@@ -1,20 +1,20 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .endorsement import *
-from .wizard import *
+import endorsement
+import wizard
 
 
 def register():
     Pool.register(
-        ContractFee,
-        Premium,
-        EndorsementContract,
-        PreviewContractPremiums,
-        ContractPreview,
-        ContractPreviewPremium,
+        endorsement.ContractFee,
+        endorsement.Premium,
+        endorsement.EndorsementContract,
+        wizard.PreviewContractPremiums,
+        wizard.ContractPreview,
+        wizard.ContractPreviewPremium,
         module='endorsement_premium', type_='model')
 
     Pool.register(
-        StartEndorsement,
+        wizard.StartEndorsement,
         module='endorsement_premium', type_='wizard')
