ced3a9a80742f1dc4d028af915d21b07895c5d10
adrien.benduc <adrien.benduc@coopengo.com>
2017-07-17 15:46:58 +0200

    commission_waiting_cog: Fix init file for pep8

--- modules/commission_waiting_cog/__init__.py
+++ modules/commission_waiting_cog/__init__.py
@@ -1,17 +1,17 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .commission import *
-from .invoice import *
+import commission
+import invoice
 
 
 def register():
     Pool.register(
-        Commission,
-        Agent,
-        CreateAgentsAsk,
-        InvoiceLine,
+        commission.Commission,
+        commission.Agent,
+        commission.CreateAgentsAsk,
+        invoice.InvoiceLine,
         module='commission_waiting_cog', type_='model')
     Pool.register(
-        CreateAgents,
+        commission.CreateAgents,
         module='commission_waiting_cog', type_='wizard')
