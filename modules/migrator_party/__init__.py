3a6428628fe74cc2ab6e08e9095c42ec15a3710c
Frederic Langevin <frederic.langevin@coopengo.com>
2017-07-17 11:22:07 +0200

    migrator_party: Update init file for pep8

--- modules/migrator_party/__init__.py
+++ modules/migrator_party/__init__.py
@@ -2,16 +2,16 @@
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
 
-from .party import *
-from .address import *
+import party
+import address
 
 
 def register():
     Pool.register(
-        MigratorCountry,
-        MigratorParty,
-        MigratorAddress,
-        MigratorContact,
-        MigratorZip,
-        MigratorPartyRelation,
+        address.MigratorCountry,
+        party.MigratorParty,
+        address.MigratorAddress,
+        address.MigratorContact,
+        address.MigratorZip,
+        party.MigratorPartyRelation,
         module='migrator_party', type_='model')
