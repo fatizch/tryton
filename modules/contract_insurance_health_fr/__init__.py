1b6b2b4015ad0383958b04d04741c78f1f609e37
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-18 09:53:30 +0200

    contract_insurance_health_fr : Update init file for pep8

--- modules/contract_insurance_health_fr/__init__.py
+++ modules/contract_insurance_health_fr/__init__.py
@@ -1,22 +1,23 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .health import *
-from .party import *
-from .contract import *
-from .rule_engine import *
-from .test_case import *
+
+import health
+import party
+import contract
+import rule_engine
+import test_case
 
 
 def register():
     Pool.register(
-        Party,
-        PartyRelation,
-        HealthCareSystem,
-        InsuranceFund,
-        HealthPartyComplement,
-        CoveredElement,
-        Contract,
-        RuleEngineRuntime,
-        TestCaseModel,
+        party.Party,
+        party.PartyRelation,
+        health.HealthCareSystem,
+        health.InsuranceFund,
+        party.HealthPartyComplement,
+        contract.CoveredElement,
+        contract.Contract,
+        rule_engine.RuleEngineRuntime,
+        test_case.TestCaseModel,
         module='contract_insurance_health_fr', type_='model')
