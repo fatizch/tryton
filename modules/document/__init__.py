5f0e1aaa7f0d2868881a597fb8fb23eea2fa3fb5
Jean Cavallo <jean.cavallo@coopengo.com>
2017-07-18 09:48:29 +0200

    document : Update init file for pep8

--- modules/document/__init__.py
+++ modules/document/__init__.py
@@ -1,19 +1,20 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .document import *
-from .attachment import *
+
+import document
+import attachment
 
 
 def register():
     Pool.register(
-        DocumentDescription,
-        DocumentDescGroup,
-        DocumentReception,
-        Attachment,
-        ReattachDocument,
+        document.DocumentDescription,
+        document.DocumentDescGroup,
+        document.DocumentReception,
+        attachment.Attachment,
+        document.ReattachDocument,
         module='document', type_='model')
 
     Pool.register(
-        ReceiveDocument,
+        document.ReceiveDocument,
         module='document', type_='wizard')
