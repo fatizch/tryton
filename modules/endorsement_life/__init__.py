f0f41c7463b1575cb108e2c67f5365c821860ba8
MikeSquall <michael.arbib@coopengo.com>
2017-07-18 10:33:36 +0200

    endorsement_life: update __init__ file for pep8

--- modules/endorsement_life/__init__.py
+++ modules/endorsement_life/__init__.py
@@ -1,24 +1,24 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .offered import *
-from .endorsement import *
-from .wizard import *
+import offered
+import endorsement
+import wizard
 
 
 def register():
     Pool.register(
-        EndorsementPart,
-        EndorsementBeneficiaryField,
-        Beneficiary,
-        EndorsementContract,
-        EndorsementCoveredElementOption,
-        EndorsementBeneficiary,
-        ManageBeneficiaries,
-        ManageBeneficiariesOptionDisplayer,
-        ManageBeneficiariesDisplayer,
+        offered.EndorsementPart,
+        offered.EndorsementBeneficiaryField,
+        endorsement.Beneficiary,
+        endorsement.EndorsementContract,
+        endorsement.EndorsementCoveredElementOption,
+        endorsement.EndorsementBeneficiary,
+        wizard.ManageBeneficiaries,
+        wizard.ManageBeneficiariesOptionDisplayer,
+        wizard.ManageBeneficiariesDisplayer,
         module='endorsement_life', type_='model')
 
     Pool.register(
-        StartEndorsement,
+        wizard.StartEndorsement,
         module='endorsement_life', type_='wizard')
