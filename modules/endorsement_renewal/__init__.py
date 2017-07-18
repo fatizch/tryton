5886311e6e49876728790ddddb5d4645a949bfd9
MikeSquall <michael.arbib@coopengo.com>
2017-07-18 10:11:53 +0200

    endorsement_renewal: update __init__ file for pep8

--- modules/endorsement_renewal/__init__.py
+++ modules/endorsement_renewal/__init__.py
@@ -1,15 +1,15 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .wizard import *
-from .contract import *
+import wizard
+import contract
 
 
 def register():
     Pool.register(
-        ContractRenew,
-        Contract,
+        wizard.ContractRenew,
+        contract.Contract,
         module='endorsement_renewal', type_='model')
     Pool.register(
-        StartEndorsement,
+        wizard.StartEndorsement,
         module='endorsement_renewal', type_='wizard')
