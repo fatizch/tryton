bac6d470492eef5a9526f8c1ed88e40b332aa6d9
MikeSquall <MikeSquall@users.noreply.github.com>
2017-09-28 12:01:24 +0200

    async: allow to set job timeout from an env variable (#1142)
    Fix #7165

--- bin/lib/async/config.py
+++ bin/lib/async/config.py
@@ -1,3 +1,9 @@
-JOB_TIMEOUT = 60 * 60              # job exec time <= 1h
+import os
+
+JOB_TIMEOUT = 60 * 60              # job exec time <= 1h is default
+if 'COOG_JOB_TIMEOUT' in os.environ:
+    # job custom exec time in seconds
+    JOB_TIMEOUT = int(os.environ['COOG_JOB_TIMEOUT'])
+
 JOB_TTL = 24 * 60 * 60             # job queue wait time <= 1d
 JOB_RESULT_TTL = 5 * 24 * 60 * 60  # job result wait time <= 5d
