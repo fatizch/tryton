4c49c208fca14767b661374213f3b71719fb71dc
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2017-09-29 09:50:39 +0200

    claim_life_process: Add importable std process in JSON directory (#1140)
    Fix #7149

--- modules/claim_life_process/__init__.py
+++ modules/claim_life_process/__init__.py
@@ -2,9 +2,11 @@
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
 import claim
+import wizard
 
 
 def register():
     Pool.register(
         claim.Claim,
+        wizard.ImportProcessSelect,
         module='claim_life_process', type_='model')
---
+++ modules/claim_life_process/wizard.py
@@ -0,0 +1,36 @@
+# This file is part of Coog. The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import PoolMeta
+
+
+__all__ = [
+    'ImportProcessSelect',
+    ]
+
+
+class ImportProcessSelect:
+    __metaclass__ = PoolMeta
+    __name__ = 'import.process.select'
+
+    @classmethod
+    def __setup__(cls):
+        super(ImportProcessSelect, cls).__setup__()
+        cls._error_messages.update({
+                'std_declaration_process': 'STD Declaration Process (FR)',
+                'std_declaration_process_description': 'This process allows '
+                'managers to make a STD declaration',
+                })
+
+    def available_processes(self):
+        return super(ImportProcessSelect, self).available_processes() + [
+            {
+                'name': self.raise_user_error(
+                    'std_declaration_process', raise_exception=False),
+                'path':
+                'claim_life_process/json/process_std_declaration_fr.json',
+                'description': self.raise_user_error(
+                    'std_declaration_process_description',
+                    raise_exception=False),
+                },
+            ]
