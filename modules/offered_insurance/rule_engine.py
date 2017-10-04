e8b63c7f959c63a2fe041ef45567cdfbc09ca3bc
Frederic Langevin <frederic.langevin@coopengo.com>
2017-10-04 15:16:03 +0200

    Add description rule engine fonction (#1145)
    * all: add long description for each rule engine fonction
    
    Fix #7167
    
    * fix typo
    
    * Fix following jeremy review

--- modules/offered_insurance/rule_engine.py
+++ modules/offered_insurance/rule_engine.py
@@ -262,61 +262,6 @@ class RuleEngineRuntime:
         cls.append_error(args, 'deprecated_method')
 
     @classmethod
-    @check_args('price_details')
-    def _re_get_sub_component(cls, args, the_code):
-        for detail in args['price_details']:
-            if detail.on_object.code == the_code:
-                return detail.amount
-        cls.append_error(args, 'Inexisting code : %s' % the_code)
-
-    @classmethod
-    @check_args('price_details', 'final_details')
-    def _re_append_detail(cls, args, the_code, amount):
-        found = False
-        for detail in args['price_details']:
-            if not detail.on_object.code == the_code:
-                continue
-            args['final_details'][the_code] = (amount +
-                args['final_details'].get(the_code, (0,))[0], detail)
-            found = True
-            break
-        if not found:
-            cls.append_error(args, 'Undefined Code %s' % the_code)
-            return
-
-    @classmethod
-    @check_args('price_details')
-    def _re_apply_tax(cls, args, code, base):
-        tax = None
-        for detail in args['price_details']:
-            if detail.on_object.kind == 'tax':
-                if detail.on_object.code == code:
-                    tax = detail.on_object.tax
-                    break
-        if tax is None:
-            cls.append_error(args, 'Undefined Tax %s' % code)
-            return
-
-        tax_vers = tax.get_version_at_date(args['date'])
-        return tax_vers.apply_tax(base)
-
-    @classmethod
-    @check_args('price_details')
-    def _re_apply_fee(cls, args, code, base):
-        fee = None
-        for detail in args['price_details']:
-            if detail.on_object.kind == 'fee':
-                if detail.on_object.code == code:
-                    fee = detail.on_object.fee
-                    break
-        if fee is None:
-            cls.append_error(args, 'Undefined Fee %s' % code)
-            return
-
-        fee_vers = fee.get_version_at_date(args['date'])
-        return fee_vers.apply_fee(base)
-
-    @classmethod
     @check_args('option')
     def _re_option_code(cls, args):
         return args['option'].coverage.code
