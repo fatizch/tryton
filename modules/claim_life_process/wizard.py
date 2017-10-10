db9fe9ed3c56d866cf205f442833eac0a2be5157
manalaboutayeb <manal.aboutayeb@coopengo.com>
2017-10-10 17:35:20 +0200

    claim_life_process: add afterpart - new period process to initial configuration (#1179)
    Ref #7211

--- modules/claim_life_process/wizard.py
+++ modules/claim_life_process/wizard.py
@@ -36,7 +36,12 @@ class ImportProcessSelect:
                 'Process (FR)',
                 'death_std_declaration_process_description': 'This process '
                 'allows managers to reopen a std claim to make a death '
-                'declaration'
+                'declaration',
+                'std_continue_new_period_process': 'STD Continue/New Period '
+                'Process (FR)',
+                'std_continue_new_period_process_description': 'This process '
+                'allows managers to continue a terminated STD declaration and '
+                'add new scheduling periods',
                 })
 
     def available_processes(self):
@@ -114,4 +119,19 @@ class ImportProcessSelect:
                 utils.is_module_installed('process_rule') and
                 utils.is_module_installed('claim_eligibility')
                 },
+            {
+                'name': self.raise_user_error(
+                    'std_continue_new_period_process', raise_exception=False),
+                'path':
+                'claim_life_process/json/process_std_continue_new_period_fr.'
+                'json',
+                'description': self.raise_user_error(
+                    'std_continue_new_period_process_description',
+                    raise_exception=False),
+                'is_visible': utils.is_module_installed('claim_salary_fr') and
+                utils.is_module_installed('claim_group_process') and
+                utils.is_module_installed('underwriting_claim') and
+                utils.is_module_installed('process_rule') and
+                utils.is_module_installed('claim_eligibility'),
+                }
             ]
