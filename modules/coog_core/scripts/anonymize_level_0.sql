aaf09682cc458d633aa4d808e8b1438648a24408
PierreCookie <pierre.kunkel@coopengo.com>
2018-05-03 14:47:56 +0200

    coog_core: Remove bank anonymisation and add where script (#1781)

--- modules/coog_core/scripts/anonymize_level_0.sql
+++ modules/coog_core/scripts/anonymize_level_0.sql
@@ -1,7 +1,6 @@
 /* level 0 */
 DO $$
 BEGIN
-/* first argument is table name, second argument is a list of field to md5, third argument a list of fields to set null */
-PERFORM anon_table('contract', 'contract_number, quote_number', '');
-PERFORM anon_table('party_party', 'code', '');
+PERFORM anon_table('contract', 'contract_number, quote_number');
+PERFORM anon_table('party_party', 'code');
 END $$;
