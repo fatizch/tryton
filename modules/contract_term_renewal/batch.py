d78569cae46f070526c197c9a9a1f21c08210931
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-05-11 10:22:04 +0200

    contract_term_renewal: Fix batch query (#1818)
    The previous version used the end_date field, which is problematic since
    it depends on "today". If the batch is launched with a few days of
    anticipation, it will renew the same contract multiple times since.
    
    Fix #8924

--- modules/contract_term_renewal/batch.py
+++ modules/contract_term_renewal/batch.py
@@ -1,9 +1,15 @@
 # -*- coding:utf-8 -*-
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import datetime
 import logging
 
+from sql import Literal, Window
+from sql.conditionals import Coalesce
+from sql.aggregate import Max
+
 from trytond.pool import Pool
+from trytond.transaction import Transaction
 from trytond.modules.coog_core import batch
 
 
@@ -19,21 +25,32 @@ class RenewContracts(batch.BatchRoot):
         return 'contract'
 
     @classmethod
-    def get_batch_search_model(cls):
-        return 'contract'
-
-    @classmethod
-    def get_batch_domain(cls, treatment_date):
+    def select_ids(cls, treatment_date):
         pool = Pool()
+        cursor = Transaction().connection.cursor()
         Product = pool.get('offered.product')
-        renewable_products = Product.search([
-                ('term_renewal_rule.allow_renewal', '=', True)])
-        return [
-            ('status', '=', 'active'),
-            ('end_date', '!=', None),
-            ('product', 'in', renewable_products),
-            ('activation_history.final_renewal', '!=', True),
-            ('end_date', '<=', treatment_date)]
+        renewable_products = [x.id for x in Product.search([
+                    ('term_renewal_rule.allow_renewal', '=', True)])]
+        contract = pool.get('contract').__table__()
+        activation_history = pool.get('contract.activation_history'
+            ).__table__()
+        win_query = activation_history.select(
+            activation_history.contract,
+            activation_history.active,
+            activation_history.final_renewal,
+            Coalesce(activation_history.end_date,
+                datetime.date.max).as_('end_date'),
+            Max(Coalesce(activation_history.end_date, datetime.date.max),
+                window=Window([activation_history.contract])).as_('max_end'),
+            )
+        cursor.execute(*contract.join(win_query,
+                condition=win_query.contract == contract.id
+                ).select(contract.id, where=(contract.status == 'active')
+                & contract.product.in_(renewable_products)
+                & (win_query.end_date == win_query.max_end)
+                & (win_query.end_date <= treatment_date)
+                & (win_query.final_renewal == Literal(False))))
+        return [x for x in cursor.fetchall()]
 
     @classmethod
     def execute(cls, objects, ids, treatment_date):
