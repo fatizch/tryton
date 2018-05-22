fatizch/tryton

commit b57dd902c7414acab56d48f2dafd16d538915a56

endorsement_party_health_fr: do not allow complement health endorsement on social security dependent parties (#1228)
* Do not allow party health complement endorsement if the party is social security dependent
* Do not allow writing on health complement if the party is social security dependent. Display main complement health in this case.

Fix #7371

modules/contract_insurance_health_fr/party.py
@@ -5,9 +5,10 @@ from sql.aggregate import Max
 from collections import defaultdict
 
 from trytond import backend
+from trytond.pyson import Eval, Bool
 from trytond.transaction import Transaction
 from trytond.pool import PoolMeta, Pool
-from trytond.modules.coog_core import fields
+from trytond.modules.coog_core import fields, model
 
 __metaclass__ = PoolMeta
 __all__ = [
@@ -33,22 +34,46 @@ class Party:
         'get_main_insured_ssn', searcher='search_main_insured_ssn')
     main_health_complement = fields.Function(
         fields.Many2One('health.party_complement', 'Main Health Complement',
-            depends=['social_security_insured']),
+            depends=['social_security_dependent'],
+            states={
+                'invisible': Bool(~Eval('social_security_dependent'))}),
         'get_main_health_complement')
     func_key = fields.Function(fields.Char('Functional Key'),
         'get_func_key', searcher='search_func_key')
+
+    @classmethod
+    def __setup__(cls):
+        super(Party, cls).__setup__()
+        cls.health_complement.states['invisible'] |= \
+            Bool(Eval('social_security_dependent'))
+        cls._error_messages.update({
+                'ss_dependent_party': 'This party is social security dependent:'
+                '\n%(dependent_party)s\nYou cannot modify the health complement'
+                })
+
+    @classmethod
+    def write(cls, *args):
+        with model.error_manager():
+            params = iter(args)
+            for parties, values in zip(params, params):
+                ss_dependent_parties = [p for p in parties
+                    if p.social_security_dependent]
+                if ss_dependent_parties:
+                    cls.append_functional_error('ss_dependent_party',
+                        {
+                            'dependent_party': [p.name
+                                for p in ss_dependent_parties]
+                        })
 
     def get_main_health_complement(self, name):
         complement = self.get_health_complement_at_date()
         return complement.id if complement else None
 
     def get_health_complement_at_date(self, at_date=None):
-        res = super(Party, self).get_health_complement_at_date(at_date)
-        if res:
-            return res
         if self.social_security_dependent:
             parent = self.social_security_dependent[0]
             return parent.get_health_complement_at_date(at_date)
+        return super(Party, self).get_health_complement_at_date(at_date)
 
     def get_main_insured_ssn(self, name):
         if self.social_security_dependent:

modules/contract_insurance_health_fr/locale/fr.po
@@ -14,6 +14,16 @@ msgctxt "error:health.party_complement:"
 msgid "Wrong Insurance Fund Number: %s"
 msgstr "Numéro de fond invalide : %s"
 
+msgctxt "error:party.party:"
+msgid ""
+"This party is social security dependent:\n"
+"%(dependent_party)s\n"
+"You cannot modify the health complement"
+msgstr ""
+"Ce party est ayant droit:\n"
+"%(dependent_party)s\n"
+"Vous ne pouvez modifer le complément santé"
+
 msgctxt "error:party.relation:"
 msgid "%s can't be both a social security insured and dependent"
 msgstr "%s ne peut être à la fois assuré social et ayant droit"

modules/contract_insurance_health_fr/party.xml
@@ -25,5 +25,11 @@ this repository contains the full copyright notices and license terms. -->
         <record model="party.relation.type" id="social_security_insured_relation_type" update="1">
             <field name="reverse" ref="contract_insurance_health_fr.social_security_dependent_relation_type"/>
         </record>
+        <!-- Party -->
+        <record model="ir.ui.view" id="party_view_form">
+            <field name="model">party.party</field>
+            <field name="inherit" ref="party.party_view_form"/>
+            <field name="name">party_form</field>
+        </record>
     </data>
 </tryton>

modules/contract_insurance_health_fr/rule_engine.py
@@ -16,9 +16,9 @@ class RuleEngineRuntime:
     @classmethod
     def get_health_complement(cls, args, person):
         hc = None
-        if person and person.health_complement:
+        if person and person.main_health_complement:
             hc = utils.get_value_at_date(
-                person.health_complement, args['date'])
+                person.main_health_complement, args['date'])
         if not hc:
             cls.append_error(args, 'Cannot find health complement information')
             return

modules/contract_insurance_health_fr/view/party_form.xml
@@ -2,11 +2,8 @@
 <!-- This file is part of Coog. The COPYRIGHT file at the top level of
 this repository contains the full copyright notices and license terms. -->
 <data>
-    <xpath expr="/form/group[@id='person']" position="inside">
-        <label name="birth_order"/>
-        <field name="birth_order"/>
-    </xpath>
     <xpath expr="/form/notebook/page[@id='general']" position="inside">
-        <field name="health_complement" colspan="4"/>
+        <label name="main_health_complement"/>
+        <field name="main_health_complement" colspan="4"/>
     </xpath>
 </data>

modules/endorsement_party_health_fr/locale/fr.po
@@ -2,6 +2,14 @@
 msgid ""
 msgstr "Content-Type: text/plain; charset=utf-8\n"
 
+msgctxt "error:endorsement.party.change_health_complement:"
+msgid ""
+"The party %(full_name)s is social security dependent. You cannot change the "
+"health complement"
+msgstr ""
+"Ce tiers %(full_name)s est ayant droit. Vous ne pouvez modifier le "
+"complément santé."
+
 msgctxt "error:endorsement.party.health_complement:"
 msgid "New version"
 msgstr "Nouvelle version"

modules/endorsement_party_health_fr/wizard.py
@@ -24,6 +24,14 @@ class ChangePartyHealthComplement(EndorsementWizardStepMixin):
         'New Health Complement', size=1)
 
     @classmethod
+    def __setup__(cls):
+        super(ChangePartyHealthComplement, cls).__setup__()
+        cls._error_messages.update({
+                'social_security_dependent': 'The party %(full_name)s is social'
+                ' security dependent. You cannot change the health complement',
+                })
+
+    @classmethod
     def is_multi_instance(cls):
         return False
 
@@ -123,6 +131,20 @@ class ChangePartyHealthComplement(EndorsementWizardStepMixin):
                     )
                 h_complement_endorsement.save()
             party_endorsement.save()
+
+    @classmethod
+    def check_before_start(cls, select_screen):
+        super(ChangePartyHealthComplement, cls).check_before_start(
+            select_screen)
+        ss_dependents = [x.party
+            for x in select_screen.endorsement.party_endorsements
+            if x.party.social_security_dependent]
+        ss_depents_names = [' '.join([str(p.name), str(p.first_name or '')])
+            for p in ss_dependents]
+        if ss_dependents:
+            cls.append_functional_error('social_security_dependent', {
+                    'full_name': ss_depents_names,
+                    })
 
 
 class StartEndorsement:

modules/health/tryton.cfg
@@ -3,7 +3,7 @@ version=1.13.0
 depends:
     ir
     res
-    party
+    party_cog
     coog_core
     offered_insurance
     contract_insurance

commit 9daefad5dfa98d621917f7e11d897cce449839cc

contract_insurance_health_fr: Restoration to the call to super on the write of a party (#1841)
Broken since b57dd902c
Fix #8993
Ref #7371

modules/contract_insurance_health_fr/party.py
@@ -68,6 +68,7 @@ class Party:
                             'dependent_party': [p.name
                                 for p in ss_dependent_parties]
                         })
+        super(Party, cls).write(*args)
 
     def get_main_health_complement(self, name):
         complement = self.get_health_complement_at_date()