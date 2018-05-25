d5f56ad5ef7872b4cda9db405026e2431f435141
ClemFaure <ClemFaure@users.noreply.github.com>
2018-05-25 11:27:47 +0200

    claim_reporting: add a searcher on claim_stock_reports function field (#1854)
    Fix #9019

--- modules/claim_reporting/party.py
+++ modules/claim_reporting/party.py
@@ -18,8 +18,13 @@ class Insurer:
     claim_stock_reports = fields.Function(
         fields.Many2Many('insurer-report.template', None,
         None, 'Claim Stock Reports'),
-        'get_claim_stock_reports')
+        'get_claim_stock_reports', searcher='search_claim_stock_reports')
 
     def get_claim_stock_reports(self, name):
-        return [x for x in self.stock_reports
+        return [x.id for x in self.stock_reports
             if x.kind == 'claim_insurer_report']
+
+    @classmethod
+    def search_claim_stock_reports(cls, name, clause):
+        return [('stock_reports',) + tuple(clause[1:]), (
+                'stock_reports.kind', '=', 'claim_insurer_report')]
