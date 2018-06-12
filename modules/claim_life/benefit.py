fe387bb8a4d5fe5817662a800aeb7bdd03215503
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-06-12 11:06:59 +0200

    Fix flake8, add changelog

--- modules/claim_life/benefit.py
+++ modules/claim_life/benefit.py
@@ -1,6 +1,6 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import PoolMeta
 from trytond.pyson import Eval, Bool, Or, In, If
 
 from trytond.modules.coog_core import fields, model
