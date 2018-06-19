c4879d0bf90dd515860073c92491e3665975c2da
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-06-19 10:04:14 +0200

    insurer_reporting: Effectively remove field following 606aaa9d9

--- modules/insurer_reporting/account.py
+++ modules/insurer_reporting/account.py
@@ -1,9 +1,7 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import logging
-
 from trytond.pool import PoolMeta, Pool
-from trytond.modules.coog_core import fields, model
+from trytond.modules.coog_core import model
 
 __all__ = [
     'Invoice',
@@ -13,25 +11,6 @@ __all__ = [
 class Invoice:
     __metaclass__ = PoolMeta
     __name__ = 'account.invoice'
-
-    report_commissions = fields.Function(
-        fields.One2Many('commission', None, 'Commissions To Report'),
-        'get_report_commissions')
-
-    @classmethod
-    def __setup__(cls):
-        super(Invoice, cls).__setup__()
-        logger = logging.getLogger('trytond.fields')
-        logger.warning('[DEPRECATION WARNING]: Field report_commissions will '
-            'be removed as of version 2.2')
-
-    def get_report_commissions(self, name):
-        good_lines = [x for x in self.lines if x.from_commissions and x.amount]
-        commissions = [x for good_line in good_lines
-            for x in good_line.from_commissions]
-        commissions.sort(key=lambda x: x.commissioned_contract.id)
-        commissions = [x.id for x in commissions]
-        return commissions
 
     def insurer_reporting_lines(self, **kwargs):
         domain = [
