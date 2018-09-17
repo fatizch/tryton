f9d17235db43c7fec0b63431ce49a3737e905651
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-09-17 15:57:11 +0200

    bin: Update generate_test_base script

--- misc/python_scripts/generate_test_base.py
+++ misc/python_scripts/generate_test_base.py
@@ -7,18 +7,19 @@ import subprocess
 try:
     _, source_db, target_db = sys.argv[:3]
     if len(sys.argv) == 4:
-        pg_user = sys.argv[3]
+        conn_info = sys.argv[3].split(' ')
     else:
-        pg_user = 'tryton'
+        conn_info = ['-U', 'tryton']
 except ValueError:
     sys.stderr.write('''
 Usage :
-    generate_test_base.py <source_db> <target_db> [db_user]
+    generate_test_base.py <source_db> <target_db> [connexion_info]
 
     Designed and tested for postgresql only.
 
     <target_db> must be an already existing empty database.
-    The default postgresql user is 'tryton'.
+    <connexion_info> will be passed as parameters to the psql command (use it
+        to set user, host, etc...)
 
 ''')
     sys.exit()
@@ -31,23 +32,24 @@ if not conf_folder:
 
 conf_file = os.path.join(conf_folder, 'db_export.json')
 
-print 'Reading configuration file'
+print('Reading configuration file')
 with open(conf_file, 'r') as f:
     exclude = json.loads(f.read())
 
-cmd = ['pg_dump', '-U', pg_user, '-n', 'public']
+cmd = ['pg_dump', '-n', 'public'] + conn_info
 for table in exclude:
     cmd += ['--exclude-table-data', table]
 
-cmd += [source_db, '|', 'psql', '-U', pg_user, '-d', target_db]
+cmd += [source_db, '|', 'psql', '-d', target_db] + conn_info
 
-print 'Copying trimmed database'
+print('Copying trimmed database')
 with open(os.devnull, 'w') as devnull:
     p = subprocess.Popen(' '.join(cmd), shell=True, stdout=devnull)
     p.communicate()
 
 # Ugly hack to remove unused lines
-constraints = 'psql -U ' + pg_user + ' -c "\\d+ party_party;" ' + target_db
+constraints = 'psql -c "\\d+ party_party;" '
+constraints += target_db + ' ' + ' '.join(conn_info)
 constraints += ' | grep "^ *TABLE"'
 constraints += ' | grep "ON DELETE RESTRICT"'
 constraints += ' | sed -e "s/.*TABLE \\\"\([^\\\"]*\)\\\".*FOREIGN KEY '
@@ -66,8 +68,8 @@ for constraint in out.split('\n'):
 query = 'DELETE FROM party_party WHERE id NOT IN ('
 query += ' UNION '.join(exclude) + ')'
 
-print 'Removing useless parties'
-cmd = 'psql -U ' + pg_user + ' -c "' + query + '" -d ' + target_db
+print('Removing useless parties')
+cmd = 'psql -c "' + query + '" -d ' + target_db + ' ' + ' '.join(conn_info)
 with open(os.devnull, 'w') as devnull:
     p = subprocess.Popen(cmd, shell=True, stdout=devnull)
     p.communicate()
