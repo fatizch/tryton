23501fd26533697a7adc214edb3b26ee89f3ab1d
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2018-09-21 10:57:02 +0200

    claim_group_life_fr: Add default deductible rules (#2173)
    Fix #9904

--- modules/claim_deduction_period/rule_engine.py
+++ modules/claim_deduction_period/rule_engine.py
@@ -1,5 +1,6 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import datetime
 from decimal import Decimal
 
 from trytond.pool import PoolMeta
@@ -17,6 +18,43 @@ class RuleEngineRuntime:
     __name__ = 'rule_engine.runtime'
 
     @classmethod
+    def _period_is_valid(cls, loss, period, start_date, end_date,
+            allowed):
+        if period.deduction_kind not in allowed:
+            return False
+        if period.start_date > (end_date or datetime.date.min):
+            return False
+        latest_start = max(start_date, period.start_date)
+        if not period.end_date:
+            cls.append_error((loss, period, start_date, end_date),
+                'the end date must be defined for '
+                'the deduction %s from %s' % (period.deduction_kind.name,
+                    period.start_date))
+        earliest_end = min(end_date, period.end_date)
+        days = (earliest_end - latest_start).days + 1
+        if days <= 0:
+            return False
+        return True
+
+    @classmethod
+    @check_args('loss')
+    def _re_get_deduction_periods(cls, args, code, start_date,
+            end_date):
+        loss = args['loss']
+        allowed = {x for x in loss.loss_desc.deduction_period_kinds
+            if not code or x.code == code}
+        periods = []
+        if code and code not in (x.code
+                for x in loss.loss_desc.deduction_period_kinds):
+            return periods
+        for period in loss.deduction_periods:
+            if not cls._period_is_valid(loss, period, start_date, end_date,
+                    allowed):
+                continue
+            periods.append((period.start_date, period.end_date))
+        return periods
+
+    @classmethod
     @check_args('loss')
     def _re_get_deduction_period_amount(cls, args, code, start_date,
             end_date, daily=True, round=False):
@@ -29,20 +67,12 @@ class RuleEngineRuntime:
                 for x in loss.loss_desc.deduction_period_kinds):
             return amount
         for period in loss.deduction_periods:
-            if period.deduction_kind not in allowed:
-                continue
-            if period.start_date > end_date:
+            if not cls._period_is_valid(loss, period, start_date, end_date,
+                    allowed):
                 continue
             latest_start = max(start_date, period.start_date)
-            if not period.end_date:
-                cls.append_error(args, 'the end date must be defined for '
-                    'the deduction %s from %s' % (period.deduction_kind.name,
-                        period.start_date))
-                return amount
             earliest_end = min(end_date, period.end_date)
             days = (earliest_end - latest_start).days + 1
-            if days <= 0:
-                continue
             if daily:
                 amount += period.on_change_with_daily_amount(round=round)
             else:
