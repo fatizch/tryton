62b6f8c7f18d9b24bd98bcb706e9207bc0461106
manalaboutayeb <manal.aboutayeb@coopengo.com>
2019-01-21 16:10:15 +0100

    claim_health: default claim bank account is contract's direct debit account first if it exists (#2482)
    Fix #10683

--- modules/claim_health/contract.py
+++ modules/claim_health/contract.py
@@ -47,13 +47,13 @@ class CoveredElement(metaclass=PoolMeta):
         return res
 
     def get_claim_default_bank_account(self, name=None):
-        account = self.contract.subscriber.get_bank_account(utils.today())
-        if account:
-            return account.id
         if utils.is_module_installed('contract_insurance_invoice'):
             billing_info = self.contract.billing_information
             if billing_info and billing_info.direct_debit_account:
                 return billing_info.direct_debit_account
+        account = self.contract.subscriber.get_bank_account(utils.today())
+        if account:
+            return account.id
 
     def get_has_health_related_option(self, name=None):
         for option in self.options:
