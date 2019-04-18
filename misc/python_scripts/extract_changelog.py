aa48460e7d5554625a0d770b5af62fa612e3c034
Frederic Langevin <frederic.langevin@coopengo.com>
2019-04-18 14:19:02 +0200

    misc: Improve major version extract changelog script

--- misc/python_scripts/extract_changelog.py
+++ misc/python_scripts/extract_changelog.py
@@ -15,29 +15,38 @@ Usage :
     sys.exit()
 
 conf = OrderedDict([
-    ('Party management', ['party_cog', 'party_ssn']),
+    ('Party management', ['party_cog', 'party_ssn', 'party_fr']),
     ('Contract management', ['contract_distribution', 'contract_group',
         'contract_insurance', 'contract_term_renewal', 'distribution_portfolio',
-        'document_request_electronic_signature', 'offered']),
-    ('Contract Endorsement', ['endorsement_commission']),
+        'document_request_electronic_signature', 'offered', 'contract',
+        'contract_identifiers', 'contract_insurance_health_fr',
+        'contract_underwriting'
+        ]),
+    ('Contract Endorsement', ['endorsement_commission', 'endorsement',
+        'endorsement_party', 'endorsement_premium', 'endorsement_process']),
     ('Claim', ['claim', 'claim_eligibility', 'claim_group_life_fr',
         'claim_indemnification', 'claim_indemnification_group', 'claim_life',
         'claim_pasrau', 'claim_prest_ij_service', 'claim_process',
-        'claim_salary_fr', 'claim_group_life_fr']),
+        'claim_salary_fr', 'claim_group_life_fr', 'claim_insurer',
+        'claim_reporting', 'dsn_standard', 'underwriting',
+        'underwriting_claim']),
     ('Billing', ['account_invoice_cog', 'contract_instalment_plan',
         'contract_insurance_invoice', 'contract_insurance_invoice_dunning',
         ]),
-    ('Payment', ['account_payment_cog']),
+    ('Payment', ['account_payment_cog', 'account_payment_sepa_cog']),
     ('Accounting', ['account_cog', 'account_per_product',
-        'analytic_account_aggregate', 'analytic_coog', 'bank_fr']),
+        'analytic_account_aggregate', 'analytic_coog', 'bank_fr',
+        'account_invoice_slip']),
     ('Commission', ['analytic_commission', 'commission_insurance',
         'commission_insurance_prepayment', 'commission_insurance_recovery',
-        'commission_insurer', 'insurer_reporting']),
+        'commission_insurer', 'insurer_reporting', 'commission_postponed']),
     ('Loan', ['loan']),
     ('Report Engine', ['report_engine']),
     ('Process', ['process_cog']),
     ('Rule Engine', ['rule_engine']),
-    ('Technical/Core', ['coog_core', 'sequence_coog']),
+    ('Transverse Functionnalities', ['event_log', 'event_log_party']),
+    ('Technical/Core', ['coog_core', 'sequence_coog', 'migrator_claim',
+        'migrator_contract_group', 'migrator']),
     ('Other', []),
     ('COOG API', []),
     ('COOG APP', []),
@@ -57,6 +66,7 @@ translation_fr = {
     'Report Engine': 'Editique',
     'Rule Engine': 'Moteur de règle',
     'Technical/Core': 'Noyau Technique',
+    'Transverse Functionnalities': 'Fonctionnalités transerves',
     'Other': 'Autre',
     'COOG API': 'COOG API',
     'COOG APP': 'COOG APP',
@@ -127,11 +137,11 @@ with open(os.path.join(coog_app_dir, 'doc', language, 'CHANGELOG'),
     nb_version_functionnality += treat_changelog('COOG APP', changelog,
         version_report)
 
-print('\n\n----------%s New functionnalities------------------\n\n' % \
+print('\n\n----------%s New functionnalities------------------\n\n' %
     nb_version_functionnality)
 print('\n\n----------Release note document------------------\n\n')
 
-print('COOG BACKEND' if language == 'en' else 'Back office COOG')
+print('## COOG BACKEND\n' if language == 'en' else '## Back office COOG\n')
 
 for key in list(conf.keys()):
     title = translation_fr[key] if language == 'fr' else key
