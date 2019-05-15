60972087c66951de76259f170a86d1c7adc77661
manalaboutayeb <manal.aboutayeb@coopengo.com>
2019-05-15 10:03:01 +0200

    endorsement_process: hotfix of client crash (#2780)
    Fix #0000

--- modules/endorsement_process/wizard.py
+++ modules/endorsement_process/wizard.py
@@ -266,4 +266,8 @@ class AskNextEndorsement(model.CoogWizard):
         with ServerContext().set_context(force_contracts_to_endorse=True):
             with Transaction().set_context(force_synchronous=True):
                 Pool().get('endorsement').apply([self.get_endorsement()])
-                return action, {}
+                return action, {
+                    'model': Transaction().context.get('active_model'),
+                    'id': Transaction().context.get('active_id'),
+                    'ids': [Transaction().context.get('active_id')]
+                    }
