6d1e1f9e20710b9152df9e6f51b7d764c4edb1e1
Jean Cavallo <jean.cavallo@hotmail.fr>
2019-06-18 13:35:11 +0200

    claim_salary_fr: Update agirc / arco tables (#2812)
    Fix #11767

--- modules/claim_salary_fr/rule_engine.py
+++ modules/claim_salary_fr/rule_engine.py
@@ -2,13 +2,122 @@
 # this repository contains the full copyright notices and license terms.
 from decimal import Decimal
 
-from trytond.pool import PoolMeta
+from trytond import backend
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
 from trytond.modules.rule_engine import check_args
 
+HARDCODED_TABLES = ['pmss', 'COOG_AGIRC', 'COOG_ARRCO']
+
 __all__ = [
+    'Table',
+    'DimensionValue',
+    'Cell',
     'RuleEngineRuntime',
     'RuleEngine',
     ]
+
+
+class Table(metaclass=PoolMeta):
+    __name__ = 'table'
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._error_messages.update({
+                'modify_configuration_table': 'You cannnot modify the content '
+                'of tables from the configuration',
+                })
+
+    @classmethod
+    def __register__(cls, module):
+        TableHandler = backend.get('TableHandler')
+        handler = TableHandler(cls, module)
+
+        # Since we provide some reference tables, we need to make sure they are
+        # up to date and accurate.
+        # To do so, we wipe them in order to avoid conflict with values that
+        # could have been created manually by the clients
+
+        clean_up_default_tables = handler.table_exist('table')
+
+        super().__register__(module)
+
+        if clean_up_default_tables:
+            pool = Pool()
+            dimensions = pool.get('table.dimension.value').__table__()
+            cells = pool.get('table.cell').__table__()
+            data = pool.get('ir.model.data').__table__()
+            cursor = Transaction().connection.cursor()
+
+            # Find the tables / dimensions / cells
+            cursor.execute(*data.select(data.db_id,
+                    where=(data.module == 'claim_salary_fr')
+                    & data.fs_id.in_(HARDCODED_TABLES)))
+            table_ids = [x[0] for x in cursor.fetchall()]
+
+            if not table_ids:
+                return
+
+            cursor.execute(*dimensions.select(dimensions.id,
+                    where=dimensions.definition.in_(table_ids)))
+            dimensions_ids = [x[0] for x in cursor.fetchall()]
+
+            cursor.execute(*cells.select(cells.id,
+                    where=cells.definition.in_(table_ids)))
+            cells_ids = [x[0] for x in cursor.fetchall()]
+
+            # Clean up cells
+            if cells_ids:
+                cursor.execute(*data.delete(
+                        where=(data.module == 'claim_salary_fr')
+                        & (data.model == 'table.cell')
+                        & data.db_id.in_(cells_ids)))
+                cursor.execute(*cells.delete(
+                        where=cells.id.in_(cells_ids)))
+
+            # Clean up dimensions
+            if dimensions_ids:
+                cursor.execute(*data.delete(
+                        where=(data.module == 'claim_salary_fr')
+                        & (data.model == 'table.dimension.value')
+                        & data.db_id.in_(dimensions_ids)))
+                cursor.execute(*dimensions.delete(
+                        where=dimensions.id.in_(dimensions_ids)))
+
+
+class DimensionValue(metaclass=PoolMeta):
+    __name__ = 'table.dimension.value'
+
+    @classmethod
+    def create(cls, vlist):
+        Table = Pool().get('table')
+
+        if Transaction().user != 0:
+            hardcoded_tables = [
+                Table.get_table_by_code(code) for code in HARDCODED_TABLES]
+            hardcoded_tables_ids = [x.id for x in hardcoded_tables if x]
+            if any(x.get('definition', 0) in hardcoded_tables_ids
+                    for x in vlist):
+                Table.raise_user_error('modify_configuration_table')
+        return super().create(vlist)
+
+
+class Cell(metaclass=PoolMeta):
+    __name__ = 'table.cell'
+
+    @classmethod
+    def create(cls, vlist):
+        Table = Pool().get('table')
+
+        if Transaction().user != 0:
+            hardcoded_tables = [
+                Table.get_table_by_code(code) for code in HARDCODED_TABLES]
+            hardcoded_tables_ids = [x.id for x in hardcoded_tables if x]
+            if any(x.get('definition', 0) in hardcoded_tables_ids
+                    for x in vlist):
+                Table.raise_user_error('modify_configuration_table')
+        return super().create(vlist)
 
 
 class RuleEngine(metaclass=PoolMeta):
--- modules/claim_salary_fr/tests/test_module.py
+++ modules/claim_salary_fr/tests/test_module.py
@@ -2,6 +2,9 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import unittest
+import datetime
+
+from dateutil.relativedelta import relativedelta
 
 import trytond.tests.test_tryton
 
@@ -13,6 +16,38 @@ class ModuleTestCase(test_framework.CoogTestCase):
     Test Coog module.
     '''
     module = 'claim_salary_fr'
+
+    @classmethod
+    def get_models(cls):
+        return {
+            'Table': 'table',
+            'Cell': 'table.cell',
+            }
+
+    def test0000_checkTableAreUpToDate(self):
+        '''
+            This test will make sure that AGIRC / ARCO / PMSS tables are up to
+            date by failing if no values are found for the new year 1 month
+            before the end of the previous year.
+
+            If for some reasons the data is not yet available, setting the
+            delta to two weeks may be done, but deployments to clients will
+            have to be quick.
+        '''
+        tables = self.Table.search([('xml_id', 'in', (
+                        'claim_salary_fr.table_agirc',
+                        'claim_salary_fr.table_arrco',
+                        'claim_salary_fr.table_pmss',
+                        ))])
+        self.assertEqual(len(tables), 3)
+
+        # Changing this is not the solution, the table.xml file should be
+        # updated with the new values
+        target_date = datetime.date.today() + relativedelta(months=1)
+        for table in tables:
+            self.assertTrue(self.Cell.get(table, target_date) is not None)
+            self.assertTrue(
+                self.Cell.get(table, datetime.date.today()) is not None)
 
 
 def suite():
