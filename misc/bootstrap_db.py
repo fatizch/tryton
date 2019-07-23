18cf67ed38ca75f9fafd8f873b5fdd20b0931cea
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2019-07-23 15:00:36 +0200

    coog: misc - Fix bootstrap DB (#2966)
    Fix #00000

--- misc/bootstrap_db.py
+++ misc/bootstrap_db.py
@@ -14,11 +14,11 @@ def parse_environ(name, default):
     return value
 
 
-DB_USER = parse_environ('COOG_DB_USER', 'tryton')
-DB_PASSWORD = parse_environ('COOG_DB_PASSWORD', 'tryton')
+DB_USER = parse_environ('ADMIN_DB_USER', 'tryton')
+DB_PASSWORD = parse_environ('ADMIN_DB_PASSWORD', 'tryton')
 DB_HOST = parse_environ('COOG_POSTGRES_HOST', 'localhost')
 DB_NAME = parse_environ('COOG_DB_NAME', 'coog')
-DB_PORT = parse_environ('COOG_DB_PORT', 5432)
+DB_PORT = parse_environ('COOG_POSTGRES_PORT', 5432)
 
 assert all([DB_NAME, DB_PASSWORD, DB_HOST, DB_USER, DB_PORT]
     ), 'DB connection data not set'
@@ -28,13 +28,19 @@ def get_connection(new=False):
     try:
         conn_string = "user='%s' host='%s' password='%s' port='%s'"
         conn_args = (DB_USER, DB_HOST, DB_PASSWORD, DB_PORT)
+        conn_string += " dbname='%s'"
+        conn_args += (DB_NAME,)
+        connection = psycopg2.connect(conn_string % conn_args)
+        cur = connection.cursor()
         if not new:
-            conn_string += " dbname='%s'"
-            conn_args += (DB_NAME,)
-        return psycopg2.connect(conn_string % conn_args)
+            cur.execute('SELECT 1 from ir_modules')
+            cur.execute('SELECT 1 from res_user')
+        return connection
     except psycopg2.OperationalError:
         if new:
             raise
+        return None
+    except psycopg2.ProgrammingError:
         return None
 
 
@@ -51,7 +57,9 @@ def main():
     conn.autocommit = True
     cur = conn.cursor()
 
+    # Will crash if the database already exists
     cur.execute("CREATE DATABASE %s;" % DB_NAME)
+
     # }}}
 
     os.system('echo "enT2BduLKrxI" > /tmp/trpass')
@@ -59,5 +67,6 @@ def main():
     os.system('TRYTONPASSFILE=/tmp/trpass trytond-admin -d %s '
         '--email %s -u ir' % (
             DB_NAME, 'admin@coopengo.com'))
+
 
 main()
