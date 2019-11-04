3b3f4cfc9ffaac9e1034da172811aa61a82d1e66
PierreCookie <pkunkel@sfR.fr>
2019-11-04 18:49:49 +0100

    health: Handle party api birth order (#3303)
    Ref #13248

--- modules/health/api.py
+++ modules/health/api.py
@@ -31,6 +31,12 @@ class APIParty(metaclass=PoolMeta):
     __name__ = 'api.party'
 
     @classmethod
+    def _party_person_schema(cls):
+        schema = super()._party_person_schema()
+        schema['properties']['birth_order'] = {'type': 'number'}
+        return schema
+
+    @classmethod
     def _update_person(cls, party, data, options):
         super()._update_person(party, data, options)
         cls._update_party_complement(party, data, options)
