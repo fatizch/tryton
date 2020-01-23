dad2dbc8ac27ff5ebd109f8e716a2ca1590b11a1
jmousset <jeremy.mousset@coopengo.com>
2020-01-23 17:23:58 +0100

    scripts: add raw option to generate_yml_api_doc (#3597)

--- misc/python_scripts/generate_yml_api_doc.py
+++ misc/python_scripts/generate_yml_api_doc.py
@@ -1,16 +1,19 @@
 import argparse
-from trytond.config import parse_uri
-from trytond.config import config as trytond_config
-from proteus import config, Model
+import json
 import yaml
 
+from proteus import config, Model
 
-def get_json_api_desc(api_name, config):
+
+def get_json_api_desc(api_name, config, raw=False):
     splitted = api_name.split('.')
     method = splitted[-1]
     api_model = '.'.join(splitted[:-1])
     api = Model.get(api_model)
     description = getattr(api, method + '_description')(config.context)
+    if raw:
+        print(json.dumps(description, indent=4))
+        return
     for inout in ('input', 'output'):
         schema = description['%s_schema' % inout]
         schema.update({'example': description['examples'][0][inout]})
@@ -29,13 +32,15 @@ def main():
     parser.add_argument('--host', default='localhost', help='Coog Server Host')
     parser.add_argument('api',
             help='The API name. For example: api.party.upload_documents')
+    parser.add_argument("--raw", help="print raw json output instead of yml",
+        action="store_true")
     arguments = parser.parse_args()
 
     proteus_conf = config.set_xmlrpc('http://%s:%s@%s:%s/%s/' % (
         arguments.user, arguments.password, arguments.host,
         arguments.port, arguments.database))
 
-    get_json_api_desc(arguments.api, proteus_conf)
+    get_json_api_desc(arguments.api, proteus_conf, raw=arguments.raw)
 
 
 if __name__ == '__main__':
